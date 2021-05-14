389274bc0fadf7cdd7dffa6b6588f62c2b89235b
pcmodeactivate <xiaoyucai2015@gmail.com>
2021-05-13 21:31:31 -0400

    hooked to server

--- prototype/plot_plotly.py
+++ prototype/plot_plotly.py
@@ -1,4 +1,5 @@
 import collections
+from collections import deque
 import datetime
 import time
 import msgpack
@@ -6,13 +7,13 @@ import plotly
 import numpy as np
 import zmq
 
-#context = zmq.Context()
+context = zmq.Context()
 
-#receiver = context.socket(zmq.SUB)
-#receiver.connect("tcp://localhost:5560") # or whatever url the server is running on
-#receiver.setsockopt(zmq.SUBSCRIBE, b"") # subscribe to all messages
+receiver = context.socket(zmq.SUB)
+receiver.connect("tcp://localhost:5560") # or whatever url the server is running on
+receiver.setsockopt(zmq.SUBSCRIBE, b"") # subscribe to all messages
 
-#print("Connected to 0MQ server.")
+print("Connected to 0MQ server.")
 
 import dash
 import dash_core_components as dcc
@@ -23,14 +24,13 @@ import plotly.express as px
 import pandas as pd
 
 app = dash.Dash()
-dt = [4, 1, 2, 2, 4, 5]
-t = [1, 2, 3, 4, 5, 6]
 
-df = pd.DataFrame(
-    dict(time=t, data=dt)
-)
+dt = deque(np.zeros(6*100))
+t = deque(np.arange(6*100))
 
-fig = px.scatter(df, x=t, y=dt)
+df = pd.DataFrame(dict(time=t, data=dt))
+
+fig = px.line(df, x=t, y=dt)
 
 fig.update_layout(
     title="Test Layout",
@@ -48,7 +48,7 @@ app.layout = html.Div(children=[
 
     dcc.Interval(
         id='graph-update',
-        interval=1*1000
+        interval=1*1000 #Millisecond
     )]
 )
 
@@ -57,10 +57,23 @@ app.layout = html.Div(children=[
     Input('graph-update', 'n_intervals'))
 def update_figure(n): 
 
-    t.append(t[-1]+1)
-    dt.append(dt[-1]+1)
+    #sent = time.time() + 1
+    while receiver.poll(1): # Timeout of 1 ms checking for new data
+        sent, new = msgpack.unpackb(receiver.recv())
+        dt.append(new[0][0]) #new[1..16 sensors][0]    
+        dt.popleft()
+        #unbundle new
 
-    fig = px.scatter(df, x=t, y=dt)
+        #for channel, points, line in zip(data, new, lines):
+            # Just plot the first data point. Since the server bulk reads 10 samples at
+            # once this effectively downsamples to 100 samples/sec
+        #    channel.popleft()
+        #    channel.append(points[0])
+        #    line.set_ydata(channel)
+        #count += len(new[0])
+        
+
+    fig = px.line(df, x=t, y=dt)
 
     fig.update_layout(transition_duration=500)
 
