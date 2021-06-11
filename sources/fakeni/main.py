d7cccfa1407a6c7c2e2c6186da94cfad412f9f78
pcmodeactivate <xiaoyucai2015@gmail.com>
2021-06-10 23:45:56 -0400

    single channel test version

--- sources/fakeni/main.py
+++ sources/fakeni/main.py
@@ -1,19 +1,17 @@
 # FakeNI - Mimic the output of the NI source with dummy data for testing.
 import random
 import time
-from omnibus import Sender, Message
-
+from omnibus import Sender
 READ_BULK = 200  # mimic how the real NI box samples in bulk for better performance
 SAMPLE_RATE = 20000  # total samples/second
 CHANNELS = 8  # number of analog channels to read from
-CHANNEL_NAMES = [f"DAQ/Fake{i}" for i in range(CHANNELS)] # analog channels to read from
-
-sender = Sender("")
-
+sender = Sender("DAQ/Fake")
 while True:
     start = time.time()
-    for channel in CHANNEL_NAMES:
-        # send a tuple of when the data was recorded and an array of the data
-        payload = (start, [random.random() for _ in range(READ_BULK)])
-        sender.send_message(Message(channel, time.time(), payload))
+    # send a tuple of when the data was recorded and an array of the data for each channel
+    data = {
+        "timestamp": start,
+        "data": {f"Fake{i}": [random.random() for _ in range(READ_BULK)] for i in range(CHANNELS)}
+    }
+    sender.send(data)
     time.sleep(max(READ_BULK/SAMPLE_RATE - (time.time() - start), 0))
