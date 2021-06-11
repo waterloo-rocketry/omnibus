8ce14738e58dff3f35719789cc1f06729c407365
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2021-06-10 22:25:36 -0400

    Global log: Use automatic ip detection

--- sinks/globallog/main.py
+++ sinks/globallog/main.py
@@ -6,12 +6,11 @@ from omnibus import Receiver
 
 # Will log all messages passing through bus
 CHANNEL = ""
-SERVER = "tcp://localhost:5076"
 # Retrieves current date and time
 CURTIME = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
 # Creates filename
 fname = CURTIME + ".log"
-receiver = Receiver(SERVER, CHANNEL)
+receiver = Receiver(CHANNEL)
 # Creates new file
 with open(fname, "w") as f:
     while True:
