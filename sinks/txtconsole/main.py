204e8fb08cff5f9e5b864d4e72dd33118ccc91d8
James Li <77023901+jli35@users.noreply.github.com>
2021-06-15 21:03:20 -0400

    Txtconsole: modified main.py
    Fixed possible bug where if the channel happens to be the file name

--- sinks/txtconsole/main.py
+++ sinks/txtconsole/main.py
@@ -2,15 +2,15 @@ import sys
 
 from omnibus import Receiver
 
-def print_console():
+def print_console(channels_filter):
     receiver = Receiver(CHANNEL)
-    print('Cmd line arguments entered: ')
-    print(sys.argv)
+    print('Filter/Cmd line arguments entered: ')
+    print(channels_filter)
     while True:
         msg = receiver.recv_message()
-        if msg.channel in sys.argv:
+        if msg.channel in channels_filter:
             print(msg.payload)
 
 if __name__ == '__main__':
     CHANNEL = ""  # all channels
-    print_console()
+    print_console(sys.argv[1:])
