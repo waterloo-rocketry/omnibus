0ee76f0475c5fa6dfb69e0b2c6e43747f11fabaf
Robert Cai <xiaoyucai2015@gmail.com>
2021-06-23 22:25:31 -0400

    documentation update & intuitive variable decl.

--- sinks/plot/main.py
+++ sinks/plot/main.py
@@ -8,16 +8,22 @@ import numpy as np
 
 import time
 
+demo = True #Demo Mode
 CHANNEL = "DAQ"
-
-SENSOR_COUNT = 8
-
-# Sensor names; make sure this matches the names at the source!
+SENSOR_COUNT = 0
 SENSORS = []
-for i in range(SENSOR_COUNT):
-    SENSORS.append("Fake"+str(i))
 
-receiver = Receiver(CHANNEL)  # Receiving everything in DAQ channel
+if (demo):
+    CHANNEL = "DAQ"
+
+    SENSOR_COUNT = 8
+
+    # Sensor names; make sure this matches the names at the source!
+    SENSORS = []
+    for i in range(SENSOR_COUNT):
+        SENSORS.append("Fake"+str(i))
+
+    receiver = Receiver(CHANNEL)  # Receiving everything in DAQ channel
 
 print("Connected to 0MQ server.")
 
@@ -31,8 +37,8 @@ pg.setConfigOptions(antialias=True)
 # Layout Algorithm
 plots = []
 
-min_col = int(np.ceil(np.sqrt(SENSOR_COUNT)))
-min_row = int(np.ceil(SENSOR_COUNT / min_col))
+min_col = int(np.sqrt(SENSOR_COUNT))+1
+min_row = (SENSOR_COUNT / min_col)+1
 for i in range(0, min_row):
     for j in range(min_col*i, min_col*(i+1) if i != (min_row - 1) else SENSOR_COUNT):
         plots.append(win.addPlot(title=("Sensor: " + SENSORS[j])))
@@ -42,7 +48,8 @@ for i in range(0, min_row):
 # plot generation
 curves = [plots[i].plot(pen='y') for i in range(SENSOR_COUNT)]
 
-data_streams = [[0 for _ in range(100)] for _ in range(SENSOR_COUNT)]  # 100 data points
+graph_dp = 100 # 100 data points in the graph
+data_streams = [[0 for _ in range(graph_dp)] for _ in range(SENSOR_COUNT)]  
 
 
 def update():
@@ -51,7 +58,11 @@ def update():
             data_streams[i].pop(0)
             # Update Data Stream, currently only grabbing the first element in the payload obj
             data_streams[i].append(new_data["data"][sensor][0])
-
+            # new_data is the received the payload object of class message (see omnibus.py)
+            # whereby the payload is currently parsed as dictionary of a list (of a list)
+            # the dictionary should have a key value pair where the key is the "data type", in this case "data"
+            # the value is then a list of sensors, which consist of a list of data registered from each sensor
+            # currently we are only plotting the first data point in each sensor
             curves[i].setData(data_streams[i])  # Update Graph Stream
 
 
