540a2d17f2ed1e054aca5f522434bea0ad6b948b
Robert Cai <xiaoyucai2015@gmail.com>
2021-06-28 23:43:49 -0400

    fixed omnibus decoding

--- omnibus/omnibus.py
+++ omnibus/omnibus.py
@@ -120,7 +120,7 @@ class Receiver(OmnibusCommunicator):
 
         if self.subscriber.poll(timeout):
             channel, timestamp, payload = self.subscriber.recv_multipart()
-            return Message(channel.decode("utf-8"), msgpack.unpackb(timestamp))
+            return Message(channel.decode("utf-8"), msgpack.unpackb(timestamp), msgpack.unpackb(payload))
         return None
 
     def recv(self, timeout=None):
