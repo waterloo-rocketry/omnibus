12b1276db1f2eb14cc8db43ff03e211a0b5930d3
Robert Cai <xiaoyucai2015@gmail.com>
2021-07-13 14:29:53 -0400

    PyQt Plot Implementation (new PR) (#11)
    Python plotting completed for first iteration
    
    Co-authored-by: Jack C <22122986+BluCodeGH@users.noreply.github.com>

--- sources/fakeni/main.py
+++ sources/fakeni/main.py
@@ -9,11 +9,14 @@ READ_BULK = 200  # mimic how the real NI box samples in bulk for better performa
 SAMPLE_RATE = 20000  # total samples/second
 CHANNELS = 8  # number of analog channels to read from
 
-sender = Sender("NI/Fake")
+sender = Sender("DAQ/Fake")
 
 while True:
     start = time.time()
     # send a tuple of when the data was recorded and an array of the data for each channel
-    data = (start, [[random.random() for _ in range(READ_BULK)] for _ in range(CHANNELS)])
+    data = {
+        "timestamp": start,
+        "data": {f"Fake{i}": [random.random() for _ in range(READ_BULK)] for i in range(CHANNELS)}
+    }
     sender.send(data)
     time.sleep(max(READ_BULK/SAMPLE_RATE - (time.time() - start), 0))
