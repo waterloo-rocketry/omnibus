aeecbaacea0f1cf1d044f5799a405cbad4b31605
Ben Levy <ben@hmjbl.com>
2021-07-14 10:37:59 -0400

    fix message_types.py

--- sources/parsley/message_types.py
+++ sources/parsley/message_types.py
@@ -4,15 +4,18 @@
  # GENERAL CMD:    TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    COMMAND_TYPE            None            None            None            None
 # VENT_VALVE_CMD:  TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    VENT_VALVE_STATE        None            None            None            None
 # INJ_VALVE_CMD:   TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    INJ_VALVE_STATE         None            None            None            None
+# ALT_ARM_CMD:     TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    ALT_ARM_STATE & #       None            None            None            None
 # DEBUG_MSG:       TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    DEBUG_LEVEL | LINUM_H   LINUM_L         MESSAGE_DEFINED MESSAGE_DEFINED MESSAGE_DEFINED
 # DEBUG_PRINTF:    ASCII       ASCII        ASCII          ASCII                   ASCII           ASCII           ASCII           ASCII
 # VENT_VALVE_STAT: TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    VENT_VALVE_STATE        CMD_VALVE_STATE None            None            None
 # INJ_VALVE_STAT:  TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    INJ_VALVE_STATE         CMD_VALVE_STATE None            None            None
+# ALT_ARM_STAT:    TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    ALT_ARM_STATE & #       V_DROGUE_H      V_DROGUE_L      V_MAIN_H        V_MAIN_L
 # BOARD_STAT:      TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    ERROR_CODE              BOARD_DEFINED   BOARD_DEFINED   BOARD_DEFINED   BOARD_DEFINED
 # SENSOR_ACC:      TSTAMP_MS_M TSTAMP_MS_L  VALUE_X_H      VALUE_X_L               VALUE_Y_H       VALUE_Y_L       VALUE_Z_H       VALUE_Z_L
 # SENSOR_GYRO:     TSTAMP_MS_M TSTAMP_MS_L  VALUE_X_H      VALUE_X_L               VALUE_Y_H       VALUE_Y_L       VALUE_Z_H       VALUE_Z_L
 # SENSOR_MAG:      TSTAMP_MS_M TSTAMP_MS_L  VALUE_X_H      VALUE_X_L               VALUE_Y_H       VALUE_Y_L       VALUE_Z_H       VALUE_Z_L
 # SENSOR_ANALOG:   TSTAMP_MS_M TSTAMP_MS_L  SENSOR_ID      VALUE_H                 VALUE_L         None            None            None
+# SENSOR_ALTITUDE: TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    ALTITUDE_H              ALTITUDE_HM     ALTITUDE_LM     ALTITUDE_L      None
 # LEDS_ON:         None        None         None           None                    None            None            None            None
 # LEDS_OFF:        None        None         None           None                    None            None            None            None
 # FILL_LVL:        TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    FILL_LEVEL              DIRECTION       None            None            None
@@ -21,14 +24,18 @@ msg_type_hex = {
     "GENERAL_CMD" : 0x060,
     "VENT_VALVE_CMD" : 0x0C0,
     "INJ_VALVE_CMD" : 0x120,
+    "ALT_ARM_CMD" : 0x140,
 
     "DEBUG_MSG" : 0x180,
     "DEBUG_PRINTF" : 0x1E0,
 
+    "ALT_ARM_STATUS" : 0x440,
     "VENT_VALVE_STATUS" : 0x460,
     "INJ_VALVE_STATUS" : 0x4C0,
     "GENERAL_BOARD_STATUS" : 0x520,
+    "RECOVERY_STATUS" : 0x540,
 
+    "SENSOR_ALTITUDE" : 0x560,
     "SENSOR_ACC" : 0x580,
     "SENSOR_GYRO" : 0x5E0,
     "SENSOR_MAG" : 0x640,
@@ -65,6 +72,8 @@ board_id_hex = {
     "GPS_SPARE" : 0x0E,
     "FILL": 0x0F,
     "FILL_SPARE": 0x010,
+    "ARMING" : 0x11,
+    "ARMING_SPARE" : 0X12
 }
 board_id_str = {v: k for k, v in board_id_hex.items()}
 
@@ -80,6 +89,14 @@ valve_states_hex = {
     "VALVE_ILLEGAL" : 3
 }
 valve_states_str = {v: k for k, v in valve_states_hex.items()}
+
+# ARM_CMD/STATUS STATES
+arm_states_hex = {
+    "DISARMED" : 0,
+    "ARMED" : 1
+}
+arm_states_str = dict([[v,k] for k,v in arm_states_hex.items()])
+
 
 # BOARD GENERAL STATUS ERROR CODES
 # ERROR CODE (byte 3)         (byte4)             (byte 5)            (byte 6)            (byte 7)
@@ -121,7 +138,14 @@ sensor_id_hex = {
     "SENSOR_PRESSURE_OX" : 3,
     "SENSOR_PRESSURE_CC" : 4,
     "SENSOR_VENT_BATT" : 5,
-    "SENSOR_INJ_BATT" : 6
+    "SENSOR_INJ_BATT" : 6,
+    "SENSOR_ARM_BATT_1" : 7,
+    "SENSOR_ARM_BATT_2" : 8,
+    "SENSOR_BATT_CURR" : 9,
+    "SENSOR_BUS_CURR" : 10,
+    "SENSOR_VELOCITY" : 11,
+    "SENSOR_MAG_1" : 12,
+    "SENSOR_MAG_2" : 13
 }
 sensor_id_str = {v: k for k, v in sensor_id_hex.items()}
 
