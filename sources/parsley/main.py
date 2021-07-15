7b4f4a35e8b1d9857e185c409287728cde054083
Ben Levy <ben@hmjbl.com>
2021-07-15 09:52:30 -0400

    Source: format main.py (#2)

--- sources/parsley/main.py
+++ sources/parsley/main.py
@@ -5,8 +5,9 @@ import serial
 from serial import Serial
 from omnibus import Sender
 
-disp = serial.Serial('COM5',9600)
+disp = serial.Serial('COM5', 9600)
 sender = Sender("CAN/Parsley")
+
 
 def parse_gen_cmd(msg_data):
     timestamp = msg_data[0] << 16 | msg_data[1] << 8 | msg_data[2]
@@ -23,23 +24,26 @@ def parse_valve_cmd(msg_data):
     parsed_str = ['t=', str(timestamp) + 'ms', valve_state]
     return parsed_str
 
+
 def parse_valve_status(msg_data):
     timestamp = msg_data[0] << 16 | msg_data[1] << 8 | msg_data[2]
     valve_state = mt.valve_states_str[msg_data[3]]
     req_valve_state = mt.valve_states_str[msg_data[4]]
 
-    parsed_str = ['t=', str(timestamp) + 'ms', 'REQ: ' + req_valve_state, \
-            'ACTUAL: ' + valve_state]
+    parsed_str = ['t=', str(timestamp) + 'ms', 'REQ: ' + req_valve_state,
+                  'ACTUAL: ' + valve_state]
     return parsed_str
+
 
 def parse_arm_cmd(msg_data):
     timestamp = msg_data[0] << 16 | msg_data[1] << 8 | msg_data[2]
     arm_state = mt.arm_states_str[msg_data[3] >> 4]
     alt_number = msg_data[3] & 0x0F
 
-    parsed_str = ['t=', str(timestamp) + 'ms', 'ALTIMETER: ' + str(alt_number), \
-            'COMAND: ' + arm_state]
+    parsed_str = ['t=', str(timestamp) + 'ms', 'ALTIMETER: ' + str(alt_number),
+                  'COMAND: ' + arm_state]
     return parsed_str
+
 
 def parse_arm_status(msg_data):
     timestamp = msg_data[0] << 16 | msg_data[1] << 8 | msg_data[2]
@@ -48,10 +52,11 @@ def parse_arm_status(msg_data):
     v_drogue = msg_data[4] << 8 | msg_data[5]
     v_main = msg_data[6] << 8 | msg_data[7]
 
-    parsed_str = ['t=', str(timestamp) + 'ms', 'ALTIMETER: ' + str(alt_number), \
-            'STATUS: ' + arm_state, 'V DROGUE: ' + str(v_drogue) + 'mV', \
-            'V MAIN: ' + str(v_main) + 'mV']
+    parsed_str = ['t=', str(timestamp) + 'ms', 'ALTIMETER: ' + str(alt_number),
+                  'STATUS: ' + arm_state, 'V DROGUE: ' + str(v_drogue) + 'mV',
+                  'V MAIN: ' + str(v_main) + 'mV']
     return parsed_str
+
 
 def parse_debug_msg(msg_data):
     timestamp = msg_data[0] << 16 | msg_data[1] << 8 | msg_data[2]
@@ -61,9 +66,11 @@ def parse_debug_msg(msg_data):
     parsed_str = ['t=', str(timestamp) + 'ms', 'LVL: ' + debug_level, 'LINE: ' + line_number]
     return parsed_str
 
+
 def parse_debug_printf(msg_data):
     ascii_str = [''.join(chr(e) for e in msg_data)]
     return ascii_str
+
 
 def parse_board_status(msg_data):
     timestamp = msg_data[0] << 16 | msg_data[1] << 8 | msg_data[2]
@@ -76,18 +83,18 @@ def parse_board_status(msg_data):
         parsed_str.append(str(current) + ' mA')
 
     elif board_stat == 'E_BUS_UNDER_VOLTAGE' \
-        or board_stat == 'E_BUS_OVER_VOLTAGE' \
-        or board_stat == 'E_BATT_UNDER_VOLTAGE' \
-        or board_stat == 'E_BATT_OVER_VOLTAGE':
+            or board_stat == 'E_BUS_OVER_VOLTAGE' \
+            or board_stat == 'E_BATT_UNDER_VOLTAGE' \
+            or board_stat == 'E_BATT_OVER_VOLTAGE':
         voltage = msg_data[4] << 8 | msg_data[5]
         parsed_str.append(str(voltage) + ' mV')
 
     elif board_stat == 'E_BOARD_FEARED_DEAD' \
-        or board_stat == 'E_MISSING_CRITICAL_BOARD':
+            or board_stat == 'E_MISSING_CRITICAL_BOARD':
         parsed_str.append(mt.board_id_str[msg_data[4]])
 
     elif board_stat == 'E_NO_CAN_TRAFFIC' \
-        or board_stat == 'E_RADIO_SIGNAL_LOST':
+            or board_stat == 'E_RADIO_SIGNAL_LOST':
         parsed_str.append(str((msg_data[4] << 8 | msg_data[5])) + 'ms')
 
     elif board_stat == 'E_SENSOR':
@@ -113,8 +120,8 @@ def parse_sensor_altitude(msg_data):
     timestamp = msg_data[0] << 16 | msg_data[1] << 8 | msg_data[2]
     altitude = int(msg_data[3] << 24 | msg_data[4] << 16 | msg_data[5] << 8 | msg_data[6])
 
-    if altitude & 0x80000000:       #check if the value is negative
-        altitude -= 0x0100000000    #if it is negative subtract what we need to undo 2's complement
+    if altitude & 0x80000000:  # check if the value is negative
+        altitude -= 0x0100000000  # if it is negative subtract what we need to undo 2's complement
 
     parsed_str = ['t=', str(timestamp) + 'ms', 'ALTITUDE: ' + str(altitude) + 'ft']
 
@@ -126,11 +133,11 @@ def parse_gps_timestamp(msg_data):
     utc_hours = msg_data[3]
     utc_mins = msg_data[4]
     utc_secs = msg_data[5]
-    utc_dsecs= msg_data[6]
-    parsed_str = ['t=', str(timestamp) + 'ms', \
-            str(utc_hours) + 'hrs',  \
-            str(utc_mins) + 'mins',  \
-            str(utc_secs) + '.' + str(utc_dsecs) + 's']
+    utc_dsecs = msg_data[6]
+    parsed_str = ['t=', str(timestamp) + 'ms',
+                  str(utc_hours) + 'hrs',
+                  str(utc_mins) + 'mins',
+                  str(utc_secs) + '.' + str(utc_dsecs) + 's']
     return parsed_str
 
 
@@ -141,8 +148,8 @@ def parse_gps_latitude(msg_data):
     dminutes = msg_data[5] << 8 | msg_data[6]
     direction = chr(msg_data[7])
 
-    parsed_str = ['t=', str(timestamp) + 'ms', str(degrees) + 'deg', str(minutes) + '.' \
-          + str(dminutes) + 'mins', direction]
+    parsed_str = ['t=', str(timestamp) + 'ms', str(degrees) + 'deg', str(minutes) + '.'
+                  + str(dminutes) + 'mins', direction]
     return parsed_str
 
 
@@ -152,8 +159,8 @@ def parse_gps_longitude(msg_data):
     minutes = msg_data[4]
     dminutes = msg_data[5] << 8 | msg_data[6]
     direction = chr(msg_data[7])
-    parsed_str = ['t=', str(timestamp) + 'ms', str(degrees) + 'deg', str(minutes) + '.' \
-          + str(dminutes) + 'mins', direction]
+    parsed_str = ['t=', str(timestamp) + 'ms', str(degrees) + 'deg', str(minutes) + '.'
+                  + str(dminutes) + 'mins', direction]
     return parsed_str
 
 
@@ -178,7 +185,8 @@ def parse_fill_lvl(msg_data):
     timestamp = msg_data[0] << 16 | msg_data[1] << 8 | msg_data[2]
     fill_lvl = msg_data[3]
     direction = mt.fill_direction_str[msg_data[4]]
-    parsed_str = ['t=', str(timestamp) + 'ms', 'LEVEL=' + str(fill_lvl), 'DIRECTION=' + str(direction)]
+    parsed_str = ['t=', str(timestamp) + 'ms', 'LEVEL=' + str(fill_lvl),
+                  'DIRECTION=' + str(direction)]
     return parsed_str
 
 
@@ -239,8 +247,8 @@ def parse_line(args, line):
         parsed_data.extend(parse_board_status(msg_data))
 
     elif msg_type == 'SENSOR_ACC' \
-        or msg_type == 'SENSOR_GYRO' \
-        or msg_type == 'SENSOR_MAG':
+            or msg_type == 'SENSOR_GYRO' \
+            or msg_type == 'SENSOR_MAG':
         # not supported yet
         print(header + ' ' + line)
 
@@ -281,33 +289,30 @@ def parse_line(args, line):
 
     for data in parsed_data[start_data:]:
         output = output + '{:<20}'.format(data)
-    
+
     sender.send(output)
     print(output)
-    
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--format', help='Options: logger, usb. Parse input in RocketCAN Logger or USB format')
+    parser.add_argument(
+        '--format', help='Options: logger, usb. Parse input in RocketCAN Logger or USB format')
     args = parser.parse_args()
-    
-    
+
     while True:
-        
-        
+
         try:
-            
+
             line = disp.readline()
             line = line.strip(b'\r\n').decode('utf-8')
-            
-            
+
         except KeyboardInterrupt:
             break
 
         # stop when there are no lines left
         if not line:
-            
+
             break
 
         # treat repeated messages in the same way as USB
@@ -315,16 +320,14 @@ if __name__ == '__main__':
             print('.')
             continue
 
-         #Print the header message cause it looks cool
+         # Print the header message cause it looks cool
         if 'WATERLOO ROCKETRY CAN LOGGER' in line:
             print(line)
             continue
 
         try:
             parse_line(args, line)
-            
+
         except KeyError:
             print('Unable to parse message: ' + line)
             continue
-        
-        
--- sources/parsley/message_types.py
+++ sources/parsley/message_types.py
@@ -1,7 +1,7 @@
 # This file tracks message_types.h in canlib. I just copy and pasted cause I want a quick
 # and dirty CAN message decoder and not much more.
 #                  byte 0      byte 1       byte 2         byte 3                  byte 4          byte 5          byte 6          byte 7
- # GENERAL CMD:    TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    COMMAND_TYPE            None            None            None            None
+# GENERAL CMD:    TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    COMMAND_TYPE            None            None            None            None
 # VENT_VALVE_CMD:  TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    VENT_VALVE_STATE        None            None            None            None
 # INJ_VALVE_CMD:   TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    INJ_VALVE_STATE         None            None            None            None
 # ALT_ARM_CMD:     TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    ALT_ARM_STATE & #       None            None            None            None
@@ -21,131 +21,131 @@
 # FILL_LVL:        TSTAMP_MS_H TSTAMP_MS_M  TSTAMP_MS_L    FILL_LEVEL              DIRECTION       None            None            None
 
 msg_type_hex = {
-    "GENERAL_CMD" : 0x060,
-    "VENT_VALVE_CMD" : 0x0C0,
-    "INJ_VALVE_CMD" : 0x120,
-    "ALT_ARM_CMD" : 0x140,
+    "GENERAL_CMD": 0x060,
+    "VENT_VALVE_CMD": 0x0C0,
+    "INJ_VALVE_CMD": 0x120,
+    "ALT_ARM_CMD": 0x140,
 
-    "DEBUG_MSG" : 0x180,
-    "DEBUG_PRINTF" : 0x1E0,
+    "DEBUG_MSG": 0x180,
+    "DEBUG_PRINTF": 0x1E0,
 
-    "ALT_ARM_STATUS" : 0x440,
-    "VENT_VALVE_STATUS" : 0x460,
-    "INJ_VALVE_STATUS" : 0x4C0,
-    "GENERAL_BOARD_STATUS" : 0x520,
-    "RECOVERY_STATUS" : 0x540,
+    "ALT_ARM_STATUS": 0x440,
+    "VENT_VALVE_STATUS": 0x460,
+    "INJ_VALVE_STATUS": 0x4C0,
+    "GENERAL_BOARD_STATUS": 0x520,
+    "RECOVERY_STATUS": 0x540,
 
-    "SENSOR_ALTITUDE" : 0x560,
-    "SENSOR_ACC" : 0x580,
-    "SENSOR_GYRO" : 0x5E0,
-    "SENSOR_MAG" : 0x640,
-    "SENSOR_ANALOG" : 0x6A0,
+    "SENSOR_ALTITUDE": 0x560,
+    "SENSOR_ACC": 0x580,
+    "SENSOR_GYRO": 0x5E0,
+    "SENSOR_MAG": 0x640,
+    "SENSOR_ANALOG": 0x6A0,
 
-    "GPS_TIMESTAMP" : 0x6C0,
-    "GPS_LATITUDE" : 0x6E0,
-    "GPS_LONGITUDE" : 0x700,
-    "GPS_ALTITUDE" : 0x720,
-    "GPS_INFO" : 0x740,
+    "GPS_TIMESTAMP": 0x6C0,
+    "GPS_LATITUDE": 0x6E0,
+    "GPS_LONGITUDE": 0x700,
+    "GPS_ALTITUDE": 0x720,
+    "GPS_INFO": 0x740,
 
     "FILL_LVL": 0x780,
 
-    "LEDS_ON" : 0x7E0,
-    "LEDS_OFF" : 0x7C0,
+    "LEDS_ON": 0x7E0,
+    "LEDS_OFF": 0x7C0,
 }
 msg_type_str = {v: k for k, v in msg_type_hex.items()}
 
 board_id_hex = {
-    "DUMMY" : 0x00,
-    "INJECTOR" : 0x01,
-    "INJECTOR_SPARE" : 0x02,
-    "LOGGER" : 0x03,
-    "LOGGER_SPARE" : 0x04,
-    "RADIO" : 0x05,
-    "RADIO_SPARE" : 0x06,
-    "SENSOR" : 0x07,
-    "SENSOR_SPARE" : 0x08,
-    "USB" : 0x09,
-    "USB_SPARE" : 0x0A,
-    "VENT" : 0x0B,
-    "VENT_SPARE" : 0x0C,
-    "GPS" : 0x0D,
-    "GPS_SPARE" : 0x0E,
+    "DUMMY": 0x00,
+    "INJECTOR": 0x01,
+    "INJECTOR_SPARE": 0x02,
+    "LOGGER": 0x03,
+    "LOGGER_SPARE": 0x04,
+    "RADIO": 0x05,
+    "RADIO_SPARE": 0x06,
+    "SENSOR": 0x07,
+    "SENSOR_SPARE": 0x08,
+    "USB": 0x09,
+    "USB_SPARE": 0x0A,
+    "VENT": 0x0B,
+    "VENT_SPARE": 0x0C,
+    "GPS": 0x0D,
+    "GPS_SPARE": 0x0E,
     "FILL": 0x0F,
     "FILL_SPARE": 0x010,
-    "ARMING" : 0x11,
-    "ARMING_SPARE" : 0X12
+    "ARMING": 0x11,
+    "ARMING_SPARE": 0X12
 }
 board_id_str = {v: k for k, v in board_id_hex.items()}
 
 # GEN_CMD
-gen_cmd_hex = { "BUS_DOWN_WARNING" : 0 }
+gen_cmd_hex = {"BUS_DOWN_WARNING": 0}
 gen_cmd_str = {v: k for k, v in gen_cmd_hex.items()}
 
 # VALVE_CMD/STATUS STATES
 valve_states_hex = {
-    "VALVE_OPEN" :  0,
-    "VALVE_CLOSED" : 1,
-    "VALVE_UNK" : 2,
-    "VALVE_ILLEGAL" : 3
+    "VALVE_OPEN":  0,
+    "VALVE_CLOSED": 1,
+    "VALVE_UNK": 2,
+    "VALVE_ILLEGAL": 3
 }
 valve_states_str = {v: k for k, v in valve_states_hex.items()}
 
 # ARM_CMD/STATUS STATES
 arm_states_hex = {
-    "DISARMED" : 0,
-    "ARMED" : 1
+    "DISARMED": 0,
+    "ARMED": 1
 }
-arm_states_str = dict([[v,k] for k,v in arm_states_hex.items()])
+arm_states_str = dict([[v, k] for k, v in arm_states_hex.items()])
 
 
 # BOARD GENERAL STATUS ERROR CODES
 # ERROR CODE (byte 3)         (byte4)             (byte 5)            (byte 6)            (byte 7)
 board_stat_hex = {
-    "E_NOMINAL" : 0,                   # x                x                   x                   x
+    "E_NOMINAL": 0,                   # x                x                   x                   x
 
-    "E_BUS_OVER_CURRENT" : 1,          # mA_high          mA_low              x                   x
-    "E_BUS_UNDER_VOLTAGE" : 2,         # mV_high          mV_low              x                   x
-    "E_BUS_OVER_VOLTAGE" : 3,          # mV_high          mV_low              x                   x
+    "E_BUS_OVER_CURRENT": 1,          # mA_high          mA_low              x                   x
+    "E_BUS_UNDER_VOLTAGE": 2,         # mV_high          mV_low              x                   x
+    "E_BUS_OVER_VOLTAGE": 3,          # mV_high          mV_low              x                   x
 
-    "E_BATT_UNDER_VOLTAGE" : 4,        # mV_high          mV_low              x                   x
-    "E_BATT_OVER_VOLTAGE" : 5,         # mV_high          mV_low              x                   x
+    "E_BATT_UNDER_VOLTAGE": 4,        # mV_high          mV_low              x                   x
+    "E_BATT_OVER_VOLTAGE": 5,         # mV_high          mV_low              x                   x
 
-    "E_BOARD_FEARED_DEAD" : 6,         # board_id         x                   x                   x
-    "E_NO_CAN_TRAFFIC" : 7,            # time_s_high      time_s_low          x                   x
-    "E_MISSING_CRITICAL_BOARD" : 8,    # board_id         x                   x                   x
-    "E_RADIO_SIGNAL_LOST" : 9,         # time_s_high      time_s_low          x                   x
+    "E_BOARD_FEARED_DEAD": 6,         # board_id         x                   x                   x
+    "E_NO_CAN_TRAFFIC": 7,            # time_s_high      time_s_low          x                   x
+    "E_MISSING_CRITICAL_BOARD": 8,    # board_id         x                   x                   x
+    "E_RADIO_SIGNAL_LOST": 9,         # time_s_high      time_s_low          x                   x
 
-    "E_VALVE_STATE" : 10,              # expected_state   valve_state         x                   x
-    "E_CANNOT_INIT_DACS" : 11,         # x                x                   x                   x
+    "E_VALVE_STATE": 10,              # expected_state   valve_state         x                   x
+    "E_CANNOT_INIT_DACS": 11,         # x                x                   x                   x
     "E_VENT_POT_RANGE": 12,            # lim_upper (mV)   lim_lower (mV)      pot (mV)            x
 
-    "E_LOGGING" : 13,                  # x                x                   x                   x
-    "E_GPS" : 14,                      # x                x                   x                   x
-    "E_SENSOR" : 15,                   # sensor_id        x                   x                   x
+    "E_LOGGING": 13,                  # x                x                   x                   x
+    "E_GPS": 14,                      # x                x                   x                   x
+    "E_SENSOR": 15,                   # sensor_id        x                   x                   x
 
-    "E_ILLEGAL_CAN_MSG" : 16,          # x                x                   x                   x
-    "E_SEGFAULT" : 17,                 # x                x                   x                   x
-    "E_UNHANDLED_INTERRUPT" : 18,      # x                x                   x                   x
-    "E_CODING_FUCKUP" : 19             # x                x                   x                   x
+    "E_ILLEGAL_CAN_MSG": 16,          # x                x                   x                   x
+    "E_SEGFAULT": 17,                 # x                x                   x                   x
+    "E_UNHANDLED_INTERRUPT": 18,      # x                x                   x                   x
+    "E_CODING_FUCKUP": 19             # x                x                   x                   x
 }
 board_stat_str = {v: k for k, v in board_stat_hex.items()}
 
 # SENSOR_ID
 sensor_id_hex = {
-    "SENSOR_IMU1" : 0,
-    "SENSOR_IMU2" : 1,
-    "SENSOR_BARO" : 2,
-    "SENSOR_PRESSURE_OX" : 3,
-    "SENSOR_PRESSURE_CC" : 4,
-    "SENSOR_VENT_BATT" : 5,
-    "SENSOR_INJ_BATT" : 6,
-    "SENSOR_ARM_BATT_1" : 7,
-    "SENSOR_ARM_BATT_2" : 8,
-    "SENSOR_BATT_CURR" : 9,
-    "SENSOR_BUS_CURR" : 10,
-    "SENSOR_VELOCITY" : 11,
-    "SENSOR_MAG_1" : 12,
-    "SENSOR_MAG_2" : 13
+    "SENSOR_IMU1": 0,
+    "SENSOR_IMU2": 1,
+    "SENSOR_BARO": 2,
+    "SENSOR_PRESSURE_OX": 3,
+    "SENSOR_PRESSURE_CC": 4,
+    "SENSOR_VENT_BATT": 5,
+    "SENSOR_INJ_BATT": 6,
+    "SENSOR_ARM_BATT_1": 7,
+    "SENSOR_ARM_BATT_2": 8,
+    "SENSOR_BATT_CURR": 9,
+    "SENSOR_BUS_CURR": 10,
+    "SENSOR_VELOCITY": 11,
+    "SENSOR_MAG_1": 12,
+    "SENSOR_MAG_2": 13
 }
 sensor_id_str = {v: k for k, v in sensor_id_hex.items()}
 
