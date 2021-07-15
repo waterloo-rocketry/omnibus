5821e30f0b25e03fb742d19fa2ecdd8f9c38c6f3
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2021-07-14 20:19:30 -0400

    Break out plots into their own class, fix circular import issues by ignoring them

--- sinks/plot/config.py
+++ sinks/plot/config.py
@@ -4,8 +4,9 @@ from series import Series
 GRAPH_DURATION = 30 # size of x axis in seconds
 GRAPH_RESOLUTION = 10 # data points per second
 
-# DAQ_SENSORS = ["P5 - Pneumatics", "P4 - Ox Fill", "P3 - Ox Tank", "P2 - CC",
-#                "Thrust", "SP1 - Nozzle", "FAST", "Omega S-Type", "T8 - Tank Heating"]
-DAQ_SENSORS = [f"Fake{i}" for i in range(8)]
-for sensor in DAQ_SENSORS:
-  Series(sensor, 50, DAQParser("DAQ", sensor), GRAPH_RESOLUTION, GRAPH_DURATION)
+def setup():
+  # DAQ_SENSORS = ["P5 - Pneumatics", "P4 - Ox Fill", "P3 - Ox Tank", "P2 - CC",
+  #                "Thrust", "SP1 - Nozzle", "FAST", "Omega S-Type", "T8 - Tank Heating"]
+  DAQ_SENSORS = [f"Fake{i}" for i in range(8)]
+  for sensor in DAQ_SENSORS:
+    Series(sensor, 50, DAQParser("DAQ", sensor))
--- sinks/plot/plot.py
+++ sinks/plot/plot.py
@@ -4,29 +4,22 @@ import numpy as np
 from pyqtgraph.Qt import QtCore
 import pyqtgraph as pg
 
-from omnibus import Receiver
 from series import Series
 
-# pg.setConfigOptions(antialias=True)
-
 class Plotter:
-    def __init__(self):
-        self.receiver = Receiver("")
+    def __init__(self, receiver):
+        self.receiver = receiver
 
-        SENSOR_COUNT = len(Series.series)
+        columns = int(np.ceil(np.sqrt(len(Series.series))))
 
-        columns = int(np.ceil(np.sqrt(SENSOR_COUNT)))
-        rows = int(np.ceil(SENSOR_COUNT / columns))
-
-        self.win = pg.GraphicsLayoutWidget(show=True, title="Omnibus Plotter", size=(rows, columns))
+        self.win = pg.GraphicsLayoutWidget(show=True, title="Omnibus Plotter")
         self.win.resize(1000, 600)
 
         self.plots = []
-        self.curves = []
         for i, series in enumerate(Series.series):
-            self.plots.append(self.win.addPlot(row=i // columns, col = i % columns,
-                title=series.name, left="Data", bottom="Seconds"))
-            self.curves.append(self.plots[-1].plot(series.times, series.points, pen='y'))
+            plot = Plot(series)
+            self.plots.append(plot)
+            self.win.addItem(plot.plot, i // columns, i % columns)
 
         self.rates = []
 
@@ -36,15 +29,22 @@ class Plotter:
             self.rates.pop(0)
         print(f"\rFPS: {len(self.rates)/(time.time() - self.rates[0]): >4.0f}  ", end='')
 
-        changed = False
         while msg := self.receiver.recv_message(0):
-            changed |= Series.parse(msg.channel, msg.payload)
-        if changed:
-            for i, series in enumerate(Series.series):
-                self.curves[i].setData(series.times, series.points)
+            Series.parse(msg.channel, msg.payload)
 
     def exec(self):
         timer = QtCore.QTimer()
         timer.timeout.connect(self.update)
         timer.start(16)  # Capped at 60 Fps, 1000 ms / 16 ~= 60
         pg.mkQApp().exec_()
+
+class Plot:
+    def __init__(self, series):
+        self.series = series
+        self.series.registerUpdate(self.update)
+
+        self.plot = pg.PlotItem(title=series.name, left="Data", bottom="Seconds")
+        self.curve = self.plot.plot(self.series.times, self.series.points, pen='y')
+
+    def update(self):
+        self.curve.setData(self.series.times, self.series.points)
