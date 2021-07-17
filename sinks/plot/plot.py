bd4ba780c1898b48178bda8d617fc33f3e42de76
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2021-07-17 00:46:20 -0400

    Move omnibus out of plotter, comment

--- sinks/plot/plot.py
+++ sinks/plot/plot.py
@@ -1,50 +1,68 @@
+import signal
 import time
 
 import numpy as np
 from pyqtgraph.Qt import QtCore
 import pyqtgraph as pg
 
-from series import Series
-
 class Plotter:
-    def __init__(self, receiver):
-        self.receiver = receiver
+    """
+    Displays a grid of plots in a window
+    """
+    def __init__(self, series, callback):
+        self.series = series
+        self.callback = callback # called every frame to get new data
 
-        columns = int(np.ceil(np.sqrt(len(Series.series))))
+        # try for a square layout
+        columns = int(np.ceil(np.sqrt(len(self.series))))
 
+        # window that lays out plots in a grid
         self.win = pg.GraphicsLayoutWidget(show=True, title="Omnibus Plotter")
         self.win.resize(1000, 600)
 
         self.plots = []
-        for i, series in enumerate(Series.series):
-            plot = Plot(series)
+        for i, s in enumerate(self.series):
+            plot = Plot(s)
             self.plots.append(plot)
+            # add the plot to a specific coordinate in the window
             self.win.addItem(plot.plot, i // columns, i % columns)
 
         self.rates = []
 
+    # called every frame
     def update(self):
         self.rates.append(time.time())
         if len(self.rates) > 50:
             self.rates.pop(0)
         print(f"\rFPS: {len(self.rates)/(time.time() - self.rates[0]): >4.0f}  ", end='')
 
-        while msg := self.receiver.recv_message(0):
-            Series.parse(msg.channel, msg.payload)
+        self.callback()
 
     def exec(self):
         timer = QtCore.QTimer()
         timer.timeout.connect(self.update)
         timer.start(16)  # Capped at 60 Fps, 1000 ms / 16 ~= 60
+
+        # make ctrl+c close the window
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
+
         pg.mkQApp().exec_()
 
 class Plot:
+    """
+    Manages displaying and updating a single plot.
+    """
     def __init__(self, series):
         self.series = series
+        # update when data is added to the series
         self.series.registerUpdate(self.update)
 
-        self.plot = pg.PlotItem(title=series.name, left="Data", bottom="Seconds")
+        self.plot = pg.PlotItem(title=self.series.name, left="Data", bottom="Seconds")
         self.curve = self.plot.plot(self.series.times, self.series.points, pen='y')
 
     def update(self):
+        # update the displayed data
         self.curve.setData(self.series.times, self.series.points)
+
+        # current value readout in the title
+        self.plot.setTitle(f"{self.series.name} ({self.series.points[-1]:.1f})")
