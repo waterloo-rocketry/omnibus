d372d9e75ab75a3cc466f45edfca42d302fff66a
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2021-07-23 17:55:31 -0400

    Pack logs using msgpack

--- sinks/globallog/main.py
+++ sinks/globallog/main.py
@@ -2,6 +2,8 @@
 
 from datetime import datetime
 
+import msgpack
+
 from omnibus import Receiver
 
 # Will log all messages passing through bus
@@ -12,7 +14,7 @@ CURTIME = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
 fname = CURTIME + ".log"
 receiver = Receiver(CHANNEL)
 # Creates new file
-with open(fname, "w") as f:
+with open(fname, "wb") as f:
     while True:
         msg = receiver.recv_message()
-        f.write(f"{msg.timestamp} :: {msg.channel} :: {msg.payload}\n")
+        f.write(msgpack.packb([msg.channel, msg.timestamp, msg.payload]))
