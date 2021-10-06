db810d8ad060b3153f880192fe44ae6726361d5d
Robert Cai <xiaoyucai2015@gmail.com>
2021-10-06 18:34:16 -0400

    Revert "Prototyping labels and text items"
    This reverts commit 0561b71abda062fbfa036fba6fd68ee2f1e79958.

--- sinks/plot/plot.py
+++ sinks/plot/plot.py
@@ -5,8 +5,6 @@ import numpy as np
 from pyqtgraph.Qt import QtCore
 import pyqtgraph as pg
 
-from pyqtgraph.graphicsItems.LabelItem import LabelItem
-from pyqtgraph.graphicsItems.TextItem import TextItem
 
 class Plotter:
     """
@@ -30,10 +28,7 @@ class Plotter:
             self.plots.append(plot)
             # add the plot to a specific coordinate in the window
             self.win.addItem(plot.plot, i // columns, i % columns)
-        #self.fps_label = LabelItem(str(self.fps))
-        self.fps = 0
-        self.fps_label = LabelItem("FPS = " + str(self.fps))
-        self.win.addItem(self.fps_label)
+
         self.rates = []
 
     # called every frame
@@ -41,12 +36,7 @@ class Plotter:
         self.rates.append(time.time())
         if len(self.rates) > 50:
             self.rates.pop(0)
-        if (time.time() - self.rates[0] > 0):
-            #self.win.removeItem(self.fps_label)
-            self.fps = len(self.rates)/(time.time() - self.rates[0])
-            self.fps_label = LabelItem("FPS = " + str(self.fps))
-            #self.fps_label = LabelItem(str(self.fps))
-            print(f"\rFPS: {self.fps: >4.0f}  ", end='')
+        print(f"\rFPS: {len(self.rates)/(time.time() - self.rates[0]): >4.0f}  ", end='')
 
         self.callback()
 
@@ -73,8 +63,6 @@ class Plot:
 
         self.plot = pg.PlotItem(title=self.series.name, left="Data", bottom="Seconds")
         self.curve = self.plot.plot(self.series.times, self.series.points, pen='y')
-        self.label = pg.TextItem(text="This is a text", color=(0,0,0))
-        self.plot.addItem(self.label)
 
     def update(self):
         # update the displayed data
@@ -82,4 +70,3 @@ class Plot:
 
         # current value readout in the title
         self.plot.setTitle(f"{self.series.name} ({self.series.points[-1]:.1f})")
-        self.label.setText("Running Avg")
