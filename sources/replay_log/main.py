0a25fb007fa7f63316e68d0291435a7e2183dc33
Devin Leamy <devinleamy@gmail.com>
2021-10-15 22:47:30 -0400

    added argument parser for log_file selection, replay speed, and log display cap

--- sources/replay_log/main.py
+++ sources/replay_log/main.py
@@ -5,8 +5,20 @@ import time
 import os
 import msgpack
 from datetime import datetime
+import argparse
 
 LOGS = "../../sinks/globallog/"
+
+def parse_arguments():
+  parser = argparse.ArgumentParser(prog="PROG")
+  parser.add_argument('--log_file', '-l', default=None, 
+                      help="relative path to a log file (default: selection from prompt)")
+  parser.add_argument('--replay_speed', '-r', default=1, type=int, 
+                      help="replay speed of log (default: 1)")
+  parser.add_argument('--max_logs', '-m', default=10, type=int,
+                      help='number of logs files to display (default: 10)')
+  args = parser.parse_args()
+  return args
 
 def retreive_log_files():
   return [file for file in os.listdir(LOGS) if file.split(".")[-1] == "log"]
@@ -20,8 +32,7 @@ def get_datetime(filename):
   datetime_obj = datetime.strptime(f_datetime, "%Y_%m_%d-%I_%M_%S_%p")
   return datetime_obj
 
-# TODO: make max_log_files a commandline argument 
-def get_replay_log(max_log_files=10):
+def get_replay_log(max_logs):
   """
   Have the user select a log to replay. Defaults to
   requesting the most recent log.
@@ -30,7 +41,7 @@ def get_replay_log(max_log_files=10):
   # sort files by datetime, newest to oldest
   log_files = sorted(log_files, key=get_datetime)[::-1]
 
-  log_files = log_files[:max_log_files]
+  log_files = log_files[:max_logs]
 
   print(f"(R): Most recent")
   for option, log_file in enumerate(log_files):
@@ -70,7 +81,7 @@ def unpack_log(log_file):
   
   return logs
 
-def replay_log(log_file):
+def replay_log(log_file, replay_speed):
   """
   Replays the contents of a log_file.
   """
@@ -84,7 +95,7 @@ def replay_log(log_file):
     r_time = time.time()
     l_time = message.timestamp
 
-    delta_r_time = r_time - r_start_time 
+    delta_r_time = (r_time - r_start_time) * replay_speed
     delta_l_time = l_time - l_start_time 
 
     # wait for the real time to catch up to the delta time
@@ -98,5 +109,18 @@ def replay_log(log_file):
     sender.send_message(message)
 
 if __name__ == "__main__":
-  replay_log_file = get_replay_log()
-  replay_log(replay_log_file)
+  start_time = time.time()
+
+  args = parse_arguments()
+  MAX_LOGS = args.max_logs
+  REPLAY_SPEED = max(1, args.replay_speed)
+  LOG_FILE = args.log_file
+
+  print(args)
+
+  if LOG_FILE == None:
+    LOG_FILE = get_replay_log(MAX_LOGS)
+
+  replay_log(LOG_FILE, REPLAY_SPEED) 
+
+  print("--- %s seconds ---" % (time.time() - start_time))
