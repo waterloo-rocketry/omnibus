1e78e8448388ff8a33d8379ec74f0c3766b15f67
Devin Leamy <devinleamy@gmail.com>
2021-10-19 00:08:04 -0400

    autopep8..

--- sources/replay_log/test_replay_log.py
+++ sources/replay_log/test_replay_log.py
@@ -12,22 +12,23 @@ from omnibus import Sender
 TEST_LOG_IN = "./sources/replay_log/test_log_in.log"
 TEST_LOG_OUT = "./sources/replay_log/test_log_out.log"
 
+
 class TestReplayLog:
     @pytest.fixture
     def sender(self, mocker):
-       mocker.patch.object(
+        mocker.patch.object(
             Sender,
             'send_message',
             new=mock_send_message
-       ) 
-    
+        )
+
     @pytest.fixture
     def input_log(self):
-        # create input log file 
+        # create input log file
         with open(TEST_LOG_IN, "wb") as f:
             last_time = time.time()
             for i in range(50):
-                f.write(msgpack.packb([rand_str(), last_time, rand_str(100)])) 
+                f.write(msgpack.packb([rand_str(), last_time, rand_str(100)]))
                 # randomly add 0-0.01s of delay
                 last_time += random.randint(0, 10) * (1 / 1000)
 
@@ -36,7 +37,7 @@ class TestReplayLog:
         # destroy test files
         delete_file(TEST_LOG_IN)
         delete_file(TEST_LOG_OUT)
-        
+
     def test_replay_consistency(self, input_log, sender):
         ReplayLogSource.replay(TEST_LOG_IN, 1)
         assert logs_match(TEST_LOG_IN, TEST_LOG_OUT)
@@ -66,36 +67,41 @@ class TestReplayLog:
         ReplayLogSource.replay(TEST_LOG_IN, 0.25)
         t2 = time.time() - s2
 
-        # timing is not very percise, a quarter to speed should be at least half as fast 
+        # timing is not very percise, a quarter to speed should be at least half as fast
         assert t2 >= t1 * 2
-    
-   
+
+
 # replaces Sender.send_message
 def mock_send_message(self, message):
     mock_receive_message(message)
 
 # mocks the receipt of a message
+
+
 def mock_receive_message(msg):
     if msg == None:
         return
     with open(TEST_LOG_OUT, "ab") as f:
         f.write(msgpack.packb([msg.channel, msg.timestamp, msg.payload]))
 
+
 def logs_match(in_log, out_log):
-    with open(in_log, "rb") as f1: 
-        with open(out_log, "rb") as f2: 
+    with open(in_log, "rb") as f1:
+        with open(out_log, "rb") as f2:
             unpacker1 = msgpack.Unpacker(file_like=f1)
             unpacker2 = msgpack.Unpacker(file_like=f2)
-            while True: 
-                msg1 = ReplayLogSource.fetch_message(unpacker1) 
-                msg2 = ReplayLogSource.fetch_message(unpacker2) 
-                if msg1 != msg2: 
+            while True:
+                msg1 = ReplayLogSource.fetch_message(unpacker1)
+                msg2 = ReplayLogSource.fetch_message(unpacker2)
+                if msg1 != msg2:
                     return False
                 if msg1 == None and msg2 == None:
                     return True
 
+
 def rand_str(l=10):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=l))
+
 
 def delete_file(f):
     try:
