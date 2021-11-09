3225cef6f042c7f9228d0cb01ade083420dd0e4f
Devin Leamy <devinleamy@gmail.com>
2021-11-09 00:18:56 -0500

    _

--- sources/replay_log/main.py
+++ sources/replay_log/main.py
@@ -2,23 +2,23 @@ from pathlib import Path
 from datetime import datetime
 import argparse
 import os
+import sys
 
 import replay_log
 
 GLOBAL_LOGS = Path("../../sinks/globallog/")
 
-
-def valid_replay_speed(n):
-    n = float(n)
-    if n <= 0:
-        raise Exception("Invalid replay speed: replay_speed > 0")
-    return n
-
-
 def parse_arguments():
     """
     Parses command line arguments.
     """
+    def valid_replay_speed(n):
+        n = float(n)
+        if n <= 0:
+            print(f"Error: replay speed must be greater than zero.")
+            sys.exit(1)
+        return n
+
     parser = argparse.ArgumentParser(prog="PROG")
     parser.add_argument('--replay_speed', '-r', default=1, type=valid_replay_speed,
                         help="replay speed of log, must be greater than zero (default: 1)")
@@ -40,14 +40,14 @@ def get_replay_log(max_logs):
     log_files = log_files[:max_logs]
 
     if len(log_files) == 0:
-        raise Exception("no global log files to replay")
+        return None 
 
     print(f"(R): Most recent")
     for option, log_file in enumerate(log_files):
         print(f"({option}): {log_file}")
 
     while selection := input("Input the log to repeat (no brackets): "):
-        if selection == 'R' or (selection.isdigit() and 0 <= int(selection) <= len(log_files)):
+        if selection == 'R' or (selection.isdigit() and 0 <= int(selection) < len(log_files)):
             selection = 0 if selection == 'R' else int(selection)
             break
         else:
@@ -61,6 +61,10 @@ if __name__ == "__main__":
     max_logs = args.max_logs
     replay_speed = args.replay_speed
     log_file = args.log_file if args.log_file != None else get_replay_log(max_logs)
+
+    if log_file == None:
+        print("Error: unable to retrieve log file.")
+        sys.exit(1)
 
     print(f"replaying log: {log_file}")
     print(f"replay speed: {replay_speed}x")
