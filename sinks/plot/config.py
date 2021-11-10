e029d4d4c3a96d79c79614add465dd50117cc87a
Robert Cai <xiaoyucai2015@gmail.com>
2021-11-10 13:51:03 -0500

    revert config changes

--- sinks/plot/config.py
+++ sinks/plot/config.py
@@ -7,14 +7,19 @@ GRAPH_RESOLUTION = 10  # data points per second
 
 def setup():
     DAQ_SENSORS = [
-        "Fake0",
-        "Fake1",
-        "Fake2",
-        "Fake3",
-        "Fake4",
-        "Fake5",
-        "Fake6",
-        "Fake7",
+        "P5 (PT-5) - SRAD Vent Valve",
+        "P4 (PT-1) - Ox Fill",
+        "P3 (PT-2) - Ox Tank",
+        "P2 (PT-3) - CC", "Thrust",
+        "SP1 (PT-4) - Nozzle",
+        "FAST", "Omega S-Type",
+        "T8 - Tank Heating"
     ]
     for sensor in DAQ_SENSORS:
         Series(sensor, 50, DAQParser("DAQ", sensor))
+        Series("Fill Sensing", 1, FillSensingParser("CAN/Parsley"))
+        Series("T10 - Exit", 6, TemperatureParser("CAN/Parsley", 10))
+        Series("T20 - Diverging", 3, TemperatureParser("CAN/Parsley", 20))
+        Series("T21 - Throat", 3, TemperatureParser("CAN/Parsley", 21))
+        Series("T30 - Reservoir", 3, TemperatureParser("CAN/Parsley", 30))
+        Series("T31 - Vent", 3, TemperatureParser("CAN/Parsley", 31))
