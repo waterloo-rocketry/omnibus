e08e051c057639ec6ac47f84c8f96b4d60513d65
Robert Cai <xiaoyucai2015@gmail.com>
2021-11-27 18:56:36 -0500

    Added configurable running average duration

--- sinks/plot/config.py
+++ sinks/plot/config.py
@@ -3,6 +3,7 @@ from series import Series
 
 GRAPH_DURATION = 30  # size of x axis in seconds
 GRAPH_RESOLUTION = 10  # data points per second
+RUNNING_AVG_DURATION = 2 # last n seconds to be accounted for in running average, please don't set it larger than GRAPH_DURATION
 
 
 def setup():
--- sinks/plot/series.py
+++ sinks/plot/series.py
@@ -3,7 +3,6 @@ from numpy.core.fromnumeric import size
 
 import config
 
-
 class Series:
     """
     Stores and downsamples the datapoints of a single series
@@ -13,19 +12,19 @@ class Series:
     def __init__(self, name, rate, parser):
         self.name = name
         self.parser = parser
-
+        self.size = 0
         if rate > config.GRAPH_RESOLUTION:  # we need to downsample
-            size = config.GRAPH_RESOLUTION * config.GRAPH_DURATION
+            self.size = config.GRAPH_RESOLUTION * config.GRAPH_DURATION
             self.downsample = rate // config.GRAPH_RESOLUTION
         else:  # don't downsample
-            size = rate * config.GRAPH_DURATION
+            self.size = rate * config.GRAPH_DURATION
             self.downsample = 1
         self.downsampleCount = 0
-        self.times = np.zeros(size)
-        self.points = np.zeros(size)
+        self.times = np.zeros(self.size)
+        self.points = np.zeros(self.size)
         self.first = True
         self.sum = 0 # Sum of series
-        self.size = size # Size of series
+        self.avgSize = config.RUNNING_AVG_DURATION * min(rate, config.GRAPH_RESOLUTION) # "size" of running average 
         self.callback = None
 
         Series.series.append(self)
@@ -53,19 +52,19 @@ class Series:
             self.first = False
             self.times.fill(time)
             self.points.fill(point)
-            self.sum += point * self.size
+            self.sum += point * self.avgSize
         else:
-            self.sum -= self.points[0]
+            self.sum -= self.points[self.size - self.avgSize]
             self.sum += point
             self.points[:-1] = self.points[1:]
             self.points[-1] = point
             self.times[:-1] = self.times[1:]
             self.times[-1] = time
-
+        
         if self.callback:
             self.callback()
     def get_running_avg(self):
-        return self.sum / self.size
+        return self.sum / self.avgSize
     
     @staticmethod
     def parse(channel, payload):
