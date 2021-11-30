3d4f70fe806fd38a9afb8ca31006f58d135c5609
Addison <74084822+addison-ch@users.noreply.github.com>
2021-11-29 20:23:38 -0500

    updated code so that console window stays open on error

--- launcher.py
+++ launcher.py
@@ -3,36 +3,27 @@ from subprocess import Popen, CREATE_NEW_CONSOLE
 
 
 profiles = {
-    "test": ['python -m omnibus',
-             'python sinks/plot/main.py',
-             'python sources/fakeni/main.py'],
-    "texas": ['python sources/parsley/main.py',
-              'python sources/ni/main.py',
-              'python -m omnibus']
+    "test": ['"python -m omnibus"',
+             '"python sinks/plot/main.py"',
+             '"python sources/fakeni/main.py"'],
+    "texas": ['"python sources/parsley/main.py"',
+              '"python sources/ni/main.py"',
+              '"python -m omnibus"']
 }
 
 
-def run():  # runs processes in seperate consoles
-    for process in processes:
-        Popen(process, creationflags=CREATE_NEW_CONSOLE)
-
-
-def test():  # function that runs all processes in a single console, so error messages get displayed
-    for process in processes:
-        Popen(process)
-
-
 try:
-    if(sys.argv[1] == "_test"):
-        selection = sys.argv[2]
-        processes = profiles[selection]
-        test()
+    if(sys.argv[1] == "_wrap"):
+
+        p = Popen(sys.argv[2])
+        print('...')
+        x = input()
+
     else:
         selection = sys.argv[1]
         processes = profiles[selection]
-        run()
+        for script in processes:
+            Popen(f'python launcher.py _wrap {script}', creationflags=CREATE_NEW_CONSOLE)
 
 except (KeyError, IndexError):
     print('Please enter a single valid profile selection (ex. python launcher.py texas)')
-    print('''If you wish to launch the profile all in a singel console to check for errors, 
-    enter "_test" followed by the profile selection (ex. python launcher.py _test texas)''')
