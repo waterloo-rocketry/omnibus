ec1c668c2601895647960d8f25acf6fb50ed4fe5
Addison <74084822+addison-ch@users.noreply.github.com>
2021-12-01 20:32:10 -0500

    update to allow arguments to pass to child processes

--- launcher.py
+++ launcher.py
@@ -2,29 +2,38 @@ import sys
 from subprocess import Popen, CREATE_NEW_CONSOLE
 
 
-profiles = {  # Note: enter each command as a string enclosed with double quotations
-    "test": ['"python -m omnibus"',
-             '"python sinks/plot/main.py"',
-             '"python sources/fakeni/main.py"'],
-    "texas": ['"python sources/parsley/main.py"',
-              '"python sources/ni/main.py"',
-              '"python -m omnibus"']
+profiles = {
+    "test": ['python -m omnibus',
+             'python sinks/plot/main.py',
+             'python sources/fakeni/main.py'],
+    "texas": ['python sources/parsley/main.py $arg',
+              'python sources/ni/main.py',
+              'python -m omnibus']
     #   Add other profiles in here
+    #   If any commands require arguments to be passed in, write $arg in its place
+
 }
 
 
 try:
-    if(sys.argv[1] == "_wrap"):
+    if sys.argv[1] == "_wrap":
         p = Popen(sys.argv[2])
-        p.wait()
-        print('...')
-        x = input('Press enter to quit')
+        if p.wait() != 0:
+            close = input('Press enter to quit')
 
     else:
         selection = sys.argv[1]
         processes = profiles[selection]
-        for script in processes:
-            Popen(f'python launcher.py _wrap {script}', creationflags=CREATE_NEW_CONSOLE)
+        args = sys.argv[2:]
+
+        for process in processes:
+            if '$arg' in process:
+                command = process.replace('$arg', args[0])
+                args.pop(0)
+            else:
+                command = process
+            Popen(f'python launcher.py _wrap "{command}"', creationflags=CREATE_NEW_CONSOLE)
 
 except (KeyError, IndexError):
     print('Please enter a single valid profile selection (ex. python launcher.py texas)')
+    print('Ensure that you have entered the correct amount of args required for each script in the right order')
