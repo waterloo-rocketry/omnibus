1182c23fc6ebf4a3f47748b7df488db53ecd71ef
Robert Cai <xiaoyucai2015@gmail.com>
2022-03-20 02:28:52 -0400

    Fix TC test rounding

--- omnibus/util/tick_counter_test.py
+++ omnibus/util/tick_counter_test.py
@@ -11,7 +11,7 @@ class TestTickCounter():
         t.tick()
         time.sleep(1)
         t.tick()
-        assert(int(t.tick_rate()) == 1)
+        assert(round(t.tick_rate()) == 1)
         assert(t.tick_count() == 2)
 
     @pytest.fixture()
@@ -20,5 +20,5 @@ class TestTickCounter():
         for _ in range(5):
             t.tick()
             time.sleep(0.2)
-        assert(int(t.tick_rate()*5) == 1)
+        assert(round(t.tick_rate()) == 5)
         assert(t.tick_count() == 5)
