f37259b026a4429be4241afa90018552c39d3014
Robert Cai <xiaoyucai2015@gmail.com>
2022-03-27 17:44:09 -0400

    Reduced test length

--- omnibus/tick_counter_test.py
+++ omnibus/tick_counter_test.py
@@ -8,15 +8,15 @@ class TestTickCounter():
     def test_instantaneous(self):
         t = TickCounter(2)
         t.tick()
-        time.sleep(1)
+        time.sleep(0.1)
         t.tick()
-        assert(round(t.tick_rate()) == 1)
+        assert(round(t.tick_rate()) == 10)
         assert(t.tick_count() == 2)
 
     def test_running_avg(self):
         t = TickCounter(5)
         for _ in range(5):
             t.tick()
-            time.sleep(0.2)
-        assert(round(t.tick_rate()) == 5)
+            time.sleep(0.1)
+        assert(round(t.tick_rate()) == 10)
         assert(t.tick_count() == 5)
