aae82fe1cf84dfc663cd1f7d5b718d4cba41ccf2
BluCode <22122986+BluCodeGH@users.noreply.github.com>
2022-05-10 18:04:18 -0400

    Update sensor calibration (#49)
    Co-authored-by: KavinSatheeskumar <kavinsathees@gmail.com>
    Co-authored-by: Team Laptop <waterloorocketry@gmail.com>
    Co-authored-by: KavinSatheeskumar <kavinsatheesk@gmail.com>

--- sources/ni/config.py
+++ sources/ni/config.py
@@ -1,44 +1,54 @@
 from calibration import Sensor, Connection, LinearCalibration, ThermistorCalibration
 
-RATE = 10000  # Analog data sample rate
-READ_BULK = 200  # Number of samples to read at once for better performance
-
-CC = True
+RATE = 1000  # Analog data sample rate
+READ_BULK = 20  # Number of samples to read at once for better performance
 
 
 def setup():
-    Sensor("Omega S-Type", "ai3", 0.2, Connection.DIFFERENTIAL,
-           LinearCalibration(6637, -4.3, "lbs"))  # Roughly calibrated 2/7/2021
-    # Sensor("Honeywell S-Type", "", 0.2, Connection.DIFFERENTIAL, LinearCalibration(18.3*61.2, -0.59, "lbs")) # RECALIBRATE
-    Sensor("P5 (PT-5) - SRAD Vent Valve", "ai7", 10, Connection.SINGLE,
-           LinearCalibration(620, -39.1, "psi"))  # Calibrated 2/7/2021
-    Sensor("P4 (PT-1) - Ox Fill", "ai6", 10, Connection.SINGLE,
-           LinearCalibration(615, -44.1, "psi"))  # Calibrated 2/7/2021
-    Sensor("P3 (PT-2) - Ox Tank", "ai0", 10, Connection.SINGLE,
-           LinearCalibration(605, -53.3, "psi"))  # Calibrated 2/7/2021
-    Sensor("T8 - Tank Heating", "ai23", 10, Connection.SINGLE,
-           ThermistorCalibration(10000, 3434, 0.099524))  # Calibration pulled from LabVIEW
+    Sensor("Power in", "ai31", 10, Connection.SINGLE,
+           LinearCalibration(3, 0, "V"))
+    Sensor("+12V", "ai23", 10, Connection.SINGLE,
+           LinearCalibration(3, 0, "V"))
+    Sensor("+10V", "ai30", 10, Connection.SINGLE,
+           LinearCalibration(3, 0, "V"))
+    Sensor("+5V", "ai22", 10, Connection.SINGLE,
+           LinearCalibration(3, 0, "V"))
+    Sensor("Big Omega S-Type - Ox Tanks", "ai18", 0.2, Connection.DIFFERENTIAL,
+           # Factory calibration: 1000 kG / (2.9991 mV/V * 10 V)
+           LinearCalibration(1000 / (2.9991 / 1000 * 10), -10.1, "kg"))
+    Sensor("Honeywell S-Type - Fuel Tank Mass", "ai17", 0.2, Connection.DIFFERENTIAL,
+           LinearCalibration(5116, -0.94, "kg"))  # calibrated 13/3/2022
 
-    if CC:
-        Sensor("P2 (PT-3) - CC", "ai4", 10, Connection.SINGLE,
-               LinearCalibration(621, -267, "psi"))  # Calibrated 13/7/2021
-        Sensor("Thrust", "ai2", 0.2, Connection.DIFFERENTIAL, LinearCalibration(
-            65445, -20.9, "lbs"))  # Roughly calibrated 17/7/2021
-        Sensor("SP1 (PT-4) - Nozzle", "ai5", 0.2, Connection.DIFFERENTIAL,
-               LinearCalibration(171346, -99.8, "psi"))  # Calibrated 2/7/2021
-        Sensor("FAST", "ai1", 10, Connection.SINGLE, LinearCalibration(
-            35.3, -34.2, "psi"))  # Calibrated 13/7/2021
-        Sensor("T1 - CC", "ai16", 10, Connection.SINGLE,
-               ThermistorCalibration(10000, 3434, 0.099524))  # Calibration pulled from LabVIEW
-        Sensor("T2 - CC", "ai17", 10, Connection.SINGLE,
-               ThermistorCalibration(10000, 3434, 0.099524))  # Calibration pulled from LabVIEW
-        Sensor("T3 - CC", "ai18", 10, Connection.SINGLE,
-               ThermistorCalibration(10000, 3434, 0.099524))  # Calibration pulled from LabVIEW
-        Sensor("T4 - CC", "ai19", 10, Connection.SINGLE,
-               ThermistorCalibration(10000, 3434, 0.099524))  # Calibration pulled from LabVIEW
-        Sensor("T5 - CC", "ai20", 10, Connection.SINGLE,
-               ThermistorCalibration(10000, 3434, 0.099524))  # Calibration pulled from LabVIEW
-        Sensor("T6 - CC", "ai21", 10, Connection.SINGLE,
-               ThermistorCalibration(10000, 3434, 0.099524))  # Calibration pulled from LabVIEW
-        Sensor("T7 - CC", "ai22", 10, Connection.SINGLE,
-               ThermistorCalibration(10000, 3434, 0.099524))  # Calibration pulled from LabVIEW
+    Sensor("PNew (PT-5) - Ox Injector", "ai5", 2, Connection.SINGLE,
+           # Factory calibration mapping 4-20mA to 0-3000psi
+           LinearCalibration(1/98.1*3000/0.016, -0.004*3000/0.016, "psi"))
+    # Sensor("P5 (PT-2) - Ox Tank", "ai19", 10, Connection.SINGLE,
+    #        LinearCalibration(600, -54.9, "psi")) # calibrated 25/3/2022
+    Sensor("PNew3 (PT-3) - Fuel Tank", "ai7", 10, Connection.SINGLE,
+           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
+    Sensor("PNew2 - Fuel Injector", "ai6", 10, Connection.SINGLE,
+           LinearCalibration(1/98.3*3000/0.016, -0.004*3000/0.016, "psi"))
+    Sensor("PNew4 - Ox Tanks", "ai2", 10, Connection.SINGLE,
+           LinearCalibration(1/98.3*3000/0.016, -0.004*3000/0.016, "psi"))
+    Sensor("SP1 (PT-1) - Ox Fill", "ai16", 0.2, Connection.DIFFERENTIAL,
+           LinearCalibration(167706, -91.5, "psi"))  # Calibrated 25/3/2022
+
+    # Directly plugging in K-type thermocouples. 41uV / C and a cold junction temperature guessed at 23 C.
+    Sensor("T1 - Ox Tank Temp A", "ai0", 0.2, Connection.DIFFERENTIAL,
+           LinearCalibration(1 / (41 / 1000 / 1000), 0.0009 / (41 / 1000 / 1000), "C"))
+    Sensor("T2 - Ox Tank Temp B", "ai1", 0.2, Connection.DIFFERENTIAL,
+           LinearCalibration(1 / (41 / 1000 / 1000), 0.0009 / (41 / 1000 / 1000), "C"))
+    Sensor("T3 - Fuel Tank Temp A", "ai3", 0.2, Connection.DIFFERENTIAL,
+           LinearCalibration(1 / (41 / 1000 / 1000), 0.0009 / (41 / 1000 / 1000), "C"))
+    Sensor("T4 - Fuel Tank Temp B", "ai4", 0.2, Connection.DIFFERENTIAL,
+           LinearCalibration(1 / (41 / 1000 / 1000), 0.0009 / (41 / 1000 / 1000), "C"))
+
+    # Sensor("Big Omega S-Type", "ai17", 0.2, Connection.DIFFERENTIAL,
+    #        # Factory calibration: 1000 kG / (2.9991 mV/V * 10 V)
+    #        LinearCalibration(1000 / (2.9991 / 1000 * 10), -10.1, "kg"))
+    # Sensor("Thrust", "ai2", 0.2, Connection.DIFFERENTIAL, LinearCalibration(
+    #    65445, -20.9, "lbs"))  # Roughly calibrated 17/7/2021
+    Sensor("Pneumatic Pressure", "ai19", 10, Connection.SINGLE, LinearCalibration(
+        35.3, -34.2, "psi"))  # Calibrated 13/7/2021
+    # Sensor("T8 - Tank Heating", "ai23", 10, Connection.SINGLE,
+    #       ThermistorCalibration(10000, 3434, 0.099524))  # Calibration pulled from LabVIEW
