30aba0d2e8bb42b5d7749b8de6a264fb8da92d87
Robert Cai <xiaoyucai2015@gmail.com>
2022-05-20 20:54:29 -0400

    config file

--- sinks/plot/config.py
+++ sinks/plot/config.py
@@ -3,3 +3,9 @@ GRAPH_RESOLUTION = 10  # data points per second
 GRAPH_STEP = GRAPH_DURATION / 60  # how often to shift the graphs left in seconds.
 # last n seconds to be accounted for in running average, please don't set it larger than GRAPH_DURATION
 RUNNING_AVG_DURATION = 2
+WINDOW_SIZE_X = 1000
+WINDOW_SIZE_Y = 600
+DOCK_SIZE_X = 200
+DOCK_SIZE_Y = 200
+ITEMS_PER_ROW = WINDOW_SIZE_X // DOCK_SIZE_X
+ITEMS_PER_COLUMN = WINDOW_SIZE_Y // DOCK_SIZE_Y
