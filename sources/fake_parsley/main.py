410ae28faf4b96f30e6b0f8a45a962011a1fb74b
KavinSatheeskumar <kavinsathees@gmail.com>
2022-06-22 22:23:48 -0400

    Create beginnings of a bench marking tool for future can message dash items

---
+++ sources/fake_parsley/main.py
@@ -0,0 +1,17 @@
+from omnibus import Sender
+import time
+
+sender = Sender()
+CHANNEL = "CAN/Parsley"
+
+while True:
+	fake_log_message = {
+		"msg_type":"GENERAL_BOARD_STATUS",
+		"board_id":"LOGGER",
+		"data": {
+			"time": time.time(),
+			"status":"E_LOGGING"
+		}
+	}
+	sender.send(CHANNEL, fake_log_message)
+	time.sleep(0.01)
