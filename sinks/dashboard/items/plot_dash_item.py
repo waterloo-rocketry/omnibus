8d71178b9b4706fd06f73c929238a98c6805e2ad
Paniz Ojaghi <90856064+Panizghi@users.noreply.github.com>
2022-12-02 21:44:40 -0500

    Update plot_dash_item.py

--- sinks/dashboard/items/plot_dash_item.py
+++ sinks/dashboard/items/plot_dash_item.py
@@ -1,6 +1,6 @@
 from parsers import Parser
 from pyqtgraph.Qt import QtWidgets
-from pyqtgraph.QtWidgets import QGridLayout
+from pyqtgraph.Qt import QGridLayout
 
 import pyqtgraph as pg
 from pyqtgraph.console import ConsoleWidget
