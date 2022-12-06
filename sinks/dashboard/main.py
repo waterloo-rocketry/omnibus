2cac2a05791a5e3a27de7514e3c03dfe150d0999
Robert Cai <xiaoyucai2015@gmail.com>
2022-12-05 23:55:31 -0500

    Publisher and Subscriber Abstraction (Observer Pattern) (#71)
    Rework the entirety of the data processing and categorization logic of the dashboard. Replace parser classes with decorator function list. Replace Series with data stream stored within a publisher. 
    
    Co-authored-by: Ozayr Raazi <oraazi2004@gmail.com>
    Co-authored-by: KavinSatheeskumar <kavinsathees@gmail.com>

--- sinks/dashboard/main.py
+++ sinks/dashboard/main.py
@@ -1,6 +1,6 @@
 from omnibus import Receiver
 
-from parsers import Parser
+import parsers
 from dashboard import dashboard_driver
 
 receiver = Receiver("")  # subscribe to all channels
@@ -9,8 +9,8 @@ receiver = Receiver("")  # subscribe to all channels
 def update():  # gets called every frame
     # read all the messages in the queue and no more (zero timeout)
     while msg := receiver.recv_message(0):
-        # update whatever series subscribed to this channel
-        Parser.all_parse(msg.channel, msg.payload)
+        # updates streams, which them updates the dashitems
+        parsers.parse(msg.channel, msg.payload)
 
 
 dashboard_driver(update)
--- sinks/dashboard/series_test.py
+++ sinks/dashboard/series_test.py
@@ -12,24 +12,24 @@ class TestSeries:
 
     def test_no_downsample(self):
         s = Series("NAME")
-        s.add(1/10, 1)
+        s.add([1/10, 1])
         assert s.points[-2] == 1  # make sure we back-fill()ed the array
         assert s.points[-1] == 1
-        s.add(2/10, 2)
+        s.add([2/10, 2])
         assert s.points[-3] == 1
         assert s.points[-2] == 1
         assert s.points[-1] == 2
 
     def test_downsample(self):
         s = Series("NAME")
-        s.add(1/20, 1)  # downsampled away
-        s.add(2/20, 2)
+        s.add([1/20, 1])  # downsampled away
+        s.add([2/20, 2])
         assert s.points[-2] == 2
         assert s.points[-1] == 2
-        s.add(3/20, 3)  # downsampled away
+        s.add([3/20, 3])  # downsampled away
         assert s.points[-2] == 2
         assert s.points[-1] == 2
-        s.add(4/20, 4)
+        s.add([4/20, 4])
         assert s.points[-3] == 2
         assert s.points[-2] == 2
         assert s.points[-1] == 4
