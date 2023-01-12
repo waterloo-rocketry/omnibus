25a39819cdf88ff061577c2fe4658dcf95fb20c1
Robert Cai <xiaoyucai2015@gmail.com>
2023-01-11 22:20:55 -0500

    image resizing v0.1

--- sinks/dashboard/items/image_dash_item.py
+++ sinks/dashboard/items/image_dash_item.py
@@ -1,10 +1,11 @@
 from pyqtgraph.Qt import QtWidgets
 from pyqtgraph.Qt.QtWidgets import QVBoxLayout
 from pyqtgraph.Qt.QtGui import QPixmap
+from pyqtgraph.Qt.QtCore import Qt
 
 from .dashboard_item import DashboardItem
-from .registry import Register
 
+import time
 
 class ImageDashItem(DashboardItem):
     def __init__(self, imgPath):
@@ -18,12 +19,23 @@ class ImageDashItem(DashboardItem):
         self.picture = QPixmap(self.imgPath)
         self.picWidget = QtWidgets.QLabel()
         self.picWidget.setPixmap(self.picture)
-        self.layout.addWidget(self.picWidget)
-        self.layout.addWidget(QtWidgets.QLabel(self.imgPath))
+        self.layout.addWidget(self.picWidget, alignment=Qt.AlignmentFlag.AlignTop)
+        self.labelWidget = QtWidgets.QLabel(self.imgPath)
+        self.layout.addWidget(self.labelWidget, alignment=Qt.AlignmentFlag.AlignBottom)
         
+        self.w = self.width()
+        self.h = self.height()
 
     def on_delete(self):
         pass
 
     def get_name(self):
         return self.imgPath
+
+    # Honestly? This is a hard-coded and very scuffed solution. It doesn't even come with aspect ratio preservation. Don't use this lest we have no solutions.
+    def resizeEvent(self, event):
+        time.sleep(0.0000001)
+        self.picture = QPixmap(self.imgPath).scaled(self.width() - 40, self.height() - self.labelWidget.height() - 40)
+        self.picWidget.setPixmap(self.picture)
+
+        QtWidgets.QWidget.resizeEvent(self, event)
