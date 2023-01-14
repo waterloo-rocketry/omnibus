34e4ee1cc425c85f43bdbf5cb4e02549bc3aa022
KavinSatheeskumar <kavinsathees@gmail.com>
2023-01-14 11:27:00 -0500

    Enforce autopepe8

--- sources/pipe_output/main.py
+++ sources/pipe_output/main.py
@@ -4,10 +4,10 @@ import time
 from omnibus import Sender
 import json
 
-inp = Popen(sys.argv[1:], stdout = PIPE)
+inp = Popen(sys.argv[1:], stdout=PIPE)
 sender = Sender()
 CHANNEL = "SE/Fake"
 
 for line in inp.stdout:
-	js = json.loads(line.strip())
-	sender.send(CHANNEL, js)
+    js = json.loads(line.strip())
+    sender.send(CHANNEL, js)
