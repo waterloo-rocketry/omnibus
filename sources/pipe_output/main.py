9a388a55b9493db898addfc36c0101832a690198
KavinSatheeskumar <kavinsathees@gmail.com>
2023-01-14 11:51:14 -0500

    Address merge conflicts while rebasing

--- sources/pipe_output/main.py
+++ sources/pipe_output/main.py
@@ -1,13 +1,14 @@
+import json
 from subprocess import Popen, PIPE
 import sys
-import time
+
 from omnibus import Sender
-import json
+
 
 inp = Popen(sys.argv[1:], stdout=PIPE)
 sender = Sender()
 CHANNEL = "SE/Fake"
 
 for line in inp.stdout:
-    js = json.loads(line.strip())
-    sender.send(CHANNEL, js)
+    data = json.loads(line.strip())
+    sender.send(CHANNEL, data)
