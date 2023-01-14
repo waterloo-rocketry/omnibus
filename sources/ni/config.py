e2d72d400f718efa5175153cc77d283b18439d56
DAQ Computer <j2224che@uwaterloo.ca>
2023-01-14 16:03:48 -0500

    Initial draft of SF7 config

--- sources/ni/config.py
+++ sources/ni/config.py
@@ -3,6 +3,15 @@ from calibration import Sensor, Connection, LinearCalibration, ThermistorCalibra
 RATE = 1000  # Analog data sample rate
 READ_BULK = 20  # Number of samples to read at once for better performance
 
+# Mapping between briefcase ports and ai channels such that you can do ports[1] through ports[12]
+ports = [
+    None,  # port 0
+    "ai16", "ai17", "ai18",  # Differental channels 1-3
+    "ai19", "ai27", "ai20",  # Direct voltage channels 4-6
+    "ai28", "ai21", "ai29",  # 4-20 mA channels 7-9
+    "ai15", "ai7", "ai14",  # 4-20 mA channels 10-12
+]
+
 
 def setup():
     """
@@ -16,85 +25,67 @@ def setup():
            LinearCalibration(3, 0, "V"))
     Sensor("+5V", "ai22", 10, Connection.SINGLE,
            LinearCalibration(3, 0, "V"))
-    """
-       Sensor Configs, will change from test to test
-       """
-    # Port 7
-    # Sensor("(PT-5) - Fuel Injector", "ai28", 10, Connection.SINGLE,
-    #        LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
-    # Port 9
-    Sensor("(PT-3) - Injector", "ai29", 10, Connection.SINGLE,
-           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
-    # Port 10
-    Sensor("(PT-4) - Injector Tank", "ai15", 10, Connection.SINGLE,
-           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
-    # Port 11
-    Sensor("(PT-2) - Ox Tank", "ai7", 10, Connection.SINGLE,
-           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
-    # Port 12
-    Sensor("(PT-1) - Ox Fill Block", "ai14", 10, Connection.SINGLE,
-           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
-    # Port 1
-    # Sensor("Honeywell S-type", "ai16", 0.2, Connection.DIFFERENTIAL,
-    #        LinearCalibration(4177, -0.853, "kgs"))
-    # Port 2 - if used
-    Sensor("Omega S-Type - Ox Tanks", "ai17", 0.2, Connection.DIFFERENTIAL,
-           LinearCalibration(2936, -0.181, "V"))
 
-    Sensor("Thermocouple 1", "ai0", 5, Connection.SINGLE,
-           LinearCalibration(1200/5, -100 + (23.4 - -18), "C"))
-    Sensor("Thermocouple 2", "ai8", 5, Connection.SINGLE,
-           LinearCalibration(1200/5, -100 + (23.4 - -3), "C"))
-    Sensor("Thermocouple 3", "ai1", 5, Connection.SINGLE,
-           LinearCalibration(1200/5, -100 + (23.4 - -24), "C"))
-    Sensor("Thermocouple 4", "ai9", 5, Connection.SINGLE,
+    """
+    Sensor Configs, will change from test to test
+    """
+
+    # Standard TDL35 and TDM51 transducers: 4-20mA 0-3000 psi
+    Sensor("PT-1 Fill Block", ports[7], 10, Connection.SINGLE,
+           LinearCalibration(1/100*3000/0.016, -0.004*3000/0.016, "psi"))
+    Sensor("PT-2 Ox Tank", ports[8], 10, Connection.SINGLE,
+           LinearCalibration(1/100*3000/0.016, -0.004*3000/0.016, "psi"))
+    Sensor("PT-3 Injector", ports[9], 10, Connection.SINGLE,
+           LinearCalibration(1/100*3000/0.016, -0.004*3000/0.016, "psi"))
+    Sensor("PT-4 CC", ports[10], 10, Connection.SINGLE,
+           LinearCalibration(1/100*3000/0.016, -0.004*3000/0.016, "psi"))
+    Sensor("PT-6 Nitrogen", ports[11], 10, Connection.SINGLE,
+           LinearCalibration(1/100*3000/0.016, -0.004*3000/0.016, "psi"))
+
+    # Perseus, 1500 psi with a FSO@10V of 75 mV, but we're running it at 12V.
+    Sensor("PT-5 Perseus", ports[3], 0.2, Connection.DIFFERENTIAL,
+           LinearCalibration(1500 / (0.075 / 10 * 12), 0, "psi")
+
+    # Honeywell S-type, 1000 N (divide by 9.81 to kg) an 2.002 mv/V at 12V
+    Sensor("Honeywell S-type (Ox tank)", ports[1], 0.2, connection.DIFFERENTIAL,
+           LinearCalibration((1000/9.81)/(2.002/1000*12), 0, "kg")
+
+    # CAS BSA-5KLB 5000 lbf, 3 mv/v, 12v excitation
+    Sensor("Thrust", ports[2], 0.2, Connection.DIFFERENTIAL,
+           LinearCalibration(5000 / (3/1000*12), -20, "lbs"))
+
+    Sensor("Thermocouple 1", "ai10", 5, Connection.SINGLE,
+           LinearCalibration(1200/5, -100, "C"))
+    Sensor("Thermocouple 2", "ai2", 5, Connection.SINGLE,
+           LinearCalibration(1200/5, -100, "C"))
+    Sensor("Thermocouple 3", "ai9", 5, Connection.SINGLE,
+           LinearCalibration(1200/5, -100, "C"))
+    Sensor("Thermocouple 4", "ai1", 5, Connection.SINGLE,
            LinearCalibration(1200/5, -100, "C"))
 
-    # Port 3
-    # CAS BSA-5KLB 5000 lbf, 3 mv/v, 12v excitation
-    Sensor("Thrust", "ai18", 0.2, Connection.DIFFERENTIAL,
-           LinearCalibration(5000 / (3/1000*12), -20, "lbs"))
+    Sensor("Thermocouple 5", "ai8", 5, Connection.SINGLE,
+           LinearCalibration(1/220*1300/0.016, -0.04*1300/0.016, "C"))
+    Sensor("Thermocouple 6", "ai0", 5, Connection.SINGLE,
+           LinearCalibration(1/220*1300/0.016, -0.04*1300/0.016, "C"))
+
     """
-       Everything below here is just for documentation purposes
-       """
-    # For every V it powered it will output 2 mV when experiencing Maximum load.
-    # Will output about 0 mV when experiencing nothing
-    # Linear in between
+    Everything below here is just for documentation purposes
+    """
 
-    # Mass = m(volage * input power)
-    # 200Kg = m (10 * 2 / 1000)
-    # m = 200 / (10 * 2 / 1000)
-
-    # Sensor("Honeywell S-Type - Fuel Tank Mass", "ai17", 0.2, Connection.DIFFERENTIAL,
-    #        LinearCalibration(5116, -0.94, "kg"))  # calibrated 13/3/2022
-
-    # Sensor("PNew (PT-5) - Ox Injector", "ai5", 2, Connection.SINGLE,
-    #     # Factory calibration mapping 4-20mA to 0-3000psi
-    #        LinearCalibration(1/98.1*3000/0.016, -0.004*3000/0.016, "psi"))
-    # Sensor("P5 (PT-2) - Ox Tank", "ai19", 10, Connection.SINGLE,
-    #        LinearCalibration(600, -54.9, "psi")) # calibrated 25/3/2022
-    # Sensor("PNew3 (PT-3) - Fuel Tank", "ai7", 10, Connection.SINGLE,
-    #        LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
-    # Sensor("PNew2 - Fuel Injector", "ai6", 10, Connection.SINGLE,
-    #        LinearCalibration(1/98.3*3000/0.016, -0.004*3000/0.016, "psi"))
-    # Sensor("PNew4 - Ox Tanks", "ai2", 10, Connection.SINGLE,
-    #        LinearCalibration(1/98.3*3000/0.016, -0.004*3000/0.016, "psi"))
     # Sensor("SP1 (PT-1) - Ox Fill", "ai16", 0.2, Connection.DIFFERENTIAL,
     #        LinearCalibration(167706, -91.5, "psi"))  # Calibrated 25/3/2022
 
     # # Directly plugging in K-type thermocouples. 41uV / C and a cold junction temperature guessed at 23 C.
     # Sensor("T1 - Ox Tank Temp A", "ai0", 0.2, Connection.DIFFERENTIAL,
     #        LinearCalibration(1 / (41 / 1000 / 1000), 0.0009 / (41 / 1000 / 1000), "C"))
-    # Sensor("T2 - Ox Tank Temp B", "ai1", 0.2, Connection.DIFFERENTIAL,
-    #        LinearCalibration(1 / (41 / 1000 / 1000), 0.0009 / (41 / 1000 / 1000), "C"))
-    # Sensor("T3 - Fuel Tank Temp A", "ai3", 0.2, Connection.DIFFERENTIAL,
-    #        LinearCalibration(1 / (41 / 1000 / 1000), 0.0009 / (41 / 1000 / 1000), "C"))
-    # Sensor("T4 - Fuel Tank Temp B", "ai4", 0.2, Connection.DIFFERENTIAL,
-    #        LinearCalibration(1 / (41 / 1000 / 1000), 0.0009 / (41 / 1000 / 1000), "C"))
 
+    # Sensor("Honeywell S-Type - Fuel Tank Mass", "ai17", 0.2, Connection.DIFFERENTIAL,
+    #        LinearCalibration(5116, -0.94, "kg"))  # calibrated 13/3/2022
+    # Sensor("Omega S-Type - Ox Tanks", "ai17", 0.2, Connection.DIFFERENTIAL,
+    #        LinearCalibration(2936, -0.181, "V"))
     # Sensor("Big Omega S-Type", "ai17", 0.2, Connection.DIFFERENTIAL,
-    #         # Factory calibration: 1000 kG / (2.9991 mV/V * 10 V)
-    #         LinearCalibration(1000 / (2.9991 / 1000 * 10), -10.1, "kg"))
+    #        # Factory calibration: 1000 kG / (2.9991 mV/V * 10 V)
+    #        LinearCalibration(1000 / (2.9991 / 1000 * 10), -10.1, "kg"))
     # Sensor("Thrust", "ai2", 0.2, Connection.DIFFERENTIAL,
     #        LinearCalibration(65445, -20.9, "lbs"))  # Roughly calibrated 17/7/2021
     # Sensor("Pneumatic Pressure", "ai19", 10, Connection.SINGLE,
