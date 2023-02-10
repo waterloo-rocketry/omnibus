63350ecff0081574f0405ac7c93151272eec1f6a
KavinSatheeskumar <kavinsathees@gmail.com>
2023-02-10 15:07:43 -0500

    Add orientation mode, clean up code

--- sinks/dashboard/items/payloads_plot_dash_item.py
+++ sinks/dashboard/items/payloads_plot_dash_item.py
@@ -17,8 +17,6 @@ import time
 #if there is error for opengl use the following command to install the acc : sudo easy_install pyopengl
 from .registry import Register
 
-pos_list = []
-
 @Register
 class PayloadDashItem (DashboardItem):
     def __init__(self, props):
@@ -32,38 +30,54 @@ class PayloadDashItem (DashboardItem):
         # save props as a field
         self.props = props
 
-        # subscribe to stream dictated by properties
-        publisher.subscribe(self.props[0], self.on_data_update)
-
-        self.w = gl.GLViewWidget()
-
-        pos = np.random.random(size=(1,3))
-        pos *= [10,-10,10]
-        pos[0] = (0,0,0)
-        size = np.random.random(size=pos.shape[0])*10
-        self.widget = gl.GLLinePlotItem() #pos=pos, color=(1,1,1,1), size=size
-
-        self.w.setCameraPosition(distance=40)
+        # initilize 3D environment
+        self.view = gl.GLViewWidget()
+        self.view.setCameraPosition(distance=40)
         gx = gl.GLGridItem()
         gx.rotate(90, 0, 1, 0)
         gx.translate(-10, 0, 0)
-        self.w.addItem(gx)
+        self.view.addItem(gx)
         gy = gl.GLGridItem()
         gy.rotate(90, 1, 0, 0)
         gy.translate(0, -10, 0)
-        self.w.addItem(gy)
+        self.view.addItem(gy)
         gz = gl.GLGridItem()
         gz.translate(0, 0, -10)
-        self.w.addItem(gz)
-        self.w.addItem(self.widget)
+        self.view.addItem(gz)
+
+        # subscribe to stream dictated by properties
+        if self.props[1]:
+            publisher.subscribe(self.props[0], self.on_data_update_orientation)
+            self.xaxis = gl.GLLinePlotItem()
+            self.view.addItem(self.xaxis)
+            self.yaxis = gl.GLLinePlotItem()
+            self.view.addItem(self.yaxis)
+            self.zaxis = gl.GLLinePlotItem()
+            self.view.addItem(self.zaxis)
+            self.orientation = (0, 0, 0) # Euler Angles
+        else:
+            publisher.subscribe(self.props[0], self.on_data_update_position)
+            self.pos_list = []
+            self.line = gl.GLLinePlotItem()
+            self.view.addItem(self.line)
 
         # add it to the layout
-        self.layout.addWidget(self.w, 0, 0)
+        self.layout.addWidget(self.view, 0, 0)
         self.start_time = time.time()
 
        
 
     def prompt_for_properties(self):
+
+        orientation_mode = prompt_user(
+            self,
+            "Orientation Mode",
+            "Is the data a position or orientation?",
+            "items",
+            ["Position", "Orientation"]
+        )
+
+        enable_orientation = True if orientation_mode == "Orientation" else False
 
         channel_and_series = prompt_user(
             self,
@@ -75,37 +89,68 @@ class PayloadDashItem (DashboardItem):
         if not channel_and_series:
             return None
 
-        return [channel_and_series]
+        return [channel_and_series, enable_orientation]
 
-    def on_data_update(self, payload):
+    def on_data_update_position(self, payload):
         time, point = payload
-        pos = tuple(point)
-        pos_list.append(pos)
-        if len(pos_list) > 200:
-            pos_list.pop(0)
+        self.pos_list.append(tuple(point))
+        if len(self.pos_list) > 200:
+            self.pos_list.pop(0)
 
-        pos_array = np.array(pos_list)
+        self.line.setData(pos=self.pos_list, color=(1.0,1.0,1.0,1.0))
 
-        size = [[1]]
-        color = np.empty((53, 4))
-        z = 0.5
-        d = 6.0
-        #print(pos_list)
-        self.widget.setData(pos=pos_array, color=(1.0,1.0,1.0,1.0))
-        #drawing_variable = gl.GLLinePlotItem(pos = pos_list, width = 1, antialias = True)   #make a variable to store drawing data(specify the points, set antialiasing)
-        #self.w.addItem(drawing_variable) #draw the item
+    def on_data_update_orientation(self, payload):
+        time, orientation = payload
 
-        #drawing_variable = gl.GLLinePlotItem(pos = pos_array[0,:], color=(1.0,1.0,1.0,1.0) , antialias = True)   #make a variable to store drawing data(specify the points, set antialiasing)
-       # self.w.addItem(drawing_variable) #draw the item
+        xlist = [(0,0,0), self.transform((10, 0, 0), orientation)]
+        ylist = [(0,0,0), self.transform((0, 10, 0), orientation)]
+        zlist = [(0,0,0), self.transform((0, 0, 10), orientation)]
+
+        self.xaxis.setData(pos=xlist, color=(1.0,0.0,0.0,1.0))
+        self.yaxis.setData(pos=ylist, color=(0.0,1.0,0.0,1.0))
+        self.zaxis.setData(pos=zlist, color=(0.0,0.0,1.0,1.0))
+
+    def transform(self, point, euler_angle):
+        return self.Ry(self.Rz(point, euler_angle[0]), euler_angle[1])
+
+    def Rz(self, point, gamma):
+        x, y, z = point
+
+        return (
+            np.cos(gamma)*x - np.sin(gamma)*y,
+            np.sin(gamma)*x + np.cos(gamma)*y,
+            z
+            )
+
+    def Ry(self, point, beta):
+        x, y, z = point
+
+        return (
+            np.cos(beta)*x + np.sin(beta)*z,
+            y,
+            -np.sin(beta)*x + np.cos(beta)*z
+            )
+
+    def Rx(self, point, alpha):
+        x, y, z = point
+
+        return (
+            x,
+            np.cos(alpha)*y - np.sin(alpha)*z,
+            np.sin(alpha)*y + np.cos(alpha)*z
+            )
 
     def get_props(self):
         return self.props
 
     def get_name():
-        return "Paniz Plot"
+        return "Payload Plot"
 
     def on_delete(self):
-        publisher.unsubscribe_from_all(self.on_data_update)
+        if self.props[1]:
+            publisher.unsubscribe_from_all(self.on_data_update_orientation)
+        else:
+            publisher.unsubscribe_from_all(self.on_data_update_position)
 
 if __name__ == '__main__':    
     pg.exec()
