851ef606ac3fa663c70fb16285b2d32dd759d255
Michael <michael.li.web@gmail.com>
2023-02-22 00:02:09 -0500

    8 byte message can go back and forth now

--- sinks/dashboard/dashboard.py
+++ sinks/dashboard/dashboard.py
@@ -29,7 +29,7 @@ from utils import ConfirmDialog
 # These need to be imported to be added to the registry
 from items.plot_dash_item import PlotDashItem
 from items.can_message_table import CanMsgTableDashItem
-from items.can_sender.can_sender import CanSender
+from items.can_sender import CanMsgSndr #idk why this is needed but it doesn't show in dropdown otherwise
 from omnibus.util import TickCounter
 from utils import prompt_user, ConfirmDialog
 
