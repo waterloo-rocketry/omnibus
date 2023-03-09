fa215947accf057383411ad88eda6d7ed5bbb0df
Ozayr Raazi <oraazi2004@gmail.com>
2023-03-09 16:15:29 -0500

    Added saving and loading

--- sinks/dashboard/dashboard2.py
+++ sinks/dashboard/dashboard2.py
@@ -1,7 +1,6 @@
 import os, sys, json
-from time import sleep
 
-from pyqtgraph.Qt.QtCore import Qt, QTimer, QRectF
+from pyqtgraph.Qt.QtCore import Qt, QTimer
 from pyqtgraph.Qt.QtWidgets import (
     QGraphicsView,
     QGraphicsScene,
@@ -10,33 +9,32 @@ from pyqtgraph.Qt.QtWidgets import (
     QMenuBar,
     QVBoxLayout,
     QGraphicsItem,
-    QGraphicsRectItem,
-    QPushButton
+    QGraphicsRectItem
 )
-from pyqtgraph.widgets.GraphicsView import GraphicsView
-
 from items import registry
+from omnibus.util import TickCounter
+
+# These need to be imported to be added to the registry
 from items.plot_dash_item import PlotDashItem
 from items.can_message_table import CanMsgTableDashItem
-from omnibus.util import TickCounter
-from utils import prompt_user
 
 class MyQGraphicsView(QGraphicsView):
 
     def __init__ (self, parent=None):
+        # Initialize the parent class
         super(MyQGraphicsView, self).__init__ (parent)
-        self.zoom = False
 
     def wheelEvent(self, event):
+        # Zoom if Shift is held, otherwise scroll
         if event.modifiers() & Qt.ShiftModifier:
             # Zoom Factor
             zoomInFactor = 1.1
             zoomOutFactor = 1 / zoomInFactor
 
-            # Set Anchors
+            # Zooms to the position of the mouse
             self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
 
-            # Zoom
+            # Determine zoom in/out by how much the wheel moves
             angle = event.angleDelta()
             if angle.x() > 0 or angle.y() > 0:
                 zoomFactor = zoomInFactor
@@ -58,7 +56,8 @@ class Dashboard(QWidget):
         # Called every frame to get new data
         self.callback = callback 
 
-        # Dictionary to keep track of widgets
+        # Dictionary to map rectitems to widgets 
+        # and dashitems
         self.widgets = {}
 
         # The file from which the dashboard is loaded
@@ -121,9 +120,6 @@ class Dashboard(QWidget):
 
         self.layout.setMenuBar(menubar)
 
-        # Load the last saved state
-        self.load()
-
         # Set the counter
         self.counter = TickCounter(1)
 
@@ -133,26 +129,35 @@ class Dashboard(QWidget):
         self.layout.addWidget(self.view)
         self.setLayout(self.layout)
 
-    def add(self, dashitem):
-        # Get the current size of the view area and map
-        # it to the underlying scene
-        viewport = self.view.viewport().size()
-        print(viewport)
-        mapped = self.view.mapToScene(viewport.width()/2, viewport.height()/2)
-
+    def add(self, dashitem, pos=None):
         # Add the dash item to the scene and get
         # its proxy widget and dimension
         proxy = self.scene.addWidget(dashitem)
         height = proxy.size().height()
         width = proxy.size().width()
-        print(f"W: {width}, H: {height}")
 
-        # Center the widget in the view. Qt sets position
-        # based on the upper left corner, so subtract
-        # half the width and height of the widget to 
-        # center the center
-        xpos = mapped.x() - (width/2)
-        ypos = mapped.y() - (height/2)
+        # If a position is given, use it. If not, position
+        # the widget in the center of the current view
+        if pos:
+            mapped = self.view.mapToScene(pos[0], pos[1])
+            xpos = mapped.x()
+            ypos = mapped.y()
+        else:
+            # Get the current size of the view area and map
+            # it to the underlying scene
+            viewport = self.view.viewport().size()
+            view_xpos = viewport.width()/2
+            view_ypos = viewport.height()/2
+
+            mapped = self.view.mapToScene(view_xpos, view_ypos)
+
+            # Center the widget in the view. Qt sets position
+            # based on the upper left corner, so subtract
+            # half the width and height of the widget to 
+            # center the center
+            xpos = mapped.x() - (width/2)
+            ypos = mapped.y() - (height/2)
+
         proxy.setPos(xpos, ypos)
         proxy.setFocusPolicy(Qt.NoFocus)
         
@@ -164,25 +169,73 @@ class Dashboard(QWidget):
         rect.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
         self.scene.addItem(rect)
 
-        # Add the proxy widget and dashitem to the dict
+        # Map the proxy widget and dashitem to the rectitem
         self.widgets[rect] = [proxy, dashitem]
 
-    def remove(self):
-        for item in self.scene.selectedItems():
-            components = self.widgets[item]
-            proxy = components[0]
-            dashitem = components[1]
-            self.scene.removeItem(item)
-            proxy.deleteLater()
-            dashitem.on_delete()
+    def remove(self, item):
+        # Remove the rectangle from the scene,
+        # delete the proxy widget and tell the
+        # dashitem to stop sending data
+        components = self.widgets[item]
+        proxy = components[0]
+        dashitem = components[1]
+        self.scene.removeItem(item)
+        proxy.deleteLater()
+        dashitem.on_delete()
+
+    def remove_all(self):
+        for item in self.widgets:
+            self.remove(item)
+
+        # Clear the mapping list. This can't be
+        # done while iterating through the dict
+        # because it changes the length and causes
+        # a RunTime Error
+        self.widgets = {}
 
     def load(self):
-        # TODO
-        pass
+        # First remove all the current widgets
+        self.remove_all()
+
+        # Then load the data from the savefile
+        if os.path.exists(self.filename):
+            with open(self.filename, "r") as savefile:
+                data = json.load(savefile)
+        else:
+            return
+
+        # Add every widget in the data
+        for widget in data:
+            # ObjectTypes can't be converted to JSON
+            # See the save method
+            for item_type in registry.get_items():
+                if widget["class"] == item_type.get_name():
+                    self.add(item_type(widget["props"]), widget["pos"])
+                    break
 
     def save(self):
-        # TODO
-        pass
+        data = []
+        for items in self.widgets.values():
+            # Get the proxy widget and dashitem
+            proxy = items[0]
+            dashitem = items[1]
+
+            # Get the coordinates of the proxy widget
+            # on the view not the scene
+            scenepos = proxy.scenePos()
+            viewpos = self.view.mapFromScene(scenepos)
+
+            # Add the position, dashitem name and dashitem props
+            for item_type in registry.get_items():
+                if type(dashitem) == item_type:
+                    data.append({"class": item_type.get_name(), 
+                                 "props": dashitem.get_props(),
+                                 "pos": [viewpos.x(), viewpos.y()]})
+                    break
+
+        # Write data to savefile
+        with open(self.filename, "w") as savefile:
+            json.dump(data, savefile)
 
     def switch(self):
         # TODO
@@ -201,7 +254,9 @@ class Dashboard(QWidget):
     # Handling user events
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Backspace:
-            self.remove()
+            for item in self.scene.selectedItems():
+                self.remove(item)
+                self.widgets.pop(item)
 
 
 def dashboard_driver(callback):
@@ -213,4 +268,5 @@ def dashboard_driver(callback):
     timer.start(16)  # Capped at 60 Fps, 1000 ms / 16 ~= 60
 
     dash.show()
+    dash.load()
     app.exec()
