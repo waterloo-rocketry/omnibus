d056654f9bac1bd7532da15a31b2fcd5c9433bef
BluCode <22122986+BluCodeGH@users.noreply.github.com>
2023-03-17 00:05:23 -0400

    LSF1 Config and Small Tweaks (#97)
    * Preliminary Config
    
    * Fix Kavin's broken-ass config (smh why didn't you base it off of sf7)
    
    * Re-add startup script
    
    * Fix plot averages
    
    * Reimplement old plotter performance improvements, significant speedups
    
    * Autopep8
    
    ---------
    
    Co-authored-by: KavinSatheeskumar <kavinsathees@gmail.com>
    Co-authored-by: DAQ Computer <j2224che@uwaterloo.ca>

--- sources/ni/config.py
+++ sources/ni/config.py
@@ -3,6 +3,21 @@ from calibration import Sensor, Connection, LinearCalibration, ThermistorCalibra
 RATE = 1000  # Analog data sample rate
 READ_BULK = 20  # Number of samples to read at once for better performance
 
+# Mapping between briefcase ports and ai channels such that you can do ports[1] through ports[12]
+ports = [
+    None,  # port 0
+    "ai16", "ai17", "ai18",  # Differental channels 1-3
+    "ai19", "ai27", "ai20",  # Direct voltage channels 4-6
+    "ai28", "ai21", "ai29",  # 4-20 mA channels 7-9
+    "ai15", "ai7",  "ai14",  # 4-20 mA channels 10-12
+]
+
+thermo_ports = [
+    None,  # port 0
+    "ai10", "ai2", "ai9", "ai1",  # Ports 1-4
+    "ai8", "ai0"                  # Ports 5-6
+]
+
 
 def setup():
     """
@@ -16,55 +31,72 @@ def setup():
            LinearCalibration(3, 0, "V"))
     Sensor("+5V", "ai22", 10, Connection.SINGLE,
            LinearCalibration(3, 0, "V"))
-    """
-       Sensor Configs, will change from test to test
-       """
-    # Port 7
-    # Sensor("(PT-5) - Fuel Injector", "ai28", 10, Connection.SINGLE,
-    #        LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
-    # Port 9
-    Sensor("(PT-3) - Injector", "ai29", 10, Connection.SINGLE,
-           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
-    # Port 10
-    Sensor("(PT-4) - Injector Tank", "ai15", 10, Connection.SINGLE,
-           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
-    # Port 11
-    Sensor("(PT-2) - Ox Tank", "ai7", 10, Connection.SINGLE,
-           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
-    # Port 12
-    Sensor("(PT-1) - Ox Fill Block", "ai14", 10, Connection.SINGLE,
-           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
-    # Port 1
-    # Sensor("Honeywell S-type", "ai16", 0.2, Connection.DIFFERENTIAL,
-    #        LinearCalibration(4177, -0.853, "kgs"))
-    # Port 2 - if used
-    Sensor("Omega S-Type - Ox Tanks", "ai17", 0.2, Connection.DIFFERENTIAL,
-           LinearCalibration(2936, -0.181, "V"))
 
-    Sensor("Thermocouple 1", "ai0", 5, Connection.SINGLE,
+    """
+    Sensor Configs, will change from test to test
+    """
+
+    # Standard TDL35 and TDM51 transducers: 4-20mA 0-3000 psi
+    Sensor("PT-1 Ox Fill", ports[7], 10, Connection.SINGLE,
+           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
+    Sensor("PT-2 Ox Tank", ports[8], 10, Connection.SINGLE,
+           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
+    Sensor("PT-3 Fuel Tank", ports[9], 10, Connection.SINGLE,
+           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
+    Sensor("PT-5 Ox Injector", ports[10], 10, Connection.SINGLE,
+           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
+    Sensor("PT-6 CC 1", ports[11], 10, Connection.SINGLE,
+           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
+    Sensor("PT-7 CC 2", ports[12], 10, Connection.SINGLE,
+           LinearCalibration(1/98.0*3000/0.016, -0.004*3000/0.016, "psi"))
+
+    # PSE540-R06 pneumatic transducer. 1-5V output for 0-1 MPa
+    Sensor("PT-4 Pneumatics", ports[4], 10, Connection.SINGLE,
+           LinearCalibration(145 / (5 - 1), -145 / (5 - 1), "psi"))
+
+    # Perseus, 1500 psi with a FSO@10V of 75 mV, but we're running it at 12V.
+    # Sensor("PT-5 Perseus", ports[3], 0.2, Connection.DIFFERENTIAL,
+    #        LinearCalibration(1500 / (0.075 / 10 * 12), 0, "psi"))
+
+    # Honeywell S-type, 1000 N (divide by 9.81 to kg) an 2.002 mv/V at 12V
+    Sensor("Honeywell S-type - Ox Tank", ports[1], 0.2, Connection.DIFFERENTIAL,
+           LinearCalibration((1000/9.81)/(2.002/1000*12), -0.7, "kg"))
+
+    # Omega S-type (LC-103B), 200 lb (divide by 2.2 to kg), 3 mV/V at 12V
+    Sensor("Omega S-Type - Fuel Tank", ports[2], 0.2, Connection.DIFFERENTIAL,
+           LinearCalibration((200/2.205) / (3/1000*12), -1.9, "kg"))
+
+    # Omega LC501 2000 lbf (divide by 2.2 to kg), 3 mv/v, 12v excitation
+    Sensor("Thrust", ports[3], 0.2, Connection.DIFFERENTIAL,
+           LinearCalibration((2000/2.205) / (3/1000*12), -7.4, "kg"))
+
+    # CAS BSA-5KLB 5000 lbf (divide by 2.2 to kg), 3 mv/v, 12v excitation
+    # Sensor("Thrust", ports[3], 0.2, Connection.DIFFERENTIAL,
+    #        LinearCalibration((5000/2.205) / (3/1000*12), -20, "kg"))
+
+    # Vent Thermistor with a 10k resistor divider between it and ground
+    # Sensor("Vent Thermistor", ports[12], 10, Connection.SINGLE,
+    #        ThermistorCalibration(12, 10000, 3434, 0.099524))  # Calibration pulled from LabVIEW
+
+    # Thermocouples using Spidey Sense R3 and 0-5V converter boards
+    Sensor("Thermocouple 1", thermo_ports[1], 5, Connection.SINGLE,
            LinearCalibration(1200/5, -100 + (23.4 - -18), "C"))
-    Sensor("Thermocouple 2", "ai8", 5, Connection.SINGLE,
+    Sensor("Thermocouple 2", thermo_ports[2], 5, Connection.SINGLE,
            LinearCalibration(1200/5, -100 + (23.4 - -3), "C"))
-    Sensor("Thermocouple 3", "ai1", 5, Connection.SINGLE,
+    Sensor("Thermocouple 3", thermo_ports[3], 5, Connection.SINGLE,
            LinearCalibration(1200/5, -100 + (23.4 - -24), "C"))
-    Sensor("Thermocouple 4", "ai9", 5, Connection.SINGLE,
+    Sensor("Thermocouple 4", thermo_ports[4], 5, Connection.SINGLE,
            LinearCalibration(1200/5, -100, "C"))
 
-    # Port 3
-    # CAS BSA-5KLB 5000 lbf, 3 mv/v, 12v excitation
-    Sensor("Thrust", "ai18", 0.2, Connection.DIFFERENTIAL,
-           LinearCalibration(5000 / (3/1000*12), -20, "lbs"))
+    # Thermocouples using the blue 4-20mA converters.
+    Sensor("Thermocouple 5", thermo_ports[5], 5, Connection.SINGLE,
+           LinearCalibration(1/220*1300/0.016, -0.004*1300/0.016, "C"))
+    Sensor("Thermocouple 6", thermo_ports[6], 5, Connection.SINGLE,
+           LinearCalibration(1/220*1300/0.016, -0.004*1300/0.016, "C"))
+
     """
-       Everything below here is just for documentation purposes
-       """
-    # For every V it powered it will output 2 mV when experiencing Maximum load.
-    # Will output about 0 mV when experiencing nothing
-    # Linear in between
-
-    # Mass = m(volage * input power)
-    # 200Kg = m (10 * 2 / 1000)
-    # m = 200 / (10 * 2 / 1000)
-
+    Everything below here is just for documentation purposes
+    """
     # Sensor("Honeywell S-Type - Fuel Tank Mass", "ai17", 0.2, Connection.DIFFERENTIAL,
     #        LinearCalibration(5116, -0.94, "kg"))  # calibrated 13/3/2022
 
