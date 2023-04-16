39639bb75f2367009ca5e642e28792b0a3d0e22e
Michael <michael.li.web@gmail.com>
2023-04-15 23:40:12 -0400

    it jankly works tested and confirmed
    there are a couple of issues that need to be addressed (that I didn't catch during earlier PRs that I might have to fix now): The dashboard widget is messing with wheelEvents and when you hover over a opened dropdown, it tries to scroll the dashboard instead of scroll through the dropdown. This is the biggest pain but will need to be addressed. Then there are some minor things such as when the layout auto expands, I don't think the boudning box is updated so the border is a little whack. Also, I havne't actually sent a CAN message yet :skull: also it would be nice to tab and shift-tab through the fields (OH RIGHT THATS WHY I NEEDED OT ABSTRACT IT AWAYT CAUSE QCOMBOBOXES ALSO NEED THE SIGNAL). Anyways, pain.

--- sinks/dashboard/dashboard.py
+++ sinks/dashboard/dashboard.py
@@ -4,7 +4,7 @@ import json
 import signal
 
 from pyqtgraph.Qt.QtCore import Qt, QTimer
-from pyqtgraph.Qt.QtGui import QPainter
+from pyqtgraph.Qt.QtGui import QPainter, QCursor
 from pyqtgraph.Qt.QtWidgets import (
     QGraphicsView,
     QGraphicsScene,
@@ -13,7 +13,8 @@ from pyqtgraph.Qt.QtWidgets import (
     QMenuBar,
     QVBoxLayout,
     QGraphicsItem,
-    QGraphicsRectItem
+    QGraphicsRectItem,
+    QComboBox
 )
 from items import registry
 from omnibus.util import TickCounter
@@ -365,8 +366,13 @@ class Dashboard(QWidget):
 
     # Method to get new data for widgets
     def update(self):
-        self.counter.tick()
-        self.callback()
+        try:
+            self.counter.tick()
+            self.callback()
+        except KeyboardInterrupt:
+            print("wtf")
+            # Catch the KeyboardInterrupt exception and exit the application
+            QApplication.quit()
 
     # Method to center the view
     def reset_zoom(self):
@@ -395,7 +401,10 @@ def dashboard_driver(callback):
 
     timer = QTimer()
     timer.timeout.connect(dash.update)
-    timer.start(16)  # Capped at 60 Fps, 1000 ms / 16 ~= 60
+    try:
+        timer.start(16)  # Capped at 60 Fps, 1000 ms / 16 ~= 60
+    except KeyboardInterrupt:
+        exit()
 
     dash.show()
     dash.load()
