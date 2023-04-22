251e427efc33e843139a610e60c700709ac55775
Michael Li <michael.li.web@gmail.co>
2023-04-22 00:01:29 -0400

    Given up on cleaning dashboard.py
    Kavin, it only took me like 10 hours but yes this is scope creep, I am not doing this. I can document what I did, but I doubt anyone would willingly go and do Python GUI things to fix this, so welp I really did try guys

--- sinks/dashboard/utils.py
+++ sinks/dashboard/utils.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional
 from pyqtgraph.Qt import QtWidgets
-from pyqtgraph.Qt.QtCore import Qt, Signal, QEvent, QObject
+from pyqtgraph.Qt.QtCore import Qt, Signal, QEvent, QObject, QPoint
 
 
 class EventTracker(QObject):
@@ -53,57 +53,6 @@ class KeyEvent:
     key_code: int
     modifiers: int
     text: Optional[str]
-
-
-class EventTracker(QObject):
-    """
-    PyQt implements the observer pattern to communicate state changes between widgets.
-    Signals are emitted by a widget (subject) and connected widget slots can listen in (observer).
-    For example, a QPushButton emits a clicked() signal when it is activated by the mouse, spacebar,
-    or keyboard shortcut and a QLabel can connect to this signal with a slot to update its text.
-    More info about this: https://doc.qt.io/qtforpython/overviews/signalsandslots.html
-
-    Unfortuantely, PyQt provides a limited amount of signals and since we use signals
-    quite freqently, it would be nice to centeralize everything in one class.
-    """
-    # Key presses
-    backspace_pressed = Signal(QtWidgets.QWidget)
-    tab_pressed = Signal(QtWidgets.QWidget)
-    reverse_tab_pressed = Signal(QtWidgets.QWidget)
-    enter_pressed = Signal()
-    zoom_in = Signal()
-    zoom_out = Signal()
-    zoom_reset = Signal()
-
-    def eventFilter(self, widget, event):
-        """
-        After we intercept the event, propagate it down the event
-        chain so that we don't disturb any default behaviours or return True
-        if we don't want any widgets to further handle the event.
-        """
-        if event.type() == QEvent.KeyPress:
-            key_press = KeyEvent(event.key(), event.modifiers())
-            match key_press:
-                case KeyEvent(Qt.Key_Backspace, _):
-                    self.backspace_pressed.emit(widget)
-                case KeyEvent(Qt.Key_Backtab, _):
-                    self.reverse_tab_pressed.emit(widget)
-                case KeyEvent(Qt.Key_Tab, _):
-                    self.tab_pressed.emit(widget)
-                case KeyEvent(Qt.Key_Enter, _) | KeyEvent(Qt.Key_Return, _):
-                    self.enter_pressed.emit()
-                case KeyEvent(Qt.Key_Equal, Qt.ControlModifier):
-                    self.zoom_in.emit()
-                case KeyEvent(Qt.Key_Minus, Qt.ControlModifier):
-                    self.zoom_out.emit()
-                case KeyEvent(Qt.Key_0, Qt.ControlModifier):
-                    self.zoom_reset.emit()
-        return super().eventFilter(widget, event)
-
-@dataclass
-class KeyEvent:
-    key_code: int
-    modifiers: int
 
 
 class ConfirmDialog(QtWidgets.QDialog):
