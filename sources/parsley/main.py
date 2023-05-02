c39f1c18ebc4fb19aa4c65c4e50771789782acc0
Michael <michael.li.web@gmail.com>
2023-05-01 21:46:18 -0400

    Update Parsley source
    removed standard in and updated the source to match the parsley library

--- sources/parsley/main.py
+++ sources/parsley/main.py
@@ -8,25 +8,18 @@ import parsley
 class SerialCommunicator:
     def __init__(self, port, baud, timeout):
         self.port = port
-        self.serial = None
-        if self.port != "-":
-            self.serial = serial.Serial(port, baud, timeout)
+        self.serial = serial.Serial(port, baud, timeout=timeout)
 
     def read(self):
-        if self.serial == None:
-            return input
         return self.serial.readline().strip(b'\r\n').decode('utf-8')
 
     def write(self, msg):
-        if self.serial == None:
-            print(msg)
-            return
         self.serial.write(msg)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('port', help='the serial port to read from, or - for stdin')
+    parser.add_argument('port', help='the serial port to read from')
     parser.add_argument('baud', type=int, nargs='?', default=115200,
                         help='the baud rate to use')
     parser.add_argument('--format', default='usb',
@@ -52,27 +45,18 @@ def main():
     while True:
         while msg := receiver.recv_message(0):
             can_msg_data = msg.payload['data']
-            formatted_string = parsley.format_line(can_msg_data)
+            msg_sid, msg_data = parsley.encode_usb_debug(can_msg_data)
 
-
-
-            # msg_sid = msg.payload['msg_sid']
-            # msg_data = msg.payload['msg_data']
-            # formatted_string = parsley.parse(msg_sid, msg_data)
-            # data = msg.payload['data']
-            # length = msg.payload['length']
-            # bit_str = parsley.BitString(data, length)
-            # msg_sid, msg_data = parsley.parse_bitstring(bit_str)
-            # formatted_msg_sid = f"{msg_sid:03X}"
-            # byte_length = (length-11 + 7) // 8  # calculate the number of bytes required
-            # byte_array = msg_data.to_bytes(byte_length, byteorder='big')  # convert to bytes
-            # # convert to hex with commas
-            # formatted_msg_data = ','.join([f"{byte:02X}" for byte in byte_array])
-            # formatted_string = f"m{formatted_msg_sid},{formatted_msg_data};"
-            # communicator.write(formatted_string)
+            formatted_msg_sid = f"{msg_sid:03X}"
+            formatted_msg_data = ','.join([f"{byte:02X}" for byte in msg_data])
+            formatted_string = f"m{formatted_msg_sid},{formatted_msg_data};"
+            if args.solo:
+                print(formatted_string)
+            else:
+                communicator.write(formatted_string)
 
         line = communicator.read()
-        if line is not None or line is input:
+        if not line:
             continue
 
         try:
