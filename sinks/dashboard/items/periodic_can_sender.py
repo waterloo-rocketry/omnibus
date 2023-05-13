ae2f89e2f79a76a047d151ff30d234b80e6c1d2c
Michael Li <michael.li.web@gmail.co>
2023-05-12 23:11:54 -0400

    Found better alternative than QTimer
    now we listen to all can messages (acts as our interrupt function) and then check if the specified peiod has changed, in which case, send the can message.

--- sinks/dashboard/items/periodic_can_sender.py
+++ sinks/dashboard/items/periodic_can_sender.py
@@ -1,12 +1,14 @@
-from pyqtgraph.Qt.QtWidgets import QHBoxLayout, QCheckBox, QComboBox
+from pyqtgraph.Qt.QtWidgets import QHBoxLayout, QCheckBox
 from pyqtgraph.Qt.QtCore import Qt, QTimer
+from pyqtgraph.parametertree.parameterTypes import ListParameter
 
 from .dashboard_item import DashboardItem
 from .registry import Register
 
-from omnibus import Sender
+from omnibus import Sender, Receiver
 import time
 import parsley.message_types as mt
+from parsers import publisher
 
 
 @Register
@@ -16,6 +18,7 @@ class PeriodicCanSender(DashboardItem):
         super().__init__(*args)
 
         self.period = self.parameters.param('period').value()
+        self.actuator = self.parameters.param('actuator').value()
 
         # Specify the layout
         self.layout = QHBoxLayout()
@@ -23,14 +26,6 @@ class PeriodicCanSender(DashboardItem):
 
         self.check = QCheckBox()
         self.layout.addWidget(self.check)
-
-        self.actuator_id = QComboBox()
-        self.actuator_id.addItems(mt.actuator_id.keys())
-        self.actuator_id.setFocusPolicy(Qt.StrongFocus)
-        self.layout.addWidget(self.actuator_id)
-
-        self.message_timer = QTimer()
-        self.message_timer.timeout.connect(self.send_message)
 
         self.pulse_timer = QTimer()
         self.pulse_timer.timeout.connect(self.pulse_widgets)
@@ -41,43 +36,55 @@ class PeriodicCanSender(DashboardItem):
         self.channel = 'CAN/Commands'
 
         self.parameters.param('period').sigValueChanged.connect(self.on_period_change)
+        self.parameters.param('actuator').sigValueChanged.connect(self.on_actuator_change)
+
+        self.last_time = time.time()
+        publisher.subscribe("CAN", self.on_data_update)
+
+    def on_data_update(self, _, __):
+        self.cur_time = time.time()
+        time_elapsed = self.cur_time - self.last_time
+        # send the can message if enough time has passed
+        if time_elapsed > self.period and self.period != 0:
+            can_message = {
+                'data': {
+                    'time': time.time(),
+                    'can_msg': {
+                        'msg_type': 'ACTUATOR_CMD',
+                        'board_id': 'ANY',
+                        'time': 0,
+                        'actuator': self.actuator,
+                        'req_state': 'ACTUATOR_ON' if self.check.isChecked() else 'ACTUATOR_OFF'
+                    }
+                }
+            }
+            print(can_message)
+            self.sender.send(self.channel, can_message)
+            self.pulse_count = 2
+            self.pulse_timer.start(self.pulse_period)
+            self.last_time = self.cur_time
 
     def add_parameters(self):
+        actuator_ids = list(mt.actuator_id.keys())
+        series_param = ListParameter(name='actuator', type='list', default=actuator_ids[0], limits=actuator_ids)
         period_param = {'name': 'period', 'type': 'int', 'value': 0}
-        return [period_param]
-
-    def send_message(self):
-      if self.period == 0:
-          self.message_timer.stop()
-      else:
-          can_message = {
-              'data': {
-                  'time': time.time(),
-                  'can_msg': {
-                      'msg_type': 'ACTUATOR_CMD',
-                      'board_id': 'ANY',
-                      'time': 0,
-                      'actuator': self.actuator_id.currentText(),
-                      'req_state': 'ACTUATOR_ON' if self.check.isChecked() else 'ACTUATOR_OFF'
-                  }
-              }
-          }
-          self.sender.send(self.channel, can_message)
-          self.pulse_count = 2
-          self.pulse_timer.start(self.pulse_period)
+        return [series_param, period_param]
           
     def pulse_widgets(self):
         if self.pulse_count > 0:
-            self.actuator_id.setDisabled(self.pulse_count % 2 == 0)
+            if self.pulse_count % 2 == 0:
+                self.setStyleSheet("background-color: black;")
+            else:
+                self.setStyleSheet("")
             self.pulse_count -= 1
         else:
             self.pulse_timer.stop()
 
-          
-
     def on_period_change(self, _, value):
         self.period = value
-        self.message_timer.start(self.period * 1000)
+
+    def on_actuator_change(self, _, value):
+        self.actuator = value
 
     @staticmethod
     def get_name():
