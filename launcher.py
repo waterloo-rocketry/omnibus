bc77d478485439fbb8f632f786225ede59e734a3
Ozayr Raazi <oraazi2004@gmail.com>
2023-09-23 00:51:01 -0400

    Rewrote launcher to dynamically get all sources and sinks

--- launcher.py
+++ launcher.py
@@ -1,93 +1,67 @@
-from subprocess import Popen, PIPE
-from sys import platform
-from os import kill 
-import time, signal
+import os
+import signal
+import subprocess
+import sys
+import time
 
-if platform == "win32":
+# Some specific commands are needed for Windows vs macOS/Linux
+if sys.platform == "win32":
     from subprocess import CREATE_NEW_PROCESS_GROUP
 
+# Parse folders for sources and sinks
+modules = {"sources" : os.listdir('sources'), "sinks" : os.listdir('sinks')}
 
-# Profiles for what parts of Omnibus should be run
-# Files inside each profile should be listed 
-# IN ORDER of how they should be run 
-profiles = {
-    "Test": [
-        ['python', '-m', 'omnibus'],
-        ['python', 'sources/fakeni/main.py'],
-        ['python', 'sinks/dashboard/main.py']
-    ],
-    "DAQ": [
-        ['python', '-m', 'omnibus'],
-        ['python', 'sources/ni/main.py'],
-    ],
-    "Dashboard": [
-        ['python', 'sinks/dashboard/main.py']
-    ],
-    "Log + Dashboard": [
-        ['python', 'sinks/globallog/main.py'],
-        ['python', 'sinks/dashboard/main.py']
-    ]
-    # Add new profiles here
-}
+for module in modules.keys():
+    print(f"{module.capitalize()}:")
+    for i, item in enumerate(modules[module]):
+        print(f"\t{i+1}. {item.capitalize()}")
 
-# Presents the user with a list of all available 
-# profiles and prompts them to choose one
-print("The following profiles are available to run Omnibus:\n")
+# Construct CLI commands to start Omnibus
+omnibus = ["python", "-m", "omnibus"]
+source_selection = input(f"\nPlease enter your Source choice [1-{len(modules['sources'])}]: ")
+sink_selection = input(f"Please enter your Sink choice [1-{len(modules['sinks'])}]: ")
+source = ["python", f"sources/{modules['sources'][int(source_selection) - 1]}/main.py"]
+sink = ["python", f"sinks/{modules['sinks'][int(sink_selection) - 1]}/main.py"]
 
-for i, profile in enumerate(profiles):
-    print(f"\t{i+1}. {profile}")
-print()
+commands = [omnibus, source, sink]
+processes = []
+print("Launching... ", end="")
 
-# Keep asking for input if invalid entry is given
-selection = 0
-while selection < 1 or selection > i+1:
-    selection = input(f"Please enter the number corresponding to your choice [1-{i+1}]: ")
-    try:
-        selection = int(selection)
-    except ValueError:
-        selection = 0
-
-profile = profiles[list(profiles)[selection-1]]
-p = []
-print(f"Running Omnibus with selection {selection}...")
-
-# Run every file in the background
-for i in range(len(profile)):
-    # This distinction is necessary to properly 
-    # exit the subprocesses on Windows
-    if platform == "win32":
-        process = Popen(profile[i], stdout=PIPE, stderr=PIPE, creationflags=CREATE_NEW_PROCESS_GROUP)
+# Execute commands as subprocesses
+for command in commands:
+    if sys.platform == "win32":
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=CREATE_NEW_PROCESS_GROUP)
+        time.sleep(0.5)
     else:
-        process = Popen(profile[i], stdout=PIPE, stderr=PIPE)
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        time.sleep(0.5)
+    processes.append(process)
 
-    p.append(process)
+print("Done!")
 
-    # Time delay cause CPU too fast and Omnibus 
-    # needs time to setup communication channels
-    time.sleep(0.5)
-
+# Blank exception just for processes to throw
 class Finished(Exception):
     pass
 
-# If any file exits or the user presses control + c
+# If any file exits or the user presses control + c,
 # terminate all other files that are running
 try:
     while True:
-        for process in p:
+        for process in processes:
             if process.poll() != None:
                 raise Finished
 except (Finished, KeyboardInterrupt, Exception):
-    for i in p:
-        if platform == "win32":
-            kill(i.pid, signal.CTRL_C_EVENT)
+    for process in processes:
+        if sys.platform == "win32":
+            os.kill(process.pid, signal.CTRL_C_EVENT)
         else:
-            i.send_signal(signal.SIGINT)
+            process.send_signal(signal.SIGINT)
 
-        output, err = i.communicate()
+        output, err = process.communicate()
         output, err = output.decode(), err.decode()
-        print(f"\nOutput from {i.args}:")
+        print(f"\nOutput from {process.args}:")
         print(output)
 
         if err and "KeyboardInterrupt" not in err:
-            print(f"\nError from {i.args}:")
+            print(f"\nError from {process.args}:")
             print(err)
