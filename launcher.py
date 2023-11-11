3b45d9cd3532b79e8609acc8ae266a77b00e61f2
wxp02 <wxp0025@gmail.com>
2023-11-10 23:41:32 -0500

    Restructure v1

--- launcher.py
+++ launcher.py
@@ -5,23 +5,108 @@ import sys
 import time
 import argparse
 import sys
+import logging
+from logtool import Logger
 from pyqtgraph.Qt import QtGui
 from pyqtgraph.Qt.QtWidgets import QApplication, QDialog, QLabel, QComboBox, QDialogButtonBox, QVBoxLayout
 
 # Some specific commands are needed for Windows vs macOS/Linux
 if sys.platform == "win32":
     from subprocess import CREATE_NEW_PROCESS_GROUP
+    python_executable = "venv/Scripts/python"
+else:
+    python_executable = "python"
 
 # Parse folders for sources and sinks
 modules = {"sources" : os.listdir('sources'), "sinks" : os.listdir('sinks')}
 
-for module in modules.keys():
-    print(f"{module.capitalize()}:")
-    for i, item in enumerate(modules[module]):
-        print(f"\t{i+1}. {item.capitalize()}")
+# Blank exception just for processes to throw
+class Finished(Exception):
+    pass
+
+# CLI Launcher
+class Launcher(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        # Remove dot files
+        for module in modules.keys():
+            for item in modules[module]:
+                if item.startswith("."):
+                    modules[module].remove(item)
+
+        for module in modules.keys():
+            print(f"{module.capitalize()}:")
+            for i, item in enumerate(modules[module]):
+                print(f"\t{i+1}. {item.capitalize()}")
+
+        # Construct CLI commands to start Omnibus
+        source_selection = input(f"\nPlease enter your Source choice [1-{len(modules['sources'])}]: ")
+        sink_selection = input(f"Please enter your Sink choice [1-{len(modules['sinks'])}]: ")
+        omnibus = [python_executable, "-m", "omnibus"]
+        source = [python_executable, f"sources/{modules['sources'][int(source_selection) - 1]}/main.py"]
+        sink = [python_executable, f"sinks/{modules['sinks'][int(sink_selection) - 1]}/main.py"]
+
+        # Create loggers
+        logger = Logger()
+        logger.add_logger(f"sources/{modules['sources'][int(source_selection) - 1]}")
+        logger.add_logger(f"sinks/{modules['sinks'][int(sink_selection) - 1]}")
+        print("Loggers Initiated")
+
+        commands = [omnibus, source, sink]
+        processes = []
+        print("Launching... ", end="")
+
+        # Execute commands as subprocesses
+        for command in commands:
+            if sys.platform == "win32":
+                process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, 
+                                        creationflags=CREATE_NEW_PROCESS_GROUP)
+                time.sleep(0.5)
+            else:
+                process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                time.sleep(0.5)
+            processes.append(process)
+
+        print("Done!")
+
+    # If any file exits or the user presses control + c,
+    # terminate all other files that are running
+    def terminate(self):
+        try:
+            while True:
+                for process in processes:
+                    if process.poll() != None:
+                        raise Finished
+        except (Finished, KeyboardInterrupt, Exception):
+            for process in processes:
+                if sys.platform == "win32":
+                    os.kill(process.pid, signal.CTRL_BREAK_EVENT)
+                else:
+                    process.send_signal(signal.SIGINT)
+
+                # Dump output and error (if exists) from every
+                # process to the coresponding log file
+                output, err = process.communicate()
+                output, err = output.decode(), err.decode()
+                
+                # Log outputs
+                logger.log_output(process, output)
+
+                # Log errors
+                if err and "KeyboardInterrupt" not in err:
+                    logger.log_error(process, err)
+                    
+            logging.shutdown()
+        finally:
+            for process in processes:
+                if sys.platform == "win32":
+                    os.kill(process.pid, signal.CTRL_BREAK_EVENT)
+                else:
+                    process.send_signal(signal.SIGINT)        
 
 # GUI Launcher
-class Launcher(QDialog):
+class GUILauncher(Launcher, QDialog):
     def __init__(self):
         super().__init__()
 
@@ -46,14 +131,8 @@ class Launcher(QDialog):
         self.source_dropdown.setGeometry(90, 52, 150, 20)
 
         # Add items to the sources dropdown
-        self.source_dropdown.addItem("Ni")
-        self.source_dropdown.addItem("Replay_log")
-        self.source_dropdown.addItem("Pipe_output")
-        self.source_dropdown.addItem("Parsley")
-        self.source_dropdown.addItem("Rlcs")
-        self.source_dropdown.addItem("Fakeni")
-        self.source_dropdown.addItem("Fake_parsley")
-        self.source_dropdown.addItem("Payload_fake")
+        for source in modules.get("sources"):
+            self.source_dropdown.addItem(source)
 
         # Create a sink label
         sink = QLabel(self)
@@ -64,12 +143,9 @@ class Launcher(QDialog):
         self.sink_dropdown = QComboBox(self)
         self.sink_dropdown.setGeometry(90, 92, 150, 20)
 
-        # Add items to the sources dropdown
-        self.sink_dropdown.addItem("Txtconsole")
-        self.sink_dropdown.addItem("Printer")
-        self.sink_dropdown.addItem("Dashboard")
-        self.sink_dropdown.addItem("Gpsd")
-        self.sink_dropdown.addItem("Globallog")
+        # Add items to the sinks dropdown
+        for sink in modules.get("sinks"):
+            self.sink_dropdown.addItem(sink)
 
         # Enter selections button
         self.button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok)
@@ -87,8 +163,8 @@ class Launcher(QDialog):
         selected_sink = self.sink_dropdown.currentText()
 
         omnibus = ["python", "-m", "omnibus"]
-        source = ["python", f"sources/{selected_source.lower()}/main.py"]
-        sink = ["python", f"sinks/{selected_sink.lower()}/main.py"]
+        source = ["python", f"sources/{selected_source}/main.py"]
+        sink = ["python", f"sinks/{selected_sink}/main.py"]
         commands = [omnibus, source, sink]
         processes = []
         print("Launching... ", end="")
@@ -96,91 +172,25 @@ class Launcher(QDialog):
         # Close the window
         self.close()
 
-        for command in commands:
-            if sys.platform == "win32":
-                process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, 
-                                        creationflags=CREATE_NEW_PROCESS_GROUP)
-                time.sleep(0.5)
-            else:
-                process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                time.sleep(0.5)
-            processes.append(process)
-
-        print("Done!")
-
 def main():
     parser = argparse.ArgumentParser(description='Omnibus Launcher')
     parser.add_argument('--text', action='store_true', help='Use text input mode')
-
     args = parser.parse_args()
+
+    app = QApplication(sys.argv)
 
     # If 'python launcher.py --text' is run this block of code will execute
     if args.text:
-        # Construct CLI commands to start Omnibus
-        omnibus = ["python", "-m", "omnibus"]
-        source_selection = input(f"\nPlease enter your Source choice [1-{len(modules['sources'])}]: ")
-        sink_selection = input(f"Please enter your Sink choice [1-{len(modules['sinks'])}]: ")
-        source = ["python", f"sources/{modules['sources'][int(source_selection) - 1]}/main.py"]
-        sink = ["python", f"sinks/{modules['sinks'][int(sink_selection) - 1]}/main.py"]
-        commands = [omnibus, source, sink]
-        processes = []
-        print("Launching... ", end="")
-
-        # Execute commands as subprocesses
-        for command in commands:
-            if sys.platform == "win32":
-                process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, 
-                                        creationflags=CREATE_NEW_PROCESS_GROUP)
-                time.sleep(0.5)
-            else:
-                process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                time.sleep(0.5)
-            processes.append(process)
-
-        print("Done!")
-
-        # If any file exits or the user presses control + c,
-        # terminate all other files that are running
-        try:
-            while True:
-                for process in processes:
-                    if process.poll() != None:
-                        raise Finished
-        except (Finished, KeyboardInterrupt, Exception):
-            for process in processes:
-                if sys.platform == "win32":
-                    os.kill(process.pid, signal.CTRL_C_EVENT)
-                else:
-                    process.send_signal(signal.SIGINT)
-
-                # Dump output and error (if exists) from every
-                # process to the shell 
-                output, err = process.communicate()
-                output, err = output.decode(), err.decode()
-                print(f"\nOutput from {process.args}:")
-                print(output)
-
-                if err and "KeyboardInterrupt" not in err:
-                    print(f"\nError from {process.args}:")
-                    print(err)
-        finally:
-            for process in processes:
-                if sys.platform == "win32":
-                    os.kill(process.pid, signal.CTRL_C_EVENT)
-                else:
-                    process.send_signal(signal.SIGINT)
-
+        print("Running in text mode")
+        launcher = Launcher()
+        launcher.terminate()
 
     # If 'python launcher.py' is run this this block of code will execute
     else:
-        app = QApplication(sys.argv)
-        window = Launcher()
-        window.show()
+        print("Running in GUI mode")
+        gui_launcher = GUILauncher()
+        gui_launcher.show()
         sys.exit(app.exec())
 
 if __name__ == '__main__':
     main()
-
-# Blank exception just for processes to throw
-class Finished(Exception):
-    pass
