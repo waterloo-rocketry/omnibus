ecd4a7e2fbc1b441d0e9435b032ec00484b21207
Xin <xhwong@uwaterloo.ca>
2023-11-15 22:25:06 -0500

    allow for multiple sink/source selection. user enters multiple sink/sources separated by a space. not working from after launching

--- launcher.py
+++ launcher.py
@@ -17,6 +17,7 @@ if sys.platform == "win32":
 else:
     python_executable = "python"
 
+<<<<<<< HEAD
 # Blank exception just for processes to throw
 class Finished(Exception):
     pass
@@ -51,13 +52,52 @@ class Launcher():
         self.omnibus = [python_executable, "-m", "omnibus"]
         self.source = [python_executable, f"sources/{self.modules['sources'][self.source_selection]}/main.py"]
         self.sink = [python_executable, f"sinks/{self.modules['sinks'][self.sink_selection]}/main.py"]
+=======
+# Parse folders for sources and sinks
+modules = {"sources": os.listdir('sources'), "sinks": os.listdir('sinks')}
 
-# Create loggers
-logger = Logger()
-logger.add_logger(f"sources/{modules['sources'][int(source_selection) - 1]}")
-logger.add_logger(f"sinks/{modules['sinks'][int(sink_selection) - 1]}")
-print("Loggers Initiated")
+# Remove dot files
+for module in modules.keys():
+    for item in modules[module]:
+        if item.startswith("."):
+            modules[module].remove(item)
 
+for module in modules.keys():
+    print(f"{module.capitalize()}:")
+    for i, item in enumerate(modules[module]):
+        print(f"\t{i+1}. {item.capitalize()}")
+
+#display the options selectable
+def displayOptions(moduleName, moduleList):
+    print(f"{moduleName.capitalize()}:")
+    for idx, item in enumerate(moduleList):
+        print(f"\t{i+1}. {item.capitalize()}")
+
+#arrays to store the user selection 
+srcSelected=[]
+sinkSelected=[]
+
+
+
+
+# Construct CLI commands to start Omnibus [PREVIOUS CODE, ONLY ACCEPTS ONE SOURCE/SINK]
+# remember to check for 0 inputs and handle accordingly 
+source_selection = input(f"\nPlease enter your Source choice [1-{len(modules['sources'])}]: ")
+sink_selection = input(f"Please enter your Sink choice [1-{len(modules['sinks'])}]: ")
+
+#process the source/sink_selection to see how many were selected 
+sources=source_selection.split()
+srcSelected=[int(item) for item in sources]
+print(srcSelected)
+
+sinks=sink_selection.split()
+sinkSelected=[int(item) for item in sinks]
+print(sinkSelected)
+>>>>>>> fbbc538 (allow for multiple sink/source selection. user enters multiple sink/sources separated by a space. not working from after launching)
+
+commands=[]
+
+<<<<<<< HEAD
 commands = [omnibus, source, sink]
 processes = []
 print("Launching... ", end="")
@@ -192,40 +232,99 @@ class GUILauncher(Launcher, QDialog):
     def closeEvent(self, event):
         if self.selected_ok:
             event.accept()
+=======
+if srcSelected:
+    omnibus = [python_executable, "-m", "omnibus"]
+    for selection in srcSelected:
+        source=[python_executable, f"sources/{modules['sources'][selection - 1]}/main.py"]
+        commands.append([omnibus, source])
+
+if sinkSelected:
+    for selection in sinkSelected:
+        sink = [python_executable, f"sinks/{modules['sinks'][selection - 1]}/main.py"]
+        commands.append([sink])
+#omnibus = [python_executable, "-m", "omnibus"]
+#source = [python_executable, f"sources/{modules['sources'][int(source_selection) - 1]}/main.py"]
+#sink = [python_executable, f"sinks/{modules['sinks'][int(sink_selection) - 1]}/main.py"]
+#commands = [omnibus, source, sink]
+processes = []
+print("Launching... ", end="")
+
+
+#if source_selection !="0":
+    #omnibus = [python_executable, "-m", "omnibus"]
+    #source = [python_executable, f"sources/{modules['sources'][int(source_selection) - 1]}/main.py"]
+    #commands = [omnibus, source]
+
+#if sink_selection !='0':
+    #sink = [python_executable, f"sinks/{modules['sinks'][int(sink_selection) - 1]}/main.py"]
+    #commands.append(sink)
+
+
+
+processes = [] 
+print("Launching... ", end="")
+
+    # Execute commands as subprocesses
+    def subprocess(self):
+        self.processes = []
+        print("Launching... ", end="")
+        for command in self.commands:
+            if sys.platform == "win32":
+                process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, 
+                                        creationflags=CREATE_NEW_PROCESS_GROUP)
+                time.sleep(0.5)
+            else:
+                process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                time.sleep(0.5)
+            self.processes.append(process)
+
+        print("Done!")
+    
+    # Create loggers
+    def logging(self):
+        self.logger = Logger()
+        self.logger.add_logger(f"sources/{self.modules['sources'][int(self.source_selection)]}")
+        self.logger.add_logger(f"sinks/{self.modules['sinks'][int(self.sink_selection)]}")
+        print("Loggers Initiated")
+
+# If any file exits or the user presses control + c,
+# terminate all other files that are running
+try:
+    while True:
+        for process in processes:
+            if process.poll() != None:
+                raise Finished
+except (Finished, KeyboardInterrupt, Exception):
+    for process in processes:
+        if sys.platform == "win32":
+            os.kill(process.pid, signal.CTRL_BREAK_EVENT)
+>>>>>>> fbbc538 (allow for multiple sink/source selection. user enters multiple sink/sources separated by a space. not working from after launching)
         else:
-            sys.exit()
+            process.send_signal(signal.SIGINT)
 
-def main():
-    parser = argparse.ArgumentParser(description='Omnibus Launcher')
-    parser.add_argument('--text', action='store_true', help='Use text input mode')
-    args = parser.parse_args()
+                # Dump output and error (if exists) from every
+                # process to the coresponding log file
+                output, err = process.communicate()
+                output, err = output.decode(), err.decode()
+                
+                # Log outputs
+                self.logger.log_output(process, output)
 
-    app = QApplication(sys.argv)
-
-    # If 'python launcher.py --text' is run this block of code will execute
-    if args.text:
-        print("Running in text mode")
-        launcher = Launcher()
-        launcher.print_choices()
-        launcher.input()
-        launcher.subprocess()
-        launcher.logging()
-        launcher.terminate()
-
-    # If 'python launcher.py' is run this this block of code will execute
-    else:
-        print("Running in GUI mode")
-        gui_launcher = GUILauncher()
-        gui_launcher.show()
-        app.exec()
-        gui_launcher.subprocess()
-        gui_launcher.logging()
-        gui_launcher.terminate()
-
-if __name__ == '__main__':
-    main()
+        # Log errors
+        if err and "KeyboardInterrupt" not in err:
+            logger.log_error(process, err)
+            
+    logging.shutdown()
+finally:
+    for process in processes:
+        if sys.platform == "win32":
+            os.kill(process.pid, signal.CTRL_BREAK_EVENT)
+        else:
+            process.send_signal(signal.SIGINT)        
 
 '''
 Questions:
 -how does the launcher work? is it able to run independently on its own? 
+-how does the user select more than one sources/sink 
 '''
