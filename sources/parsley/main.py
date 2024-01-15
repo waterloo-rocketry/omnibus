ff39e811a9e615340e698a0f17a397a7ed0de9be
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2024-01-14 19:04:29 -0500

    correct formatting for fake messages, clean up

--- sources/parsley/main.py
+++ sources/parsley/main.py
@@ -29,7 +29,7 @@ class SerialCommunicator:
     def write(self, msg):
         self.serial.write(msg)
 
-class FakeSerialCommunicator:
+class FakeSerialCommunicator: # acting as a fake usb debug board
     def __init__(self):
         # fake messages to cycle through
         self.fake_msgs = [
@@ -55,59 +55,28 @@ class FakeSerialCommunicator:
             if "value" in self.fake_msgs[self.fake_msg_index]:
                 self.fake_msgs[self.fake_msg_index]["value"] = random.randint(0, 10)
 
-            # turn the fake message from the dict to the bytes representation that would be read from the serial connection
-            # print(self.fake_msg_index, self.fake_msgs[self.fake_msg_index])
-            formatted_msg = prepCanMessage(self.fake_msgs[self.fake_msg_index])
+            # turn the fake message from the dict to the bytes representation that would be read from the serial connection, like from the USB debug board
+            msg_sid, msg_data = parsley.encode_data(self.fake_msgs[self.fake_msg_index])
+            formatted_msg = f"{msg_sid:03X}"
+            if msg_data:
+                formatted_msg += ':' + ','.join(f"{byte:02X}" for byte in msg_data) # debug messages have a colon between the sid and data, see https://github.com/waterloo-rocketry/cansw_usb/blob/1575995e9364bca99443362ff51a5311a8a10174/usb_app.c#L99
+            formatted_msg = f"${formatted_msg} \0\r\n"
 
             # update the index after encoding
             self.fake_msg_index = (self.fake_msg_index + 1) % len(self.fake_msgs)
             if self.fake_msg_index == 0:
                 self.last_fake_zero_time = now
 
-            formatted_msg += '\n'
-
             return formatted_msg.encode(encoding='utf-8')
-            # FIXME: this doesnt correctly encode the message, i'm getting Incorrect line format when I add a \n, or it's just never decoding anything if i dont add the \n
-            # It would be good to have an example of how the real devices encode the data, or closer to real so that we can match the template
-        
-            # also just lost on what formats are for what, they all give different errors. logger gives
-            # m6A1,1B,14,02,00,09;69
-            # invalid literal for int() with base 16: 'm52'
-            # m521,00,1B,14,00;C6
-            # invalid literal for int() with base 16: 'm46'
-            # m461,00,1B,14,01,01,02;B5
-            # invalid literal for int() with base 16: 'm52'
-        
-            # Ex if i add \n at the end in default format: Incorrect line format
-            # m461,00,07,EC,01,01,02;A2
-            # m52E,00,07,EC,00;12
-            # Incorrect line format
-            # m52E,00,07,EC,00;12
-        
-            # Ex: telemetry mode passes through a bit, but then can't find the 0x02 in this line       i = next((i for i, b in enumerate(buffer) if b == 0x02), -1) and gets -1
-        
-            # so what's the correct way to format it? currently my output is Buffer: b'm6AE,00,C0,01,00,01;64\n'
-
         else:
             return b''
 
     def write(self,msg):
         print(f"Fake serial write out: {msg}")
 
-
-def prepCanMessage(can_msg_data):
-    msg_sid, msg_data = parsley.encode_data(can_msg_data)
-    formatted_msg = f"m{msg_sid:03X}"
-    if msg_data:
-        formatted_msg += ',' + ','.join(f"{byte:02X}" for byte in msg_data)
-    formatted_msg += ";" + crc8.crc8(
-        msg_sid.to_bytes(2, byteorder='big') + bytes(msg_data)
-    ).hexdigest().upper()
-    return formatted_msg
-
 def main():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('port', help='the serial port to read from')
+    argparser.add_argument('port', type=str, nargs='?', default="NOPORT", help='the serial port to read from, not needed for fake mode')
     argparser.add_argument('baud', type=int, nargs='?', default=115200,
                         help='the baud rate to use')
     argparser.add_argument('--format', default='usb',
@@ -118,10 +87,18 @@ def main():
                         help="Don't read from hardware - uses fake data. Give any value for a port")
     args = argparser.parse_args()
 
+
     if not args.fake:
+        if args.port == "NOPORT":
+            print("Please specify a serial port by name or use --fake")
+            exit(1)
         communicator = SerialCommunicator(args.port, args.baud, 0)
     else:
+        if args.format != "usb":
+            print("Fake mode only supports usb format")
+            exit(1)
         communicator = FakeSerialCommunicator()
+        
     
     parser = parsley.parse_usb_debug
     if args.format == "telemetry":
@@ -166,7 +143,13 @@ def main():
 
         if receiver and (msg := receiver.recv_message(0)):  # non-blocking
             can_msg_data = msg.payload['data']['can_msg']
-            formatted_msg = prepCanMessage(can_msg_data)
+            msg_sid, msg_data = parsley.encode_data(can_msg_data)
+            formatted_msg = f"m{msg_sid:03X}"
+            if msg_data:
+                formatted_msg += ',' + ','.join(f"{byte:02X}" for byte in msg_data)
+            formatted_msg += ";" + crc8.crc8(
+                msg_sid.to_bytes(2, byteorder='big') + bytes(msg_data)
+            ).hexdigest().upper() # sent messages in the usb debug format have a crc8 checksum at the end, to be investigated: https://github.com/waterloo-rocketry/omnibus/commit/0913ff2ef1c38c3ae715ad87c805d071c1ce2c38 
             print(formatted_msg)  # always print the usb debug style can message
             # send the can message over the specified port
             communicator.write(formatted_msg.encode())
@@ -174,7 +157,6 @@ def main():
             time.sleep(0.01)
 
         line = communicator.read()
-        # print(f"Read in: {line}")
 
         if not line:
             time.sleep(0.01)
@@ -183,18 +165,15 @@ def main():
         buffer += line
 
         while True:
-            print(f"Buffer: {buffer}")
             try:
                 if args.format == "telemetry":
                     i = next((i for i, b in enumerate(buffer) if b == 0x02), -1)
-                    print(f"i: {i}")
                     if i < 0 or i + 1 >= len(buffer):
                         break
                     msg_len = buffer[i+1] >> 4
                     if i + msg_len > len(buffer):
                         break
                     msg = buffer[i:i+msg_len]
-                    print(f"Telemetry buffer: {msg}")
                     try:
                         msg_sid, msg_data = parser(msg)
                         buffer = buffer[i+msg_len:]
@@ -208,7 +187,6 @@ def main():
                         break
                     msg = text_buff[:i]
                     buffer = buffer[i+1:]
-                    # print(f"Text buffer: {msg}")
                     msg_sid, msg_data = parser(msg)
                 
                 parsed_data = parsley.parse(msg_sid, msg_data)
