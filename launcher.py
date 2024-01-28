8d3cffd2eeabcea9281b7b1e84e823883ffd5760
Xin Huey Wong <90855887+xinhuey@users.noreply.github.com>
2024-01-29 04:48:24 +0800

    xhwong/142 multiple source sink launcher (#194)
    * allow 0 for source selection
    
    * allow for multiple sink/source selection. user enters multiple sink/sources separated by a space. not working from after launching
    
    * updated with the logging feature. rn the bus doesnt run right with the multiple inputs
    
    * commit to rebase
    
    * allow 0 for source selection
    
    * allow for multiple sink/source selection. user enters multiple sink/sources separated by a space. not working from after launching
    
    * updated with the logging feature. rn the bus doesnt run right with the multiple inputs
    
    * commit to rebase
    
    * subprocesses run for multiple sources/sink, but program hangs after done.
    
    * subprocesses run for multiple sinks/sources, but getting an error saying the omnibus module is not found
    
    * continuously checks if all processes are terminated or not, goes in an infinite loop
    
    * says its logged but there's no actual log file output
    
    * Log files are being displayed
    
    * cleaned up code a bit
    
    * old launcher file, log files not being generated
    
    * allow 0 for source selection
    
    * allow for multiple sink/source selection. user enters multiple sink/sources separated by a space. not working from after launching
    
    * updated with the logging feature. rn the bus doesnt run right with the multiple inputs
    
    * commit to rebase
    
    * subprocesses run for multiple sources/sink, but program hangs after done.
    
    * allow 0 for source selection
    
    * allow for multiple sink/source selection. user enters multiple sink/sources separated by a space. not working from after launching
    
    * updated with the logging feature. rn the bus doesnt run right with the multiple inputs
    
    * commit to rebase
    
    * subprocesses run for multiple sinks/sources, but getting an error saying the omnibus module is not found
    
    * continuously checks if all processes are terminated or not, goes in an infinite loop
    
    * says its logged but there's no actual log file output
    
    * Log files are being displayed
    
    * cleaned up code a bit
    
    * old launcher file, log files not being generated
    
    * no log files generated for error processes
    
    * updated launcher.py
    
    * Added validation checking for source/sink selection
    
    * Added validation checking for source/sink selection
    
    * integrated for cli. now able to accept multiple sink/sources for cli input
    
    * implemented the checkbox input for multiple sources/sinks, having type errors in construct_commands
    
    * Implemented multiple source/sink selection for both GUI and CLI inputs. Logging works correctly. Ensured the description label is fully visible
    
    * Changed the imports from PyQt5 to PySide6
    
    * Changed the imports from PySide6 to just pyqtgraph
    
    * fixed the parsing error for the user's selection
    
    * removed all debug statements and old code from the launcher
    
    * removed all debug statements and old code from the launcher
    
    * wip for implementing checkboxes, not able to put user selection into another list for processing
    
    * fully implemented allowing the multiple input selection using checkboxes, found in launcher_checkBox.py
    
    * need to remove extra spacing and numbers
    
    * capitalised the source and sinks
    
    * removed the numbers for all the options
    
    * removed the spacing between the labels and gridlayout
    
    * cleaned up the code for launcher.py
    
    * removed testing files
    
    * removed some comments and unnecessary spacing, put the validation code into a separate function that can be called when needed
    
    * created a separate function to construct commands for the cli input
    
    * removed unnecessary blank lines
    
    * updated code according to pr review
    
    * removed stale files
    
    * removed tempCodeRunnerFile.py
    
    ---------
    
    Co-authored-by: Xin <xhwong@uwaterloo.ca>
    Co-authored-by: Ozayr Raazi <67076803+Darth-Raazi@users.noreply.github.com>

--- launcher.py
+++ launcher.py
@@ -8,8 +8,10 @@ import sys
 import logging
 from logtool import Logger
 from pyqtgraph.Qt import QtGui
-from pyqtgraph.Qt.QtWidgets import QApplication, QDialog, QLabel, QComboBox, QDialogButtonBox, QVBoxLayout
-
+from pyqtgraph.Qt.QtWidgets import (
+    QApplication, QDialog, QLabel, QDialogButtonBox, QVBoxLayout,
+    QWidget, QCheckBox, QGridLayout
+)
 # Some specific commands are needed for Windows vs macOS/Linux
 if sys.platform == "win32":
     from subprocess import CREATE_NEW_PROCESS_GROUP
@@ -30,6 +32,9 @@ class Launcher():
     def __init__(self) -> None:
         super().__init__()
         self.commands = []
+        self.src_selected = []
+        self.sink_selected = []
+        self.processes = []
 
         # Parse folders for sources and sinks
         self.modules = {"sources": os.listdir('sources'), "sinks": os.listdir('sinks')}
@@ -50,21 +55,67 @@ class Launcher():
     # Enter inputs for CLI launcher
     def input(self):
         # Construct CLI commands to start Omnibus
-        self.source_selection = int(
-            input(f"\nPlease enter your Source choice [1-{len(self.modules['sources'])}]: ")) - 1
-        self.sink_selection = int(
-            input(f"Please enter your Sink choice [1-{len(self.modules['sinks'])}]: ")) - 1
-        self.omnibus = [python_executable, "-m", "omnibus"]
-        self.source = [python_executable,
-                       f"sources/{self.modules['sources'][self.source_selection]}/main.py"]
-        self.sink = [python_executable,
-                     f"sinks/{self.modules['sinks'][self.sink_selection]}/main.py"]
 
-        self.commands = [self.omnibus, self.source, self.sink]
+        #Source selection
+        self.src_selected = self.validate_inputs(self.modules['sources'], "Source")
+        
+        #Sink selection 
+        self.sink_selected = self.validate_inputs(self.modules['sinks'], "Sink")
+
+        #Command construction
+        omnibus = [python_executable, "-m", "omnibus"]
+        self.commands.append(omnibus)
+        self.construct_commands_cli(self.src_selected, self.sink_selected)
+       
+
+    #Construct commands for the selection
+    def construct_commands_cli(self, src_list, sink_list):
+        if src_list:
+            for selection in src_list:
+                source=[python_executable, f"sources/{self.modules['sources'][selection - 1]}/main.py"]
+                self.commands.append(source)
+
+        if sink_list:
+            for selection in sink_list:
+                sink = [python_executable, f"sinks/{self.modules['sinks'][int(selection) - 1]}/main.py"]
+                self.commands.append(sink)
+        
+        return self.commands
+
+    #Validate the input selection for cli input
+    def validate_inputs(self, choices, module):
+        selected_indices=[]
+        while True:
+            user_input = input(f"\nPlease enter your {module} choices [1-{len(choices)}] separated by spaces: ")
+
+            #Split the input string into individual values
+            selection = user_input.split()
+
+            #Validate the input 
+            valid_input = True
+            
+            for item in selection:
+                #Check if the input is a number
+                if not item.isdigit():
+                    print(f"Invalid input: '{item}' is not a number.")
+                    valid_input = False
+                    break
+                
+                #Check if the index is within the choice range 
+                index = int(item)
+                if 1 <= index <= len(choices):
+                    selected_indices.append(index)
+                else:
+                    print(f"Please enter a number between 1 and {len(choices)}.")
+                    valid_input = False
+                    selected_indices.clear()
+                    break
+            if valid_input:
+                break
+        return selected_indices
 
     # Execute commands as subprocesses
     def subprocess(self):
-        self.processes = []
         print("Launching... ", end="")
         for command in self.commands:
             if sys.platform == "win32":
@@ -81,8 +132,10 @@ class Launcher():
     # Create loggers
     def logging(self):
         self.logger = Logger()
-        self.logger.add_logger(f"sources/{self.modules['sources'][int(self.source_selection)]}")
-        self.logger.add_logger(f"sinks/{self.modules['sinks'][int(self.sink_selection)]}")
+        for src in self.src_selected:
+            self.logger.add_logger(f"sources/{self.modules['sources'][src - 1]}")
+        for sink in self.sink_selected:
+            self.logger.add_logger(f"sinks/{self.modules['sinks'][sink - 1]}")
         print("Loggers Initiated")
 
     # If any file exits or the user presses control + c,
@@ -120,77 +173,122 @@ class Launcher():
                 else:
                     process.send_signal(signal.SIGINT)
 
+
 # GUI Launcher
-
-
 class GUILauncher(Launcher, QDialog):
     def __init__(self):
         super().__init__()
         self.selected_ok = False
 
-        # Sets window title and ensures size of dialog is fixed
         self.setGeometry(300, 300, 500, 230)
-        self.setFixedSize(500, 230)
+        self.setFixedSize(500, 400)
         self.setWindowTitle("Omnibus Launcher")
 
-        # Description / Title
-        description = QLabel(self)
-        description.setText("Please enter your source and sink choices")
-        description.setGeometry(20, 12, 400, 20)
-        description.setFont(QtGui.QFont("", 18))
+        source_label = QLabel(self)
+        source_label.setText("Sources:")
+        source_label.setGeometry(20, 10, 50, 10)
 
-        # Create a source label
-        source = QLabel(self)
-        source.setText("Source:")
-        source.setGeometry(20, 53, 150, 20)
+        #Create checkboxes for each source option
+        sources = self.modules.get("sources")
+        up_source = [source.capitalize() for source in sources]
+        self.src_dict = {source: i + 1 for i, source in enumerate(up_source)}
+        self.src_checkboxes = [QCheckBox(f"{src}") for src in up_source]
+        self.src_selected = []
+        
+        #Layout for source checkboxes
+        self.src_layout = QGridLayout()
+        row = 0
+        col = 0
+        for checkbox in self.src_checkboxes:
+            self.src_layout.addWidget(checkbox, row, col)
+            col += 1
+            if col == 3:  # Three checkboxes per row
+                col = 0
+                row += 1
 
-        # Create a dropdown for source
-        self.source_dropdown = QComboBox(self)
-        self.source_dropdown.setGeometry(90, 52, 150, 30)
+        source_list = QWidget()
+        source_list.setLayout(self.src_layout)
+        source_list.setContentsMargins(0, 0, 0, 0)
 
-        # Add items to the sources dropdown
-        for source in self.modules.get("sources"):
-            self.source_dropdown.addItem(source)
+        #Connect checkbox state to signals to detect which sources were selected 
+        for checkbox in self.src_checkboxes:
+            checkbox.stateChanged.connect(self.update_selected)
 
-        # Create a sink label
+        #Create a sink label
         sink = QLabel(self)
-        sink.setText("Sink:")
-        sink.setGeometry(20, 93, 150, 20)
+        sink.setText("Sinks:")
+        sink.setGeometry(20, 200, 50, 10)
+        sink.setContentsMargins(0,0,0,0)
 
-        # Create a dropdown for sink
-        self.sink_dropdown = QComboBox(self)
-        self.sink_dropdown.setGeometry(90, 92, 150, 30)
+        #Create checkboxes for each sink option 
+        sinks = self.modules.get("sinks")
+        up_sink = [sink.capitalize() for sink in sinks]
+        self.sink_dict = {sink: i + 1 for i, sink in enumerate(up_sink)}
+        self.sink_checkboxes = [QCheckBox(f"{sink}") for sink in up_sink]
+        
+        self.sink_selected = []
+        #Layout for sink checkboxes
+        self.sink_layout=QGridLayout()
+        row = 0
+        col = 0
+        for checkbox in self.sink_checkboxes:
+            self.sink_layout.addWidget(checkbox, row, col)
+            col += 1
+            if col == 3:
+                col = 0
+                row += 1
+        sink_list = QWidget()
+        sink_list.setLayout(self.sink_layout)
+        sink_list.setContentsMargins(0,0,0,0)
+        
+        #Connect checkbox state to signals to detect which sources were selected 
+        for checkbox in self.sink_checkboxes:
+            checkbox.stateChanged.connect(self.update_selected)
 
-        # Add items to the sinks dropdown
-        for sink in self.modules.get("sinks"):
-            self.sink_dropdown.addItem(sink)
-
-        # Enter selections button
         self.button_box = QDialogButtonBox(
-            QDialogButtonBox.StandardButton.Cancel | QDialogButtonBox.StandardButton.Ok)
+            QDialogButtonBox.StandardButton.Cancel | QDialogButtonBox.StandardButton.Ok
+        )
+
         self.button_box.accepted.connect(self.construct_commands)
         self.button_box.rejected.connect(self.close)
 
-        # Add button to layout
-        self.layout = QVBoxLayout()
-        self.layout.addStretch(1)
-        self.layout.addWidget(self.button_box)
-        self.setLayout(self.layout)
+        main_layout = QVBoxLayout()
+        main_layout.setSpacing(0)
+        main_layout.addWidget(source_list) 
+        main_layout.addWidget(sink_list)
+        main_layout.addWidget(self.button_box)
+        self.setLayout(main_layout)
 
     def construct_commands(self):
         self.selected_ok = True
-
-        # Selected source and sink in GUI
-        self.source_selection = self.modules['sources'].index(self.source_dropdown.currentText())
-        self.sink_selection = self.modules['sinks'].index(self.sink_dropdown.currentText())
-
         self.omnibus = ["python", "-m", "omnibus"]
-        self.source = ["python", f"sources/{self.source_dropdown.currentText()}/main.py"]
-        self.sink = ["python", f"sinks/{self.sink_dropdown.currentText()}/main.py"]
+        self.commands.append(self.omnibus)
 
-        self.commands = [self.omnibus, self.source, self.sink]
-
+        if self.src_selected:
+            for selection in self.src_selected:
+                source = [python_executable, f"sources/{self.modules['sources'][int(selection)-1]}/main.py"]
+                self.commands.append(source)    
+        if self.sink_selected:
+            for selection in self.sink_selected:
+                sink = [python_executable, f"sinks/{self.modules['sinks'][int(selection)-1]}/main.py"]
+                self.commands.append(sink)
         self.close()
+
+    def update_selected(self, state):
+        checkbox = self.sender()
+        text = checkbox.text()
+        
+        if checkbox in self.src_checkboxes:
+            selected_list = self.src_selected
+            index = self.src_dict[text]
+        else:
+            selected_list = self.sink_selected
+            index = self.sink_dict[text]
+        if checkbox.isChecked():
+            if index not in selected_list:
+                selected_list.append(int(index))        
+        else:
+            selected_list.remove(int(index))
 
     def closeEvent(self, event):
         if self.selected_ok:
