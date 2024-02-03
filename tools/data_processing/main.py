aa54809896a2646be067f37624f3a7024caa9680
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2024-02-02 23:03:30 -0500

    add comments to main, prefix on colum select

--- tools/data_processing/main.py
+++ tools/data_processing/main.py
@@ -9,13 +9,17 @@ from tools.data_processing.can_processing import get_can_lines, get_can_cols
 from tools.data_processing.daq_processing import get_daq_lines, get_daq_cols
 from tools.data_processing.log_merger import merge_logs
 
-def offset_timestamps(data1, data2): # assumes data is sorted by timestamp
+def offset_timestamps(data1, data2):
+    """Offset the timestamps of the two data sources so that they start at 0, and return the time offset that was applied to both data sources."""
+    # we need logic to handle the case where one of the data sources is empty, becuase a recording might only have CAN data
     if len(data1) > 0 and len(data2) > 0:
         time_offset = min(data1[0][0], data2[0][0])
     elif len(data1) > 0:
         time_offset = data1[0][0]
     elif len(data2) > 0:
         time_offset = data2[0][0]
+    else:
+        raise ValueError("Both data sources are empty, can't offset timestamps.")
 
     for i in range(len(data1)):
         data1[i][0] -= time_offset
@@ -34,25 +38,29 @@ def data_preview(file_path, mode = "a"):
     daq_cols = []
     can_cols = []
     cols = []
-    if mode == "a" or mode == "d":
-        with open(file_path, "rb") as infile:
+
+    with open(file_path, "rb") as infile:
             daq_cols = get_daq_cols(infile)
+            can_cols = get_can_cols(infile)
+
+    if mode == "a" or mode == "d":
             cols += daq_cols
     if mode == "a" or mode == "c":
-        with open(file_path, "rb") as infile:
-            can_cols = get_can_cols(infile)
             cols += can_cols
 
     print("The following columns are available:")
     for i in range(len(cols)):
-        print(f"{i+1}: {cols[i]}")
-    selection = input("Enter the columns you want to plot, seperated by commas, or leave empty for all: ")
+        prefix = "DAQ" if i < len(daq_cols) else "CAN"
+        print(f"({prefix}) {i+1}: {cols[i]}")
+    selection = input("Enter the numbers for the columns you want to plot, seperated by commas, or leave empty for all: ")
     
+    # parse the selection into a list of indexes in the cols list
     if selection == "":
         indexes = [i for i in range(len(cols))]
     else:
         indexes = [int(i) - 1 for i in selection.split(",")]
 
+    # split the indexes into daq and can indexes, and then get the names of the selected columns
     daq_selected_cols = [daq_cols[i] for i in indexes if i < len(daq_cols)]
     can_selected_cols = [can_cols[i - len(daq_cols)] for i in indexes if i >= len(daq_cols)]
 
@@ -84,13 +92,14 @@ def data_preview(file_path, mode = "a"):
     
     offset_timestamps(daq_data, can_data)
 
-    # check that timestamps are increasing for can
+    # sanity check that timestamps are increasing for can
     for i in range(len(can_data) - 1):
-        if can_data[i][0] > can_data[i+1][0]:
+        if int(can_data[i][0]) > int(can_data[i+1][0]): # compare the timestamps columns (redundant int cast to silence linter)
             print(f"CAN timestamp {can_data[i][0]} is greater than {can_data[i+1][0]}")
         
-    # plot the data
+    # plot the data for the selected columns
     plt.figure()
+
     if mode == "a":
         plt.title("DAQ and CAN data")
     elif mode == "d":
@@ -100,6 +109,7 @@ def data_preview(file_path, mode = "a"):
 
     if mode == "a" or mode == "d":
         for i in range(len(daq_selected_cols)):
+            # plot the time column against the column for each selected colum
             plt.plot([d[0] for d in daq_data], [d[i+1] for d in daq_data], label=daq_selected_cols[i])
 
     if mode == "a" or mode == "c":
