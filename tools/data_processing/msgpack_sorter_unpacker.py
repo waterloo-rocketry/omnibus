c450fe9c845868adf68a3360289833cf801ec9df
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2024-02-11 02:52:37 -0500

    option for later message filtering for more data

--- tools/data_processing/msgpack_sorter_unpacker.py
+++ tools/data_processing/msgpack_sorter_unpacker.py
@@ -1,6 +1,6 @@
 import msgpack
 
-def msgpackFilterUnpacker(infile): # FIXME: this method should not be used, and instead each board's real time should be determined, ignorning the msgpacked timestamp. See https://waterloorocketry.slack.com/archives/C07MX0QDS/p1706481412008559?thread_ts=1706479899.045329&cid=C07MX0QDS
+def msgpackFilterUnpacker(infile,mode="behind_stream"): # FIXME: this method should not be used, and instead each board's real time should be determined, ignorning the msgpacked timestamp. See https://waterloorocketry.slack.com/archives/C07MX0QDS/p1706481412008559?thread_ts=1706479899.045329&cid=C07MX0QDS
     """A function to unpack msgpack data, and then filter it to ensure timestamps are only increasing. Used to filter a second delayed source of messages in the same file."""
     # unpack all messages
     all_messages = []
@@ -8,11 +8,26 @@ def msgpackFilterUnpacker(infile): # FIXME: this method should not be used, and 
         all_messages.append(data)
 
     # discard all messages if the current time is lower than running current time, by not adding them to the filtered and output list
-    filtered_messages = []
-    curr_max_time = 0
-    for i in range(len(all_messages) - 1):
-        if all_messages[i][1] >= curr_max_time:
-            filtered_messages.append(all_messages[i])
-            curr_max_time = all_messages[i][1]
+    if mode == "ahead_stream":
+        filtered_messages = []
+        curr_max_time = 0
+        for i in range(len(all_messages) - 1):
+            if all_messages[i][1] >= curr_max_time:
+                filtered_messages.append(all_messages[i])
+                curr_max_time = all_messages[i][1]
 
-    return filtered_messages
+        return filtered_messages
+    
+    elif mode == "behind_stream":
+        # find all the messages that are higher than the current running time, and then actually discard the ones ahead and only keep the ones behind
+        filtered_messages = []
+        curr_max_time = 0
+        for i in range(len(all_messages) - 1):
+            if all_messages[i][1] >= curr_max_time:
+                curr_max_time = all_messages[i][1]
+            else:
+                filtered_messages.append(all_messages[i])
+
+        return filtered_messages
+    else:
+        return all_messages
