0ce6d6e9fb42229c42fde81f274a582c8f5da3cc
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2024-03-15 22:27:22 -0400

    whole log file dumper

---
+++ tools/data_processing/dump_whole_log.py
@@ -0,0 +1,17 @@
+# A quick script for Artem that dumps every message into a text 
+
+import msgpack
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Read a messagepacked file and output all the unique messages to a text file")
+    parser.add_argument("file", type=str, help="The file to read")
+    args = parser.parse_args()
+
+    with open(args.file, "rb") as infile:
+        with open(f"all_messages_{args.file.split('.log')[0]}.txt", "w") as outfile:
+            for full_data in msgpack.Unpacker(infile):
+                outfile.write(str(full_data) + "\n")
+
+if __name__ == "__main__":
+    main()
