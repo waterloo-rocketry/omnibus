8157a4d2105b05e2af297a29adf687356003317e
Artem <artemis.sonikov12@gmail.com>
2024-03-30 15:18:54 -0400

    Add processing of file at any path (on Windows)

--- tools/data_processing/dump_whole_log.py
+++ tools/data_processing/dump_whole_log.py
@@ -9,9 +9,20 @@ def main():
     args = parser.parse_args()
 
     with open(args.file, "rb") as infile:
-        with open(f"all_messages_{args.file.split('.log')[0]}.txt", "w") as outfile:
-            for full_data in msgpack.Unpacker(infile):
-                outfile.write(str(full_data) + "\n")
+        if args.file[0] == 'C':
+            import os
+            
+            dir, infile_name = os.path.split(args.file)
+            outfile_name = 'all_messages_' + (infile_name.split('.log'))[0] + '.txt'
+            outfile_path = os.path.join(dir, outfile_name)
+            with open(outfile_path, "w") as outfile:
+                for full_data in msgpack.Unpacker(infile):
+                    outfile.write(str(full_data) + "\n")
+                    
+        else:
+            with open(f"all_messages_{args.file.split('.log')[0]}.txt", "w") as outfile:
+                for full_data in msgpack.Unpacker(infile):
+                    outfile.write(str(full_data) + "\n")
 
 if __name__ == "__main__":
     main()
