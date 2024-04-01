59a20fac1990556e7126bb84012897a2a5192e11
Artem Sotnikov <artemis.sonikov12@gmail.com>
2024-04-01 19:27:27 -0400

    Add bash script for easier startup

---
+++ tools/fydp_startup_utils/start_parsley.bat
@@ -0,0 +1,5 @@
+cd ..
+cd ..
+call venv/Scripts/Activate.bat 
+
+python ./sources/parsley/main.py %1
---
+++ tools/fydp_startup_utils/startup.bat
@@ -0,0 +1,3 @@
+start cmd /k "start_omnibus.bat"
+start cmd /k "start_dashboard.bat"
+start_parsley.bat %1 
