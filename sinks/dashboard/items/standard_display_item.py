378582bed178a583b707b4cbf8fb3fad599b1f92
xinhuey <xinhuey.wong@gmail.com>
2024-06-09 11:54:09 -0400

    added the medium text label

--- sinks/dashboard/items/standard_display_item.py
+++ sinks/dashboard/items/standard_display_item.py
@@ -1,14 +1,37 @@
 from publisher import publisher
 from .dashboard_item import DashboardItem
 from .registry import Register
+from pyqtgraph.Qt.QtWidgets import QVBoxLayout, QWidget, QLabel
+import pyqtgraph as pg
 
 @Register
 class StandardDisplayItem (DashboardItem):
     def __init__(self, *args):
         super().__init__(*args)
 
+        self.text = self.parameters.param('text').value()
+
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        # Medium text label
+        self.label = QLabel()
+        
+
+        # Value detection based on plot_dah_item.py
+        self.parameters.param('text').sigValueChanged.connect(self.on_label_change)
+
+        self.layout.addWidget(self.label)
+        
+
     def add_parameters(self):
-        return []
+        text_param = {'name': 'text', 'type': 'str', 'value': ''}
+        return [text_param]
+    
+    def on_label_change(self, param, value):
+        self.text = value
+        self.label.setText(self.text)
+        self.resize(10,10)
 
     @staticmethod
     def get_name():
