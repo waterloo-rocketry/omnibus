0cb4a1bdfff0103980e96dbc92223c41e7a4a69b
xinhuey <xinhuey.wong@gmail.com>
2024-06-11 22:22:24 -0400

    Got the sparkline plot, resized the widget with the medium text label and numerical readout

--- sinks/dashboard/items/standard_display_item.py
+++ sinks/dashboard/items/standard_display_item.py
@@ -1,72 +1,69 @@
 from publisher import publisher
-from .dashboard_item import DashboardItem
-from .registry import Register
-from pyqtgraph.Qt.QtWidgets import QVBoxLayout, QWidget, QLabel
+from pyqtgraph.Qt.QtWidgets import QGridLayout, QMenu, QLabel
 from pyqtgraph.parametertree.parameterTypes import ChecklistParameter
+from pyqtgraph.Qt.QtCore import QEvent
 import pyqtgraph as pg
-from pyqtgraph.Qt.QtCore import Qt
+import numpy as np
+
+from .dashboard_item import DashboardItem
 import config
+from .registry import Register
+
 pg.setConfigOption('background', 'w')
 pg.setConfigOption('foreground', 'k')
 
+
 @Register
-class StandardDisplayItem (DashboardItem):
+class StandardDisplayItem(DashboardItem):
     def __init__(self, *args):
+        # Call this in **every** dash item constructor
         super().__init__(*args)
 
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
-
-        # Medium text label
-        self.label = QLabel("Label")
-        self.label.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-
-        # Big numerical readout
-        self.value = QLabel("Not connected")
-        self.value.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-
-        # Sparkline plot
         self.last = {}
 
-        # Storing the series name as key, time and points as value
+        # storing the series name as key, its time and points as value
+        # since each PlotDashItem can contain more than one curve
         self.times = {}
         self.points = {}
 
-        # Value detection based on plot_dah_item.py
-        self.parameters.param('text').sigValueChanged.connect(self.on_label_change)
+        # Specify the layout
+        self.layout = QGridLayout()
+        self.setLayout(self.layout)
+
         self.parameters.param('series').sigValueChanged.connect(self.on_series_change)
         self.parameters.param('offset').sigValueChanged.connect(self.on_offset_change)
+        self.parameters.param('text').sigValueChanged.connect(self.on_series_change)
 
-        self.text = self.parameters.param('text').value()
 
         self.series = self.parameters.param('series').value()
-        
-        # Single global offset 
+        # just a single global offset for now
         self.offset = self.parameters.param('offset').value()
 
-
-        # Subscribe to stream dictated by properties
+        # subscribe to stream dictated by properties
         for series in self.series:
             publisher.subscribe(series, self.on_data_update)
 
-        # Default color list for plotting multiple curves
+        # a default color list for plotting multiple curves
+        # yellow green cyan white blue magenta
         self.color = ['k', 'g', 'c', 'w', 'b', 'm']
 
-        # Create Sparkline plot
-        self.SparkLine = self.create_sparkline()
-        self.SparkLine_widget = pg.PlotWidget(plotItem = self.SparkLine)
-        
+        # create the plot
+        self.plot = self.create_plot()
 
-        # Add Sparkline to layout
-        self.layout.addWidget(self.label)
-        self.layout.addWidget(self.SparkLine_widget)
-        self.layout.addWidget(self.value)
+        # create the plot widget
+        self.widget = pg.PlotWidget(plotItem=self.plot)
 
-        
-        
+        # Medium Text Label
+        self.label = QLabel("Label")
 
+        #Big numerical readout
+        self.numRead = QLabel("Not Connected")
+
+        # add it to the layout
+        self.layout.addWidget(self.label, 0, 0)
+        self.layout.addWidget(self.widget, 1, 0)
+        self.layout.addWidget(self.numRead, 2, 0)
         self.resize(300,100)
-        
 
     def add_parameters(self):
         text_param = {'name': 'text', 'type': 'str', 'value': ''}
@@ -77,10 +74,6 @@ class StandardDisplayItem (DashboardItem):
         limit_param = {'name': 'limit', 'type': 'float', 'value': 0}
         offset_param = {'name': 'offset', 'type': 'float', 'value': 0}
         return [text_param, series_param, limit_param, offset_param]
-    
-    def on_label_change(self, param, value):
-        self.text = value
-        self.label.setText(self.text)
 
     def on_series_change(self, param, value):
         if len(value) > 6:
@@ -92,72 +85,122 @@ class StandardDisplayItem (DashboardItem):
             publisher.subscribe(series, self.on_data_update)
         # recreate the plot with new series and add it to the layout
         self.plot = self.create_plot()
+        self.text = self.parameters.param('text').value()
+        self.label.setText(self.text)
         self.widget = pg.PlotWidget(plotItem=self.plot)
         self.layout.addWidget(self.widget, 0, 0)
+        self.layout.addWidget(self.widget, 1, 0)
         self.resize(self.parameters.param('width').value(),
                     self.parameters.param('height').value())
-    
+
     def on_offset_change(self, _, offset):
         self.offset = offset
 
-    # Create the sparkline plot item 
-    def create_sparkline(self):
-        sparkline = pg.PlotItem()
-        sparkline.setMenuEnabled(False)
-        sparkline.setMouseEnabled(x = False, y = False)
-        sparkline.hideButtons()
-        sparkline.showAxis('left', False)
-        sparkline.showAxis('bottom', False)
-        sparkline.setMaximumHeight(50)
-        self.sparkline_curves = {}
-
+    # Create the plot item
+    def create_plot(self):
+        plot = pg.PlotItem(left="Data", bottom="Seconds")
+        plot.setMenuEnabled(False)     # hide the default context menu when right-clicked
+        plot.setMouseEnabled(x=False, y=False)
+        plot.hideButtons()
+        plot.setMinimumSize(300, 100)
+        if (len(self.series) > 1):
+            plot.addLegend()
+         # hide the axes
+        plot.showAxis('left', False)
+        plot.showAxis('bottom', False)
+        # draw the curves
+        # storing the series name as key, its plot object as value
+        # update all curves every time on_data_update() is called
+        self.curves = {}
         for i, series in enumerate(self.series):
-            curve = sparkline.plot([], [], pen = self.color[i])
-            self.sparkline_curves[series] = curve
-        
-        return sparkline
-    
-    # def on_data_update(self, stream, payload):
-    #     time, point = payload
+            curve = plot.plot([], [], pen=self.color[i], name=series)
+            self.curves[series] = curve
+            self.times[series] = []
+            self.points[series] = []
+            self.last[series] = 0
 
-    #     point += self.offset
+        # initialize the threshold line, but do not plot it unless a limit is specified
+        self.warning_line = plot.plot([], [], brush=(255, 0, 0, 50), pen='r')
 
-    #     if time - self.last[stream] < 1 / config.GRAPH_RESOLUTION:
-    #         return 
-        
-    #     self.last[stream] = time 
-
-    #     self.times[stream].append(time)
-    #     self.points[stream].append(point)
-
-    #     while self.time[stream][0] < time - config.GRAPH_DURATION:
-    #         self.times[stream].pop(0)
-    #         self.points[stream].pop(0)
-        
-    #     values = list(self.points.values())
-
-    #     if not any(values):
-    #         min_point = 0
-    #         max_point = 0
-
-    #     else:
-    #         min_point = min(min(v) for v in values if v)
-    #         max_point = max(max(v) for v in values if v)
-
-    #     # Update the data curve for sparkline plot
-    #     self.sparkline_curves[stream].setData(self.times[stream], self.points[stream])
-
-    #     # Title for sparkline plot
-    #     self.sparkline.setTitle("/".join(self.series))        
+        return plot
 
     def on_data_update(self, stream, payload):
         time, point = payload
+
+        point += self.offset
+
+        # time should be passed as seconds, GRAPH_RESOLUTION is points per second
+        if time - self.last[stream] < 1 / config.GRAPH_RESOLUTION:
+            return
+
+        self.last[stream] = time
+
+        self.times[stream].append(time)
+        self.points[stream].append(point)
+
+        while self.times[stream][0] < time - config.GRAPH_DURATION:
+            self.times[stream].pop(0)
+            self.points[stream].pop(0)
+
+        # get the min/max point in the whole data set
+
+        values = list(self.points.values())
+
+        if not any(values):
+            min_point = 0
+            max_point = 0
+        else:
+            min_point = min(min(v) for v in values if v)
+            max_point = max(max(v) for v in values if v)
+
+        # set the displayed range of Y axis
+        self.plot.setYRange(min_point, max_point, padding=0.1)
+
+        limit = self.parameters.param('limit').value()
+        if limit != 0:
+            # plot the warning line, using two points (start and end)
+            self.warning_line.setData(
+                [self.times[stream][0], self.times[stream][-1]], [limit] * 2)
+            # set the red tint
+            self.warning_line.setFillLevel(max_point*2)
+        else:
+            self.warning_line.setData([], [])
+            self.warning_line.setFillLevel(0)
+
+        # update the data curve
+        self.curves[stream].setData(self.times[stream], self.points[stream])
+
+        # round the time to the nearest GRAPH_STEP
+        t = round(self.times[stream][-1] / config.GRAPH_STEP) * config.GRAPH_STEP
+        self.plot.setXRange(t - config.GRAPH_DURATION + config.GRAPH_STEP,
+                            t + config.GRAPH_STEP, padding=0)
+
+        # value readout in the title for at most 2 series
+        # title = ""
+        # if len(self.series) <= 2:
+        #     # avg values
+        #     title += "    current: "
+        #     last_values = [self.points[item][-1]
+        #                    if self.points[item] else 0 for item in self.series]
+        #     for v in last_values:
+        #         title += f"[{v: < 4.4f}]"
+        #     title += "    "
+        # # data series name
+        # title += "/".join(self.series)
+        # if len(title) > 50:
+        #     title = title[:50]
+
+        # self.plot.setTitle(title)
+
+        # For the numerical readout label
         self.data = float(point)
-        self.value.setText(f"{self.data:.6f}")
+        self.numRead.setText(f"{self.data:.6f}")
+
 
     @staticmethod
     def get_name():
         return "Standard Display Item"
-    
+
     def on_delete(self):
-        publisher.unsusbcribe_from_all(self.on_data_update)
+        publisher.unsubscribe_from_all(self.on_data_update)
+
