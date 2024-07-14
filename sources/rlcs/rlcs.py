87aa5eb527dbba30fb989f6a55f701c9a5251ba2
Pranav Mahabal <95592003+Pdada1@users.noreply.github.com>
2024-07-14 12:45:47 -0400

    Add thermistor and kelvin sense math to rlcs (#263)
    * Thermistor Conversion Implemented
    (Not Tested or Polished)
    
    * Added Kelvin Sense and Finished Thermistor
    
    * Added the Kelvin Sense (For Kavin Review in PR)
    
    * Comments for Kavin
    
    * Adress PR comments
    
    * Added Try Catch for the Divide by Zero Error
    Fixed wrong parameters in function call
    
    * Fixed Error return for divide by zero
    
    ---------
    
    Co-authored-by: Patrick Gu <patrick@patrickgu.ca>

--- sources/rlcs/rlcs.py
+++ sources/rlcs/rlcs.py
@@ -1,5 +1,6 @@
 import parsley
 from parsley.fields import Enum, Numeric
+import math
 
 Number = int | float
 
@@ -43,6 +44,8 @@ MESSAGE_FORMAT = [
     Numeric("Heater Kelvin Low 2 Voltage", 16, scale=1/1000, big_endian=False),
     Numeric("Heater Kelvin High 1 Voltage", 16, scale=1/1000, big_endian=False),
     Numeric("Heater Kelvin High 2 Voltage", 16, scale=1/1000, big_endian=False),
+    Numeric("Heater Resistance 1", 16, scale=1,big_endian=False),#new field declaration to send these fields over omnibus
+    Numeric("Heater Resistance 2", 16, scale=1,big_endian=False),#new field declaration to send these fields over omnibus
 ]
 
 EXPECTED_SIZE = 2 + parsley.calculate_msg_bit_len(MESSAGE_FORMAT) // 8
@@ -57,8 +60,61 @@ def parse_rlcs(line: str | bytes) -> dict[str, str | Number] | None:
         returns none if data is invalid
     '''
     bit_str = parsley.BitString(data=line[1:-1])
+    key_list_therm=["Heater Thermistor 1","Heater Thermistor 2"]
+    key_list_kelvin=["Heater Kelvin Low 1 Voltage","Heater Kelvin Low 2 Voltage"
+                  "Heater Kelvin High 1 Voltage","Heater Kelvin High 2 Voltage"]
+    key_list_resistance=["Heater Resistance 1","Heater Resistance 2"]
     try:
-        return parsley.parse_fields(bit_str, MESSAGE_FORMAT)
+        res=parsley.parse_fields(bit_str, MESSAGE_FORMAT)
     except ValueError as e:
         print("Invalid data: " + str(e))
-        return None
+        return 
+    
+    for key in ["Heater Current 1","Heater Current 2"]:
+        res[key] = parse_thermistor(res[key],10,4.096)
+
+    #Convert adc bits to voltage to allow for kelvin resistance calculation
+    for key in ["Heater Kelvin Low 1 Voltage","Heater Kelvin Low 2 Voltage"
+                  "Heater Kelvin High 1 Voltage","Heater Kelvin High 2 Voltage"]:
+        res[key] = parse_adc_to_voltage(res[key],10,4.096)
+
+    for index, key in enumerate(["Heater Resistance 1","Heater Resistance 2"]):
+        res[key] = parse_kelvin_resistance(res[key],key_list_kelvin[2+index],key_list_kelvin[index])
+
+    return res
+        
+ 
+    
+def parse_thermistor(adc_value, adc_bits,vref):
+    #Second resistor in voltage divider
+    vlt_dvdr_rstr=5000.0
+    #resistance of the thermistor
+    st_rstnce=10000.0
+    #beta value of the thermistor
+    beta_value=3950.0
+    #temperature of the thermistor resistance
+    st_tmp_cel=25.0
+    #input voltage to the thermistor voltage divider
+    inpt_vlt=5.0
+
+    #convert the adc output to voltage output
+    thrmstr_vlt= parse_adc_to_voltage(adc_value,adc_bits,vref)
+
+    #resistance of the thermistor calculated using voltage divider
+    thrmstr_rstnce=vlt_dvdr_rstr * inpt_vlt / thrmstr_vlt - vlt_dvdr_rstr
+    
+    #uses thermistor beta value to convert 
+    # Formula: Beta=(ln(R1/R2))/((1/T1)-(1/T2))
+    therm_temp_cel = ((math.log (thrmstr_rstnce / st_rstnce) / beta_value)+ 1.0 / st_tmp_cel)**-1
+    return therm_temp_cel
+
+# resistance calculation is performed with voltages taken in from parsley
+def parse_kelvin_resistance(voltageP,voltageN,current):
+    try:
+        return (voltageP - voltageN) / current 
+    except ZeroDivisionError as e:
+        return 0.0
+
+
+def parse_adc_to_voltage(adc_value,adc_bits, vref):
+    return float(adc_value) / (2**adc_bits) * vref
