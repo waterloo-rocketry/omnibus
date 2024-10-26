bf3306f4d6b2653ce52a94c80eba0a9616bcfdf7
Jiucheng Zang <zangjiucheng@gmail.com>
2024-10-26 10:26:14 -0400

    Optimize Code Block, Cli Run Script Support

--- sinks/interamap/src/real_time_parser.py
+++ sinks/interamap/src/real_time_parser.py
@@ -1,10 +1,40 @@
 import queue
 import sys
 from omnibus import Receiver
-from PySide6.QtCore import QThread, Signal, QObject
+from PySide6.QtCore import QThread, Signal
 
-from src.data_struct import Point_GPS, LineString_GPS
+try:
+    from src.data_struct import Point_GPS
+except ImportError:
+    from data_struct import Point_GPS
 
+def parse_gps_data(gps, data):
+    timestamp = "{hrs:02}:{mins:02}:{secs:02}.{dsecs:02}".format(                                
+        **gps.get("GPS_TIMESTAMP", {"hrs": 0, "mins": 0, "secs": 0, "dsecs": 0}))
+    latitude = gps.get("GPS_LATITUDE", {"degs": 0, "mins": 0, "dmins": 0})
+    longitude = gps.get("GPS_LONGITUDE", {"degs": 0, "mins": 0, "dmins": 0})
+    altitude = gps.get("GPS_ALTITUDE", {"altitude": 0, "daltitude": 0})
+    boardId = data.get("board_id")
+
+    # Convert latitude and longitude to decimal degrees
+    lat = convert_to_decimal_degrees(latitude)
+    lon = convert_to_decimal_degrees(longitude)
+        
+    # Combine altitude and decimal altitude
+    he = altitude["altitude"] + altitude["daltitude"] / 100
+
+    point = Point_GPS(lon=lon, lat=lat, he=he, time_stamp=timestamp, board_id=boardId)
+
+    return point
+
+def convert_to_decimal_degrees(coord):
+    degs = coord["degs"]
+    mins = coord["mins"] / 60
+    dmins = coord["dmins"] / 3600
+    decimal = degs + mins + dmins
+    if coord.get("direction") in ["S", "W"]:
+        decimal = -decimal
+    return decimal
 
 class RTParser(QThread):
     gps_RT_data = Signal(object)
@@ -32,39 +62,40 @@ class RTParser(QThread):
             if not self.running:
                 break
 
-            data = self.receiver.recv()
-            msgtype = data.get("msg_type")
-            
-            if msgtype in ["GPS_TIMESTAMP", "GPS_LATITUDE", "GPS_LONGITUDE", "GPS_ALTITUDE"]:
-                gps[msgtype] = data["data"]
-
-            if all(key in gps for key in ["GPS_TIMESTAMP", "GPS_LATITUDE", "GPS_LONGITUDE", "GPS_ALTITUDE"]):
-                timestamp = "{hrs:02}:{mins:02}:{secs:02}.{dsecs:02}".format(                                
-                    **gps.get("GPS_TIMESTAMP", {"hrs": 0, "mins": 0, "secs": 0, "dsecs": 0}))
-                latitude = gps.get("GPS_LATITUDE", {"degs": 0, "mins": 0, "dmins": 0, "direction": "N"})
-                longitude = gps.get("GPS_LONGITUDE", {"degs": 0, "mins": 0, "dmins": 0, "direction": "E"})
-                altitude = gps.get("GPS_ALTITUDE", {"altitude": 0, "daltitude": 0})
-                boardId = data.get("board_id")
-
-                # Convert latitude and longitude to decimal degrees
-                lat = latitude["degs"] + latitude["mins"] / 60 + latitude["dmins"] / 3600
-                if latitude["direction"] == "S":
-                    lat = -lat
-
-                lon = longitude["degs"] + longitude["mins"] / 60 + longitude["dmins"] / 3600
-                if longitude["direction"] == "W":
-                    lon = -lon
-                    
-                # Combine altitude and decimal altitude
-                he = altitude["altitude"] + altitude["daltitude"] / 100
-
-                # Initialize Point_GPS object
-                point = Point_GPS(lon=lon, lat=lat, he=he, time_stamp=timestamp, board_id=boardId)
-
-                # Notification of new data point available
-                self.gps_RT_data.emit(point)
+            try:
+                data = self.receiver.recv()
+                msgtype = data.get("msg_type")
                 
-                # Clear the gps dictionary for the next set of data
-                gps = {}
+                if msgtype in ["GPS_TIMESTAMP", "GPS_LATITUDE", "GPS_LONGITUDE", "GPS_ALTITUDE"]:
+                    gps[msgtype] = data["data"]
+
+                if all(key in gps for key in ["GPS_TIMESTAMP", "GPS_LATITUDE", "GPS_LONGITUDE", "GPS_ALTITUDE"]):
+                    point = parse_gps_data(gps, data)
+
+                    # Notification of new data point available
+                    self.gps_RT_data.emit(point)
+                    
+                    # Clear the gps dictionary for the next set of data
+                    gps.clear()
+
+            except queue.Empty: # Handle empty case 
+                continue
+            
+            except Exception as e:
+                print(f"Error while extracting GPS data: {e}", file=sys.stderr)
+                continue  # Continue processing even if an error occurs
 
             
+if __name__ == "__main__":
+    gps = {}
+    receiver = Receiver("")
+    while True:
+        data = receiver.recv()
+        msgtype = data.get("msg_type")
+        
+        if msgtype in ["GPS_TIMESTAMP", "GPS_LATITUDE", "GPS_LONGITUDE", "GPS_ALTITUDE"]:
+            gps[msgtype] = data["data"]
+
+        if all(key in gps for key in ["GPS_TIMESTAMP", "GPS_LATITUDE", "GPS_LONGITUDE", "GPS_ALTITUDE"]):
+            print(parse_gps_data(gps, data))
+            gps.clear()
