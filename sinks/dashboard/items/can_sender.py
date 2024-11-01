db3c00b9617d585212d84ae6dffaf4801a14ab54
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2024-11-01 12:28:09 -0400

    Fix Dashboard CAN Sender regular expression string (#310)

--- sinks/dashboard/items/can_sender.py
+++ sinks/dashboard/items/can_sender.py
@@ -68,8 +68,8 @@ class CanSender(DashboardItem):
         self.installEventFilter(self.send_signals)
 
         # text field regular expression input mask (won't accept characters not defined here)
-        self.numeric_mask = '[\.\-0-9]'  # allows numbers, periods, and minus sign
-        self.ascii_mask = '[\x00-\x7F]'  # allows all ASCII characters
+        self.numeric_mask = r'[\.\-0-9]'  # allows numbers, periods, and minus sign
+        self.ascii_mask = r'[\x00-\x7F]'  # allows all ASCII characters
 
         # preallocate the arrays since the length of a CAN message is dynamic
         self.fields = [None] * 16  # stores the parsley fields
