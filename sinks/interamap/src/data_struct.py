31b6159b5365e1a0f20a8b8b146c9d5fae46f0a0
Abdel Salam A <105957558+saloxmy@users.noreply.github.com>
2024-11-02 13:01:50 -0400

    Real-time GPS Parser (#309)
    * Implemented real-time gps parser
    
    * Completed real-time parser
    
    * Minor changes
    
    * Minor changes
    
    * Removed libraries; added slot for signal; fixed thread leak
    
    * added boardId field to Point_GPS; real-time parser still doesnt parse boardId
    
    * added board_id to real-time parser
    
    * Optimize Code Block, Cli Run Script Support
    
    * Fix parse bug for the PROCESSOR and GPS messy.
    
    * Also add GPS information (for monitoring the strength of the GPS signal)
    
    * Update variable name, Fix issues, Invite Enum class
    
    ---------
    
    Co-authored-by: saloxmy <>
    Co-authored-by: Jiucheng Zang <zangjiucheng@gmail.com>

--- sinks/interamap/src/data_struct.py
+++ sinks/interamap/src/data_struct.py
@@ -1,14 +1,16 @@
 from typing import List, Optional
 
 class Point_GPS():
-    def __init__(self, lon: float, lat: float, he: float, time_stamp: Optional[str] = None):
+    def __init__(self, lon: float, lat: float, alt: float, num_sats: int, time_stamp, board_id: Optional[str] = None):
         self.time_stamp = time_stamp
+        self.num_sats = num_sats
         self.lon = lon
         self.lat = lat
-        self.he = he
+        self.alt = alt
+        self.board_id = board_id
 
     def __str__(self):
-        return f"Point: {self.lon}, {self.lat}, {self.he}, At {self.time_stamp}"
+        return f"Point: {self.lat}, {self.lon}, {self.alt}, At {self.time_stamp}, With {self.num_sats} satellites, from {self.board_id}"
 
 
 class LineString_GPS():
@@ -23,3 +25,13 @@ class LineString_GPS():
 
     def __str__(self):
         return f"LineString: {self.points}"
+
+
+class Info_GPS():
+    def __init__(self, num_sats: int, quality: int, board_id: Optional[str] = None):
+        self.num_sats = num_sats
+        self.quality = quality
+        self.board_id = board_id
+
+    def __str__(self):
+        return f"Info: {self.num_sats} satellites, Quality: {self.quality}, from {self.board_id}"
