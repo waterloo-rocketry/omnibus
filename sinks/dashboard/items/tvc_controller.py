7f712ddb23c3af7b370e2a21d51a860b352311d0
KavinSatheeskumar <kavinsatheesk@gmail.com>
2024-12-01 21:44:36 -0500

    Make remote master
    continue working on tvc controller
    
    Test and fix minor issues
    
    Add new widgets, update parsley

---
+++ sinks/dashboard/items/tvc_controller.py
@@ -0,0 +1,80 @@
+import time
+from math import ceil, log10
+from typing import List
+from pyqtgraph.Qt.QtCore import Qt, QTimer
+from pyqtgraph.Qt.QtGui import QRegularExpressionValidator, QFontMetrics, QFont
+from pyqtgraph.Qt.QtWidgets import (
+    QHBoxLayout,
+    QLineEdit,
+    QPushButton
+)
+
+from .registry import Register
+from .dashboard_item import DashboardItem
+from utils import EventTracker
+from publisher import publisher
+
+
+@Register
+class TVCController(DashboardItem):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        # Specify the layout
+        self.layout = QHBoxLayout()
+        self.setLayout(self.layout)
+
+        self.widget_one = QLineEdit()
+        self.widget_two = QLineEdit()
+
+        self.send_button = QPushButton('SEND')
+        self.send_button.clicked.connect(self.send_can_message)
+
+        self.layout.addWidget(self.widget_one)
+        self.layout.addWidget(self.widget_two)
+        self.layout.addWidget(self.send_button)
+
+    # whenever the SEND button (or equilvalent) is activated, pulse PyQT input widgets for visual feedback
+    # and upon a successful CAN message encoding, emit the encoded message
+    def send_can_message(self):
+        try:
+            actuator_value_1 = int(self.widget_one.text().strip())
+            actuator_value_2 = int(self.widget_two.text().strip())
+        except:
+            self.widget_one.setText("Actuator value must be integer")
+            self.widget_two.setText("Actuator value must be integer")
+            return
+
+
+        can_message_1 = {
+            'data': {
+                'time': time.time(),
+                'can_msg': {
+                    'msg_type': 'ACTUATOR_CMD',
+                    'board_id': 'ANY',
+                    'time': 0,
+                    'actuator': 'ACTUATOR_CHARGE_AIRBRAKE',
+                    'req_state': actuator_value_1
+                    },  # contains the message data bits
+            }
+        }
+
+        can_message_2 = {
+            'data': {
+                'time': time.time(),
+                'can_msg': {
+                    'msg_type': 'ACTUATOR_CMD',
+                    'board_id': 'ANY',
+                    'time': 0,
+                    'actuator': 'ACTUATOR_CHARGE_PAYLOAD',
+                    'req_state': actuator_value_2
+                    },  # contains the message data bits
+            }
+        }
+
+        publisher.update('outgoing_can_messages', can_message_1)
+        publisher.update('outgoing_can_messages', can_message_2)
+
+    @staticmethod
+    def get_name():
+        return 'TVC controller'
---
+++ sinks/dashboard/items/tvc_controller_preset.py
@@ -0,0 +1,88 @@
+import time
+import os
+from math import ceil, log10
+from typing import List
+from pyqtgraph.Qt.QtCore import Qt, QTimer
+from pyqtgraph.Qt.QtGui import QRegularExpressionValidator, QFontMetrics, QFont
+from pyqtgraph.Qt.QtWidgets import (
+    QHBoxLayout,
+    QFileDialog,
+    QPushButton,
+    QLabel
+)
+
+from .registry import Register
+from .dashboard_item import DashboardItem
+from utils import EventTracker
+from publisher import publisher
+
+
+@Register
+class TVCControllerPreset(DashboardItem):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        # Specify the layout
+        self.layout = QHBoxLayout()
+        self.setLayout(self.layout)
+        self.file = None
+
+        self.file_input = QLabel("-")
+        self.get_file_button = QPushButton('GET FILE')
+        self.get_file_button.clicked.connect(self.get_file)
+
+
+        self.send_button = QPushButton('START SEQUENCE')
+        self.send_button.clicked.connect(self.send_sequence)
+
+        self.layout.addWidget(self.file_input)
+        self.layout.addWidget(self.get_file_button)
+        self.layout.addWidget(self.send_button)
+
+    def get_file(self):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        save_directory = os.path.join(script_dir, "..", "..", "..", "sinks", "dashboard", "saved-files")
+        (filename, _) = QFileDialog.getOpenFileName(self, "Open File", save_directory)
+        self.file = filename
+        self.file_input.setText(self.file)
+
+    def send_sequence(self):
+        self.send_button.setDisabled(True)
+        with open(self.file) as f:
+            for line in f:
+                v1, v2 = line.strip().split(' ')
+
+                can_message_1 = {
+                    'data': {
+                        'time': time.time(),
+                        'can_msg': {
+                            'msg_type': 'ACTUATOR_CMD',
+                            'board_id': 'ANY',
+                            'time': 0,
+                            'actuator': 'ACTUATOR_CHARGE_AIRBRAKE',
+                            'req_state': int(v1)
+                            },  # contains the message data bits
+                    }
+                }
+
+                can_message_2 = {
+                    'data': {
+                        'time': time.time(),
+                        'can_msg': {
+                            'msg_type': 'ACTUATOR_CMD',
+                            'board_id': 'ANY',
+                            'time': 0,
+                            'actuator': 'ACTUATOR_CHARGE_PAYLOAD',
+                            'req_state': int(v2)
+                            },  # contains the message data bits
+                    }
+                }
+
+                publisher.update('outgoing_can_messages', can_message_1)
+                publisher.update('outgoing_can_messages', can_message_2)
+                time.sleep(0.01)
+        self.send_button.setDisabled(False)
+
+    @staticmethod
+    def get_name():
+        return 'TVC controller preset'
