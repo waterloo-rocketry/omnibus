ccdd72acdb0785ac5719cd8f78d02b9e78bd31f2
EmpiricalCode <daniellongschool@gmail.com>
2025-01-31 11:01:58 -0500

    chore: switch zmq to omnibus receiver class

--- sinks/web_dashboard/api/server.py
+++ sinks/web_dashboard/api/server.py
@@ -3,24 +3,21 @@ import zmq
 import tornado.ioloop
 import tornado.web
 import zmq.asyncio
-import msgpack
+
+from omnibus import Receiver
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
 async def zmq_subscriber():
-    # Connect to ZMQ publisher
-    context = zmq.asyncio.Context()
+    
+    receiver = Receiver("")
 
-    socket = context.socket(zmq.SUB)
-    socket.connect("tcp://127.0.0.1:5076")
-    socket.setsockopt_string(zmq.SUBSCRIBE, '')
-
-    # Print broadcasted messages
     while True:
-        channel, timestamp, payload = await socket.recv_multipart()
-        print(channel.decode("utf-8"), msgpack.unpackb(timestamp), msgpack.unpackb(payload))
+        data = receiver.recv()
+        print(data)
+
 
 def make_app():
     return tornado.web.Application([
@@ -31,9 +28,6 @@ if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
     io_loop = tornado.ioloop.IOLoop.current()
-
-    zmq_loop = zmq.asyncio.ZMQEventLoop()
-    io_loop.asyncio_loop = zmq_loop
 
     io_loop.add_callback(zmq_subscriber)
     io_loop.start()
