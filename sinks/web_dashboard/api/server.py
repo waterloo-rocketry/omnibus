0500865da8bb6d8ff69503d3cf5e10498600b351
EmpiricalCode <daniellongschool@gmail.com>
2025-02-10 19:06:31 -0500

    feat: implement tornado websocket to broadcast DAQ messages to client

--- sinks/web_dashboard/api/server.py
+++ sinks/web_dashboard/api/server.py
@@ -1,33 +1,51 @@
-import zmq
-
 import tornado.ioloop
 import tornado.web
-import zmq.asyncio
+import tornado.websocket
 
-from omnibus import Receiver
+import json
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+from omnibus.omnibus import Receiver
 
-async def zmq_subscriber():
-    
-    receiver = Receiver("")
+receiver = Receiver("")
+clients = set()
 
-    while True:
-        data = receiver.recv()
-        print(data)
+class WebSocket(tornado.websocket.WebSocketHandler):
 
+    # To allow connections from all origins (CORS)
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        clients.add(self)
+        print("WebSocket connection opened.")
+
+    def on_message(self, message):
+        print("Received message from client:", message)
+
+    def on_close(self):
+        clients.remove(self)
+        print("WebSocket connection closed.")
 
 def make_app():
     return tornado.web.Application([
-        (r"/", MainHandler),
+        (r"/ws", WebSocket),
     ])
+
+def zmq_subscriber():
+    data = receiver.recv_message(None)
+    stringified_json = json.dumps(data.payload)
+
+    if (data.channel == "DAQ/Fake"):
+        
+        for client in list(clients):
+            try:
+                client.write_message(stringified_json)
+            except:
+                clients.remove(client)
 
 if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
-    io_loop = tornado.ioloop.IOLoop.current()
-
-    io_loop.add_callback(zmq_subscriber)
-    io_loop.start()
+    print("Tornado Server Up")
+    tornado.ioloop.PeriodicCallback(zmq_subscriber, 50).start()
+    tornado.ioloop.IOLoop.current().start()
