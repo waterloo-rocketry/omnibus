7c3209622bc2542bade792a0c482ae82ffed4ab8
Chris Yang <chrisyx511@gmail.com>
2025-03-01 14:44:03 -0500

    Cleaned up main somewhat for data_processing_v2_beta

--- tools/data_processing_v2_beta/main.py
+++ tools/data_processing_v2_beta/main.py
@@ -2,13 +2,16 @@
 
 from processors import DAQDataProcessor
 
+GLOBAL_LOG_FILE = "[PATH HERE]"
+CHANNEL = "DAQ"
+OUTPUT_CSV_FILE = "[PATH HERE]"
 
 # TODO: Make this an actual app and not a script (and maybe GUI?)
 def main() -> None:
-    with open("2025_03_01-02_14_03_AM.log", "rb") as file:
-        processor = DAQDataProcessor(file, "DAQ/Fake")
+    with open(GLOBAL_LOG_FILE, "rb") as file:
+        processor = DAQDataProcessor(file, CHANNEL)
         processor.process()
-        processor.csv_ify("test.csv")
+        processor.csv_ify(OUTPUT_CSV_FILE)
 
 
 if __name__ == "__main__":
