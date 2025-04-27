5d7f7ee97e315c890dc6a8489d7fd8c7b4986131
Jiucheng(Oliver) <git.jiucheng@gmail.com>
2025-04-27 18:04:08 -0400

    Integration Bugs Fix  (#360)
    * Refactor interamap configuration: move to src/config.py and update imports
    
    * Fix variable bugs
    
    * Support Real Time Server with offline mode
    
    * Enhance configuration and GPS cache: add default zoom level, refactor BoardID values, and implement share server toggle functionality
    
    * Hotfix: CAN2.0 TEST
    
    * feat: add script to free a specified port on macOS

--- sinks/interamap/interamap.py
+++ sinks/interamap/interamap.py
@@ -5,6 +5,8 @@ import os
 from PySide6.QtWidgets import QApplication
 from src.main_window import MapWindow
 
+from src.config import ONLINE_MODE, MBTILES_PATH
+import subprocess
 
 def load_stylesheet(file_name):
     """Load QSS stylesheet from the provided file."""
@@ -18,6 +20,11 @@ def interamap_driver():
 
     # Get relative path to the file 
     path = os.path.dirname(os.path.realpath(__file__))
+    
+    if not ONLINE_MODE:
+        # Start the tile server
+        tileserver_process = subprocess.Popen(["python", "-m", "tileserver", MBTILES_PATH])
+        app.aboutToQuit.connect(tileserver_process.terminate)
 
     # Load and apply QSS stylesheet
     stylesheet = load_stylesheet(path+"/resources/styles/lightmode.qss")
