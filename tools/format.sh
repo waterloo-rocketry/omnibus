01dba60eec821cf86c4a53d68e01278544e0e5c0
Chris Yang <chrisyx511@gmail.com>
2025-09-25 10:50:10 -0400

    Update format paths

--- tools/format.sh
+++ tools/format.sh
@@ -10,10 +10,10 @@ fi
 WORKSPACE_DIR=$(git rev-parse --show-toplevel)
 cd "$WORKSPACE_DIR"
 
-autopep8 -i -r sources/ sinks/ omnibus/ tools/ *.py --exit-code
+autopep8 -i -r src/ tools/ *.py --exit-code
 
 # Check for syntax errors or undefined names
 # here (F): https://flake8.pycqa.org/en/latest/user/error-codes.html
 # and here (E): https://pycodestyle.pycqa.org/en/latest/intro.html#error-codes
 # flake8 config can be found in setup.cfg
-flake8
+flake8 src/ tools/
