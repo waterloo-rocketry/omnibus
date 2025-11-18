11ce2dcb9afd2292bdca57f7fb4bc0acc7c6b9fd
k72jiang <k72jiang@uwaterloo.ca>
2025-11-17 22:12:40 -0500

    fix hyphon in comments

--- src/omnibus/server.py
+++ src/omnibus/server.py
@@ -102,7 +102,7 @@ def server(quiet: bool = False) -> NoReturn:
 
 if __name__ == "__main__":  # Entry point for the server
     parser = argparse.ArgumentParser() 
-    # creates a command‑line parser to handle flags/options.
+    # creates a command-line parser to handle flags/options.
     parser.add_argument("--quiet", action="store_true", help="Stop repetitive msgs/sec output")
     ##defines a boolean flag --quiet. If you include --quiet on the command line, 
     ## args.quiet becomes True; if you omit it, it defaults to False.
