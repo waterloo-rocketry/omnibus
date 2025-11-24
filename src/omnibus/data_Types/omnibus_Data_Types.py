fe6df10a9ebaf3b9c9f53754760f95914c730e89
James Smith <jt3smith@uwaterloo.ca>
2025-11-23 23:56:27 -0500

    can message and testing

--- src/omnibus/data_Types/omnibus_Data_Types.py
+++ src/omnibus/data_Types/omnibus_Data_Types.py
@@ -15,5 +15,19 @@ class DAQMessage(BaseModel):
             if len(data_arr) != len(self.relative_timestamps):
                 raise ValueError(f"Length of timestamps must match number of data points per sensor. " +
                                  f"Length of {sensor} data is {len(data_arr)}, while timestamps is {len(self.relative_timestamps)}\n")
-            
         return self
+
+class CANMessage(BaseModel):
+    board_type_id: str
+    msg_type: str
+    msg_prio: str
+    board_inst_id: str
+    can_msg: dict
+    parsely: str
+    message_version: Literal[3]
+
+    @model_validator(mode='after')
+    def check_valid_msg_prio(self):
+        if(self.msg_prio not in ("LOW" , "MEDIUM" , "HIGH" , "HIGHEST")):
+            raise ValueError(f"{self.msg_prio} is not a valid message priority")
+        return self  
--- src/omnibus/data_Types/omnibus_data_types_test.py
+++ src/omnibus/data_Types/omnibus_data_types_test.py
@@ -1,11 +1,13 @@
 
 #Write a nominal test
 
-from .omnibus_Data_Types import DAQMessage
+from .omnibus_Data_Types import DAQMessage, CANMessage
 
 import pytest
 
-def test_DAQ_valid_data():
+class test_DAQ_message:
+
+    def test_DAQ_valid_data():
         myDict = {
             "timestamp" : 12345.2,
             "data": {
@@ -18,15 +20,61 @@ def test_DAQ_valid_data():
         msg = DAQMessage(**myDict)
         assert myDict == msg.model_dump()
 
-def test_DAQ_empty_list():
-    with pytest.raises(TypeError):
-         msg = DAQMessage({})
+    def test_DAQ_relative_timestamps_and_sensor_data_length():
+        myDict = {
+        "timestamp" : 12345.2,
+        "data": {
+        "Sensor A": [3.4, 3, 2]
+        },
+        "relative_timestamps": [1,3],
+        "sample_rate": 1000,
+        "message_version": 3
+        }
+        with pytest.raises(ValueError):
+            msg = DAQMessage(**myDict)
          
+    def test_DAQ_empty_list():
+        with pytest.raises(TypeError):
+            msg = DAQMessage({})
 
-def test_DAQ_invalid_type():
-    with pytest.raises(TypeError):
-        msg = DAQMessage("not a dict")
+    def test_DAQ_invalid_type():
+        with pytest.raises(TypeError):
+            msg = DAQMessage("not a dict")
 
+class test_CAN_Message:
+    def test_CAN_valid_data():
+        my_dict = {
+        "board_type_id": "102s",
+        "msg_type": "sensorB",
+        "msg_prio": "HIGH",
+        "board_inst_id": "432b",
+        "can_msg": {"a" : 34},
+        "parsely": "2",
+        "message_version": 3
+        }
 
+        msg =  CANMessage(**my_dict)
+        assert msg.model_dump() == my_dict
 
+    def test_CAN_invalid_msg_prio():
+        my_dict = {
+        "board_type_id": "102s",
+        "msg_type": "sensorB",
+        "msg_prio": "important",
+        "board_inst_id": "432b",
+        "can_msg": {"a" : 34},
+        "parsely": "2",
+        "message_version": 3
+        }
 
+        with pytest .raises(TypeError):
+            msg = CANMessage(**my_dict)
+
+    def test_CAN_empty_list():
+        with pytest.raises(TypeError):
+            msg = CANMessage({})
+            
+
+    def test_CAN_invalid_type():
+        with pytest.raises(TypeError):
+            msg = CANMessage("not a dict")
