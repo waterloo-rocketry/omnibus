979680641cd666b1bae8172e7b404152305997fb
James Smith <jt3smith@uwaterloo.ca>
2025-11-30 12:44:55 -0500

    fixed typo

--- src/omnibus/data_types/__init__.py
+++ src/omnibus/data_types/__init__.py
@@ -1,4 +1,6 @@
 from .data_types import DAQMessage, CANMessage, ParselyMessage
 from parsley import message_types
+from pydantic import model_validator, BaseModel
 
-__all__ = ["DAQMessage", "CANMessage","ParselyMessage","message_types"]
+__all__ = ["DAQMessage", "CANMessage","ParselyMessage","message_types",
+           "model_validator","BaseModel"]
