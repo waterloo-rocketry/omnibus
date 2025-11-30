d96d956e1ec0195f3db0542bd68e05a85229589f
James Smith <jt3smith@uwaterloo.ca>
2025-11-30 13:39:30 -0500

    added documentation

--- src/omnibus/data_types/data_types.py
+++ src/omnibus/data_types/data_types.py
@@ -1,24 +1,38 @@
 from typing import Literal, Self
 from pydantic import BaseModel, model_validator
 from parsley import message_types
-#DAQMessage type includes typical omnibus message and 4 more literals
+
 
 class DAQMessage(BaseModel):
+    """"
+    DAQmessage class, stroes and validates all DAQ data
+    """
+
     timestamp: float
     data: dict[str, list[int | float]]
     relative_timestamps: list[int | float]
     sample_rate: int
     message_version: Literal[3]
 
+
     @model_validator(mode='after')
     def check_relative_timestamp_length(self) -> Self:
+        """
+        makes sure that all sensor data has the same length as the relative timestamps 
+        """
+
         for sensor, data_arr in self.data.items():
             if len(data_arr) != len(self.relative_timestamps):
                 raise ValueError(f"Length of timestamps must match number of data points per sensor. " +
                                  f"Length of {sensor} data is {len(data_arr)}, while timestamps is {len(self.relative_timestamps)}\n")
         return self
 
+
 class CANMessage(BaseModel):
+    """
+    CANmesage dataclass, holds and validates all can message data
+    """
+
     board_type_id: str
     msg_type: str
     msg_prio: str
@@ -29,6 +43,10 @@ class CANMessage(BaseModel):
 
     @model_validator(mode = 'after')
     def check_all_parsely_data(self) ->Self:
+        """
+        checks all parsely type data for if the name is contained in the parsely library
+        """
+
         message_data = [[self.board_type_id,message_types.board_type_id,"board_type_id"],
                         [self.board_inst_id,message_types.board_inst_id,"board_inst_id"],
                         [self.msg_prio,message_types.msg_prio,"msg_prio"],
@@ -41,6 +59,10 @@ class CANMessage(BaseModel):
         return self
     
 class ParselyMessage(BaseModel):
+    """
+    dataclass for parsely message, holds and validates all types below
+    """
+    
     board_type_id: str
     board_inst_id: str
     msg_prio: str
@@ -51,6 +73,10 @@ class ParselyMessage(BaseModel):
 
     @model_validator(mode = 'after')
     def check_all_parsely_data(self) ->Self:
+        """
+        checks all parsely type data making sure that it is
+        contained in the parsely library 
+        """
         message_data = [[self.board_type_id,message_types.board_type_id,"board_type_id"],
                         [self.board_inst_id,message_types.board_inst_id,"board_inst_id"],
                         [self.msg_prio,message_types.msg_prio,"msg_prio"],
