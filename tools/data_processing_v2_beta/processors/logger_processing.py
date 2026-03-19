3d4e3251971644826373ef37f776f9e43da51d44
Jack-Brown12 <j84brown@uwaterloo.ca>
2026-03-19 18:24:53 -0400

    Implement proto CSV writer

--- tools/data_processing_v2_beta/processors/logger_processing.py
+++ tools/data_processing_v2_beta/processors/logger_processing.py
@@ -1,7 +1,7 @@
 import csv
 import os
 from dataclasses import dataclass
-from typing import TypedDict
+from typing import TypedDict, Any
 
 import parsley
 from sources.parsley.main import FileCommunicator
@@ -15,41 +15,86 @@ LOGGER_EXPECTED_RECEIVE_DATA_FORMAT = {
 }
 
 class LoggerDataProcessor:
-    def __init__(self, log_file_reader):
+    def __init__(self, log_file_reader: Any):
         self._log_file_reader = log_file_reader
-        self.master_columns = [
-            'time', 'board_type_id', 'msg_type', 
-            'pressure', 'temp', 'value', 'latitude' #add more values
+        self.MASTER_COLUMNS = [
+            #  Metadata 
+            'time', 
+            'logger_time',
+            'msg_prio', 
+            'board_type_id', 
+            'board_inst_id', 
+            'msg_type',
+            
+            #  Primary Data
+            'pressure', 
+            'temp', 
+            'imu_id',
+            'linear_accel', 
+            'angular_velocity', 
+            'mag',
+            
+            # GPS Data 
+            'hrs', 'mins', 'secs', 'dsecs',        # Time
+            'degs', 'dmins', 'direction',          # Location
+            'altitude', 'daltitude', 'unit',       # Vertical
+            'num_sats', 'quality',                 # Health
+            
+            #  Actuators & Power 
+            'actuator', 
+            'curr_state', 
+            'req_state',
+            'sensor_id', 
+            'value',
+            
+            # System Health & Errors 
+            'general_error_bitfield', 
+            'board_error_bitfield'
         ]
+        # Preprocessed dict for quick lookup
+        self.column_to_idx = {name: i for i, name in enumerate(self.MASTER_COLUMNS)}
 
-    def _flatten(self, parsed_entry: list) -> dict[str, Any]:
-        pass
 
-    def _map_dict_to_row(self, flat_data: dict) -> list:
+    def _flatten_message(self, parsed_entry: list) -> dict[str, Any]:
+        """
+        Input: ['CAN/Parsley', 1746896240.8, {'board_type_id': 'PROCESSOR', ..., 'data': {'mag': 65479}}]
+        Output: {'board_type_id': 'PROCESSOR', 'mag': 65479, ...}
+        """
+        raw_dict = parsed_entry[2]
         
-        row = [""] * len(self.master_columns)
+        flat_msg = raw_dict.copy()
+        
+        if 'data' in flat_msg:
+            payload = flat_msg.pop('data')  
+            flat_msg.update(payload)
+            
+        return flat_msg
 
-       
-        for key, value in flat_data.items():
-            if key in self.master_columns:
-               
-                index = self.master_columns.index(key)
-                
+    def _map_to_csv_row(self, parsley_output: list) -> list:
+        unix_logger_time = parsley_output[1]
+        flattened_data = self._flatten_message(parsley_output)
+
+        row = [""] * len(self.MASTER_COLUMNS)
+
+        if 'logger_time' in self.column_to_idx:
+            row[self.column_to_idx['logger_time']] = unix_logger_time
+
+        for key, value in flattened_data.items():
+            if key in self.column_to_idx:
+                index = self.column_to_idx[key]
                 row[index] = value
-        
+            
         return row
+       
 
-    def process_and_write(self, output_file, parsed_messages):
+    def process_log_and_write_csv(self, output_file: str, parsed_messages: list):
         with open(output_file, "w", newline="") as f:
             writer = csv.writer(f)
-            writer.writerow(self.master_columns)
+            writer.writerow(self.MASTER_COLUMNS)
 
             for msg in parsed_messages:
-                flat = self._flatten(msg) 
-    
-                manual_row = self._map_dict_to_row(flat)
-            
-                writer.writerow(manual_row)
+                row = self._map_to_csv_row(msg)
+                writer.writerow(row)
 
 # class LoggerDataProcessor:
 #     _log_file_reader: FileCommunicator
