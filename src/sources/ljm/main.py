dc32a8817fc89ae4abf48b5b2b1ac41adb9c1a14
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2026-03-26 14:45:42 -0400

    update ljm and ni message format

--- src/sources/ljm/main.py
+++ src/sources/ljm/main.py
@@ -40,7 +40,7 @@ sender = Sender()
 CHANNEL = "DAQ"
 # Increment whenever data format change, so that new incompatible tools don't
 # attempt to read old logs / messages.
-MESSAGE_FORMAT_VERSION = 2  # Backwards compatible with original version.
+MESSAGE_FORMAT_VERSION = 3
 
 
 class DAQ_SEND_MESSAGE_TYPE(TypedDict):
@@ -48,7 +48,7 @@ class DAQ_SEND_MESSAGE_TYPE(TypedDict):
     data: dict[str, list[float]]
     """
     Each sensor groups a certain number of readings, the bulk read rate of the DAQ.
-    The length of that list corresponds to the length of relative_timestamps_nanoseconds below.
+    The length of that list corresponds to the length of relative_timestamps_seconds below.
     The floating point numbers are arbitrary values depending on the unit of the sensor configured when it was recorded.
     """
     # Example: {
@@ -58,14 +58,14 @@ class DAQ_SEND_MESSAGE_TYPE(TypedDict):
     # }
     # 1.3 and 2.3 are the readings for each sensor at t0, 2.3 and 4.5 for t1, etc.
 
-    relative_timestamps_nanoseconds: list[int]
+    relative_timestamps_seconds: list[float]
     """
-    Corresponding timestamps for each reading of every sensors, calculated from sample rate (dt_ns = 1/sample_rate * 10^9).
+    Corresponding timestamps for each reading of every sensors, calculated from sample rate (dt_ns = 1/sample_rate).
     There can be variation of +- 1ns for every point.
-    Timestamps are based on initial time t_0 = time.time_ns(), meaning they should be always unique.
-    Unit is nanoseconds.
+    Timestamps are based on initial time t_0 = time.time(), meaning they should be always unique.
+    Unit is seconds.
     """
-    # Example: [19, 22, 25] <- 1.3 and 2.3 from above was read at t0 = 19
+    # Example: [0.000000019, 0.000000022, 0.000000025] <- 1.3 and 2.3 from above was read at t0 = 0.000000019
 
     # Rate at which the messages were read, in Hz, dt = 1/sample_rate
     sample_rate: int
@@ -78,14 +78,14 @@ class DAQ_SEND_MESSAGE_TYPE(TypedDict):
 # Function to pass to the callback function. This needs have one
 # parameter/argument, which will be the handle.
 def read_data(handle, num_addresses, scans_per_read, scan_rate, *, quiet=False, no_built_in_log=False):
-    # Converting to nanoseconds to avoid floating point inaccuracy.
-    READ_PERIOD: int = int(1 / cast(int, scan_rate) * 1000000000)
+    # Period between scans in seconds
+    READ_PERIOD: float = 1 / cast(int, scan_rate)
 
     rates = []
 
     # Relative timestamp starting point, starts at current time and scales by READ_PERIOD.
-    # Use current time to have a unique starting point on every collection, ns to prevent floating point error.
-    relative_last_read_time: float = time.time_ns()
+    # Use current time to have a unique starting point on every collection
+    relative_last_read_time: float = time.time()
 
     log = None
     if not no_built_in_log:
@@ -135,7 +135,7 @@ def read_data(handle, num_addresses, scans_per_read, scan_rate, *, quiet=False, 
             data_parsed: DAQ_SEND_MESSAGE_TYPE = {
                 "timestamp": time.time(),
                 "data": calibration.Sensor.parse(data),  # apply calibration
-                "relative_timestamps_nanoseconds": relative_timestamps,
+                "relative_timestamps_seconds": relative_timestamps,
                 "sample_rate": cast(int, scan_rate),
                 "message_format_version": MESSAGE_FORMAT_VERSION,
             }
@@ -143,7 +143,7 @@ def read_data(handle, num_addresses, scans_per_read, scan_rate, *, quiet=False, 
             # Calculate next staring timestamp.
             # Reset the timestamps to a new starting point if there were problems reading.
             relative_last_read_time = (
-                time.time_ns()
+                time.time()
                 if not data or num_of_messages_read < scans_per_read
                 else relative_timestamps[-1] + READ_PERIOD
             )
