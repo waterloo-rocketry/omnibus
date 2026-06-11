93301425c7990fe2fb42b5b3230bb927b67904c1
Chris Yang <chrisyx511@gmail.com>
2026-06-11 19:59:09 -0400

    use gunicorn because python wsgi is a complete mess :)

--- src/websocket_server/docker_entrypoint.sh
+++ src/websocket_server/docker_entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 OM_HOST="${OMNIBUS_SERVER_HOST:-localhost}"
 echo "Omnibus Server: $OM_HOST"
-exec uv run --no-sync ./src/websocket_server/main.py "$@" <<EOF
+cd ./src/websocket_server
+exec uv run --no-sync gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 wsgi:application --bind 0.0.0.0:6767 "$@" <<EOF
 $OM_HOST
 EOF
