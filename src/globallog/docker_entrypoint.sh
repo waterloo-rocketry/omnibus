d07ce5c30793a5cad293a3259eea4e5d573eaa81
Qian Qian "Qubik"‎ <qubik65536@qubik65536.top>
2026-07-23 15:48:15 -0400

    refactor(globallog): Pass Omnibus Server Address via CLI Flag
    Signed-off-by: Qian Qian "Qubik"‎ <qubik65536@qubik65536.top>

--- src/globallog/docker_entrypoint.sh
+++ src/globallog/docker_entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 OM_HOST="${OMNIBUS_SERVER_HOST:-localhost}"
 echo "Omnibus Server: $OM_HOST"
-exec uv run --no-sync ./src/globallog/main.py --quiet "$@" <<EOF
-$OM_HOST
-EOF
+exec uv run --no-sync ./src/globallog/main.py --quiet --omnibus-server-host "$OM_HOST" "$@"
