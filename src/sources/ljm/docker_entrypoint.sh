bb67f31ff037d656a12b0daca7babc9fbd9cc4d3
Qian Qian "Qubik"‎ <qubik65536@qubik65536.top>
2026-07-23 15:49:01 -0400

    refactor(sources/ljm): Pass Omnibus Server Address via CLI Flag
    Signed-off-by: Qian Qian "Qubik"‎ <qubik65536@qubik65536.top>

--- src/sources/ljm/docker_entrypoint.sh
+++ src/sources/ljm/docker_entrypoint.sh
@@ -1,8 +1,5 @@
-#!/bin/sh                                                                                                                                                                                                
+#!/bin/sh
 OM_HOST="${OMNIBUS_SERVER_HOST:-localhost}"
 cp /config/config.py ./src/sources/ljm/config.py || exit $?
 echo "Omnibus Server: $OM_HOST"
-exec uv run --no-sync ./src/sources/ljm/main.py --quiet --no-built-in-log <<EOF
-$OM_HOST
-EOF
-
+exec uv run --no-sync ./src/sources/ljm/main.py --quiet --no-built-in-log --omnibus-server-host "$OM_HOST" "$@"
